11ab67177e7765917bcd88b5d25d0cf2d88ceec8
Anthony Fox <anthony@ccri.com>
2014-04-29 11:05:05 -0400

    Errant keystrokes

--- geomesa-core/src/main/scala/geomesa/core/data/SimpleFeatureEncoder.scala
+++ geomesa-core/src/main/scala/geomesa/core/data/SimpleFeatureEncoder.scala
@@ -52,7 +52,7 @@ class TextFeatureEncoder extends SimpleFeatureEncoder{
   def encode(feature:SimpleFeature) : Value =
     new Value(ThreadSafeDataUtilities.encodeFeature(feature).getBytes)
 
-  def decode(simpleFeatureType: SimpleFeatureTy07pe, featureValue: Value) = {
+  def decode(simpleFeatureType: SimpleFeatureType, featureValue: Value) = {
     ThreadSafeDataUtilities.createFeature(simpleFeatureType, featureValue.toString)
   }
 
