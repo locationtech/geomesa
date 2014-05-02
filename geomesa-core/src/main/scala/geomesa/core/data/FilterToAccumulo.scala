3040cc1a368c59936d4a4f05f498346902b56ffc
Chris Eichelberger <cne1x@ccri.com>
2014-05-02 12:29:31 -0400

    GEOMESA-102 filter-to-accumulo should handle ANDs and ORs
    Simplistic addition that supports relativley simple
    conjunction and disjunction.  Does NOT support negation.

--- geomesa-core/src/main/scala/geomesa/core/data/FilterToAccumulo.scala
+++ geomesa-core/src/main/scala/geomesa/core/data/FilterToAccumulo.scala
@@ -21,6 +21,7 @@ import com.vividsolutions.jts.geom._
 import geomesa.core.index
 import geomesa.utils.geotools.Conversions._
 import geomesa.utils.geotools.GeometryUtils
+import geomesa.utils.text.WKTUtils
 import org.geotools.data.Query
 import org.geotools.factory.CommonFactoryFinder
 import org.geotools.filter.visitor.SimplifyingFilterVisitor
@@ -36,6 +37,7 @@ import org.opengis.filter.expression._
 import org.opengis.filter.spatial._
 import org.opengis.filter.temporal._
 import org.opengis.temporal.Instant
+import org.geotools.filter.text.ecql.ECQL
 
 // FilterToAccumulo extracts the spatial and temporal predicates from the
 // filter while rewriting the filter to optimize for scanning Accumulo
@@ -44,26 +46,184 @@ class FilterToAccumulo(sft: SimpleFeatureType) {
   val dtgField  = index.getDtgDescriptor(sft)
   val geomField = sft.getGeometryDescriptor
 
-  val allTime            = new Interval(0, Long.MaxValue)
-  val wholeWorld         = new Envelope(-180, -90, 180, 90)
+  val allTime              = new Interval(0, Long.MaxValue)
+  val wholeWorld           = new Envelope(-180, -90, 180, 90)
 
-  var spatialPredicate:  Polygon  = null
-  var temporalPredicate: Interval    = null
+  val noPolygon : Polygon  = null
+  val noInterval: Interval = null
+
+  var spatialPredicate:  Polygon  = noPolygon
+  var temporalPredicate: Interval = noInterval
 
   val ff = CommonFactoryFinder.getFilterFactory2
   val geoFactory = JTSFactoryFinder.getGeometryFactory
+
+  implicit def env2poly(env: Envelope): Polygon = WKTUtils.read(
+    "POLYGON((" +
+      env.getMinX + " " + env.getMinY + ", " +
+      env.getMinX + " " + env.getMaxY + ", " +
+      env.getMaxX + " " + env.getMaxY + ", " +
+      env.getMaxX + " " + env.getMinY + ", " +
+      env.getMinX + " " + env.getMinY +
+    "))"
+  ).asInstanceOf[Polygon]
 
   def visit(query: Query): Filter = visit(query.getFilter)
   def visit(filter: Filter): Filter =
     process(filter).accept(new SimplifyingFilterVisitor, null).asInstanceOf[Filter]
 
-  def processChildren(op: BinaryLogicOperator, lf: (Filter, Filter) => Filter): Filter =
-    op.getChildren.reduce { (l, r) => lf(process(l), process(r)) }
-  
+  case class EvalNode(raw: Filter, evaluated: Filter, polygon: Polygon, interval: Interval) {
+    def simplify: Filter = evaluated match {
+      case Filter.INCLUDE if polygon != noPolygon && interval != noInterval =>
+        ff.and(intersects, during)
+      case Filter.INCLUDE if polygon != noPolygon  =>
+        intersects
+      case Filter.INCLUDE if interval != noInterval =>
+        during
+      case f => f
+    }
+
+    def intersects: Filter = raw match {
+      case f: SpatialOperator => f
+      case _                  =>
+        ff.intersects(ff.property(geomField.getLocalName), ff.literal(polygon))
+    }
+
+    def during: Filter = raw match {
+      case f: BinaryTemporalOperator => f
+      case _                         =>
+        ff.during(ff.property(dtgField.getLocalName), ff.literal(interval))
+    }
+  }
+
+  def getSafeUnionPolygon(a: Polygon, b: Polygon): Polygon = {
+    if (a != noPolygon && b != noPolygon) {
+      if (a.overlaps(b)) {
+        val p = a.union(b)
+        p.normalize()
+        p.asInstanceOf[Polygon]
+      } else {
+        // they don't overlap; take the merge of their envelopes
+        // (since we don't support MultiPolygon returns yet)
+        val env = a.getEnvelopeInternal
+        env.expandToInclude(b.getEnvelopeInternal)
+        env2poly(env)
+      }
+    } else noPolygon
+  }
+
+  def getSafeUnionInterval(a: Interval, b: Interval): Interval = {
+    if (a != noInterval && b != noInterval) {
+      new Interval(
+        if (a.getStart.isBefore(b.getStart)) a.getStart else b.getStart,
+        if (a.getEnd.isAfter(b.getEnd)) a.getEnd else b.getEnd
+      )
+    } else noInterval
+  }
+
+  def simplifyChildren(nodes: Seq[EvalNode], fnx: (Filter, Filter) => Filter): Filter = {
+    nodes.map(_.evaluated).filter(_ != Filter.INCLUDE) match {
+      case children if children.size == 0 => Filter.INCLUDE
+      case children if children.size == 1 => children.head
+      case children =>
+        children.foldLeft(children.head)((filterSoFar, child) => ff.or(filterSoFar, child))
+    }
+  }
+
+  def evaluateChildrenIndependently(filter: BinaryLogicOperator): Seq[EvalNode] =
+    filter.getChildren.map(child => {
+      val oldPolygon = spatialPredicate
+      val oldInterval = temporalPredicate
+      spatialPredicate = noPolygon
+      temporalPredicate = noInterval
+      val childEval = process(child)
+      val p = spatialPredicate
+      val t = temporalPredicate
+      spatialPredicate = oldPolygon
+      temporalPredicate = oldInterval
+      EvalNode(child, childEval, p, t)
+    })
+
+  def processOrChildren(op: BinaryLogicOperator): Filter = {
+    val nodes = evaluateChildrenIndependently(op)
+
+    spatialPredicate = noPolygon
+    temporalPredicate = noInterval
+
+    // you can reduce a sequence of nodes if they all set (only) geometry
+    val onlyPolygons = nodes.filter(node => node.polygon != noPolygon && node.interval == noInterval)
+    if (onlyPolygons.size == nodes.size) {
+      spatialPredicate = nodes.foldLeft(nodes.head.polygon)((pSoFar, node) =>
+        getSafeUnionPolygon(pSoFar, node.polygon)
+      )
+      simplifyChildren(nodes, ff.or)
+    } else {
+      // you can reduce a sequence of nodes if they all set (only) time
+      val onlyIntervals = nodes.filter(node => node.polygon == noPolygon && node.interval != noInterval)
+      if (onlyIntervals.size == nodes.size) {
+        temporalPredicate = nodes.foldLeft(nodes.head.interval)((iSoFar, node) =>
+            getSafeUnionInterval(iSoFar, node.interval)
+        )
+        simplifyChildren(nodes, ff.or)
+      } else {
+        // this was neither all geometry nor all interval
+        val leftovers = nodes.map(_.simplify).filter(_ != Filter.INCLUDE)
+        leftovers.size match {
+          case 0 => Filter.INCLUDE
+          case 1 => leftovers.head
+          case _ => ff.or(leftovers)
+        }
+      }
+    }
+  }
+
+  def getSafeIntersectionPolygon(a: Polygon, b: Polygon): Polygon =
+    if (a == noPolygon) b
+    else if (b == noPolygon) a
+    else if (a.intersects(b)) {
+      val p = a.intersection(b)
+      p.normalize()
+      p.asInstanceOf[Polygon]
+    } else noPolygon
+
+  def getSafeIntersectionInterval(a: Interval, b: Interval): Interval =
+    if (a == noInterval) b
+    else if (b == noInterval) a
+    else {
+      new Interval(
+        if (a.getStart.isBefore(b.getStart)) b.getStart else a.getStart,
+        if (a.getEnd.isAfter(b.getEnd)) b.getEnd else a.getEnd
+      )
+    }
+
+  def processAndChildren(op: BinaryLogicOperator): Filter = {
+    val nodes = evaluateChildrenIndependently(op)
+
+    val result = nodes.tail.foldLeft(nodes.head)((soFar, node) => {
+      val nextEval = (soFar.evaluated, node.evaluated) match {
+        case (Filter.EXCLUDE, _) => Filter.EXCLUDE
+        case (_, Filter.EXCLUDE) => Filter.EXCLUDE
+        case (Filter.INCLUDE, e) => e
+        case (e, Filter.INCLUDE) => e
+        case (e0, e1)            => ff.and(e0, e1)
+      }
+      EvalNode(
+        Filter.INCLUDE,
+        nextEval,
+        getSafeIntersectionPolygon(soFar.polygon, node.polygon),
+        getSafeIntersectionInterval(soFar.interval, node.interval)
+      )
+    })
+
+    spatialPredicate = result.polygon
+    temporalPredicate = result.interval
+    result.evaluated
+  }
+
   def process(filter: Filter, acc: Filter = Filter.INCLUDE): Filter = filter match {
     // Logical filters
-    case op: Or    => processChildren(op, ff.or)
-    case op: And   => processChildren(op, ff.and)
+    case op: Or    => processOrChildren(op)
+    case op: And   => processAndChildren(op)
 
     // Spatial filters
     case op: BBOX       => visitBBOX(op, acc)
