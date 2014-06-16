eecca3ebd180bbe904c7d756937aa674f4c0a96e
Andrew Annex <aannex@ccri.com>
2014-06-16 10:43:00 -0400

    Conflicts: 	geomesa-core/src/main/scala/geomesa/core/avro/AvroSimpleFeature.scala 	geomesa-core/src/main/scala/geomesa/core/avro/FeatureSpecificReader.scala 	geomesa-core/src/test/scala/geomesa/core/avro/AvroSimpleFeatureTest.scala

--- geomesa-core/src/main/scala/geomesa/core/avro/AvroSimpleFeature.scala
+++ geomesa-core/src/main/scala/geomesa/core/avro/AvroSimpleFeature.scala
@@ -196,8 +196,11 @@ object AvroSimpleFeature {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         (encodeAttributeName(ad.getLocalName), ad.getType.getBinding)
 =======
+=======
+>>>>>>> f_utf
 =======
 >>>>>>> f_utf
 =======
@@ -241,7 +244,11 @@ object AvroSimpleFeature {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     case validAvro(s) => s
+=======
+    case validAvro(s) => s.replaceAll("_", "__")
+>>>>>>> f_utf
 =======
     case validAvro(s) => s.replaceAll("_", "__")
 >>>>>>> f_utf
@@ -262,7 +269,11 @@ object AvroSimpleFeature {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   val validUnicode = "(.*)_u([A-F0-9]{4})(.*)".r
+=======
+  val validUnicode = "(.*[^_])_u([A-F0-9]{4})(.*)".r
+>>>>>>> f_utf
 =======
   val validUnicode = "(.*[^_])_u([A-F0-9]{4})(.*)".r
 >>>>>>> f_utf
@@ -283,7 +294,11 @@ object AvroSimpleFeature {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       decodeAttributeName(end)
+=======
+      decodeAttributeName(end).replaceAll("__","_")
+>>>>>>> f_utf
 =======
       decodeAttributeName(end).replaceAll("__","_")
 >>>>>>> f_utf
@@ -313,8 +328,11 @@ object AvroSimpleFeature {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         addField(assembler, encodeAttributeName(ad.getLocalName), ad.getType.getBinding, ad.isNillable)
 =======
+=======
+>>>>>>> f_utf
 =======
 >>>>>>> f_utf
 =======
