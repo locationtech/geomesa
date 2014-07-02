caa7ff3876d04df653c3dd4b6a2b6aa99ff9b9dc
Andrew Annex <aannex@ccri.com>
2014-07-02 15:29:07 -0400

    GEOMESA-161 Made changes as per comments on PR

--- geomesa-core/src/main/scala/geomesa/core/data/FilterToAccumulo.scala
+++ geomesa-core/src/main/scala/geomesa/core/data/FilterToAccumulo.scala
@@ -21,10 +21,10 @@ import collection.JavaConversions._
 import com.vividsolutions.jts.geom._
 import geomesa.core.index
 import geomesa.utils.filters.Filters._
-import geomesa.utils.geotools.GeometryUtils.getAntimeridianSafeGeometry
 import geomesa.utils.geometry.Geometry._
 import geomesa.utils.geotools.Conversions._
 import geomesa.utils.geotools.GeometryUtils
+import geomesa.utils.geotools.GeometryUtils._
 import geomesa.utils.time.Time._
 import org.geotools.data.Query
 import org.geotools.filter.visitor.SimplifyingFilterVisitor
@@ -39,7 +39,6 @@ import org.opengis.filter.expression._
 import org.opengis.filter.spatial._
 import org.opengis.filter.temporal._
 import org.opengis.temporal.{Period => OGCPeriod, Instant}
-import geomesa.utils.geohash.BoundingBox
 
 object FilterToAccumulo {
   val allTime              = new Interval(0, Long.MaxValue)
@@ -238,7 +237,7 @@ class FilterToAccumulo(sft: SimpleFeatureType) {
     case op: Not   => processNot(op)
 
     // Spatial filters
-    case op: BBOX       => visitBBOX(op, acc)
+    case op: BBOX       => visitBinarySpatialOp(op, acc)
     case op: DWithin    => visitDWithin(op, acc)
     case op: Within     => visitBinarySpatialOp(op, acc)
     case op: Intersects => visitBinarySpatialOp(op, acc)
@@ -254,64 +253,47 @@ class FilterToAccumulo(sft: SimpleFeatureType) {
     case f: Filter => ff.and(acc, f)
   }
 
-  private def visitBBOX(op: BBOX, acc: Filter): Filter = {
+  private def visitBinarySpatialOp(op: BinarySpatialOperator, acc: Filter): Filter = {
     val e1 = op.getExpression1.asInstanceOf[PropertyName]
-    val e2 = op.getExpression2.asInstanceOf[Literal]
     val attr = e1.evaluate(sft).asInstanceOf[AttributeDescriptor]
     if(!attr.getLocalName.equals(sft.getGeometryDescriptor.getLocalName)) {
       ff.and(acc, op)
     } else {
-      val geom = e2.evaluate(null, classOf[Geometry])
-      val safeGeometry = getAntimeridianSafeGeometry(geom)
-      safeGeometry match {
-        case p: Polygon =>
-          spatialPredicate = JTS.toGeometry(op.getBounds)
-          
-          acc
-        case mp: MultiPolygon =>
-          spatialPredicate = safeGeometry.getEnvelope.asInstanceOf[Polygon]
-          val polygonList = getGeometryListOf(safeGeometry)
-          val filterList = polygonList.map(
-            p => doCorrectSpatialCall(op, sft.getGeometryDescriptor.getLocalName, p)
-          )
-          val alteredFilter = ff.or(filterList)
-          ff.and(acc, alteredFilter)
-      }
+      updateToAntiMeridianSafeFilter(op, acc)
     }
   }
 
-  private def visitBinarySpatialOp(op: BinarySpatialOperator, acc: Filter): Filter = {
-    val e1 = op.getExpression1.asInstanceOf[PropertyName]
+  def updateToAntiMeridianSafeFilter(op: BinarySpatialOperator, acc:Filter) = {
     val e2 = op.getExpression2.asInstanceOf[Literal]
-    val attr = e1.evaluate(sft).asInstanceOf[AttributeDescriptor]
-    if(!attr.getLocalName.equals(sft.getGeometryDescriptor.getLocalName)) {
-      ff.and(acc, op)
-    } else {
-      val geom = e2.evaluate(null, classOf[Geometry])
-      val safeGeometry = getAntimeridianSafeGeometry(geom)
-      spatialPredicate = safeGeometry.getEnvelope.asInstanceOf[Polygon]
-      safeGeometry match {
-        case p: Polygon =>
-          if(!safeGeometry.isRectangle) ff.and(acc, op)
-          else acc
-        case mp: MultiPolygon =>
-          val polygonList = getGeometryListOf(safeGeometry)
-          val filterList = polygonList.map(
-            p => doCorrectSpatialCall(op, sft.getGeometryDescriptor.getLocalName, p)
-          )
-          val alteredFilter = ff.or(filterList)
-          ff.and(acc, alteredFilter)
-      }
+    val geom = e2.evaluate(null, classOf[Geometry])
+    val safeGeometry = getAntimeridianSafeGeometry(geom)
+    safeGeometry match {
+      case p: Polygon =>
+        op match {
+          case op: BBOX =>
+            spatialPredicate = JTS.toGeometry(op.getBounds)
+          case _        =>
+            spatialPredicate = safeGeometry.getEnvelope.asInstanceOf[Polygon]
+        }
+        if (!safeGeometry.isRectangle) ff.and(acc, op)
+        else acc
+      case mp: MultiPolygon =>
+        spatialPredicate = safeGeometry.getEnvelope.asInstanceOf[Polygon]
+        val polygonList = getGeometryListOf(safeGeometry)
+        val filterList = polygonList.map(
+          p => doCorrectSpatialCall(op, sft.getGeometryDescriptor.getLocalName, p)
+        )
+        ff.and(acc, ff.or(filterList))
     }
   }
 
   def visitDWithin(op: DWithin, acc: Filter): Filter = {
     val e1 = op.getExpression1.asInstanceOf[PropertyName]
-    val e2 = op.getExpression2.asInstanceOf[Literal]
     val attr = e1.evaluate(sft).asInstanceOf[AttributeDescriptor]
     if(!attr.getLocalName.equals(sft.getGeometryDescriptor.getLocalName)) {
       ff.and(acc, op)
     } else {
+      val e2 = op.getExpression2.asInstanceOf[Literal]
       val startPoint = e2.evaluate(null, classOf[Point])
       val distance = op.getDistance
       val distanceDegrees = GeometryUtils.distanceDegrees(startPoint, distance)
@@ -362,13 +344,12 @@ class FilterToAccumulo(sft: SimpleFeatureType) {
   }
 
   def doCorrectSpatialCall(op: BinarySpatialOperator, property: String, geom: Geometry): Filter = op match {
-    case op: Within => ff.within( ff.property(property), ff.literal(geom) )
+    case op: Within     => ff.within( ff.property(property), ff.literal(geom) )
     case op: Intersects => ff.intersects( ff.property(property), ff.literal(geom) )
-    case op: Overlaps => ff.overlaps( ff.property(property), ff.literal(geom) )
-    case op: BBOX =>
-      val envelope = geom.getEnvelopeInternal
-      ff.bbox( ff.property(property), envelope.getMinX, envelope.getMinY,
-        envelope.getMaxX, envelope.getMaxY, op.getSRS )
+    case op: Overlaps   => ff.overlaps( ff.property(property), ff.literal(geom) )
+    case op: BBOX       => val envelope = geom.getEnvelopeInternal
+                           ff.bbox( ff.property(property), envelope.getMinX, envelope.getMinY,
+                           envelope.getMaxX, envelope.getMaxY, op.getSRS )
   }
 
   def getGeometryListOf(inMP: Geometry): Seq[Geometry] =
