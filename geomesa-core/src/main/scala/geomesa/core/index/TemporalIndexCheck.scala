917e8f696e40aab963667ae0cb5c4e40a7d20dba
Michael Ronquest <ronquest@ccri.com>
2014-07-08 12:51:32 -0400

    Formatting changes per ahulbert.

--- geomesa-core/src/main/scala/geomesa/core/index/TemporalIndexCheck.scala
+++ geomesa-core/src/main/scala/geomesa/core/index/TemporalIndexCheck.scala
@@ -21,7 +21,6 @@ import geomesa.core.index
 import org.opengis.feature.simple.SimpleFeatureType
 import scala.collection.JavaConverters._
 
-
 /**
  * Utility object for emitting a warning to the user if a SimpleFeatureType contains a temporal attribute, but
  * none is used in the index.
@@ -31,33 +30,39 @@ import scala.collection.JavaConverters._
  * This is useful since the only symptom of this mistake is slower than normal queries on temporal ranges.
  */
 object TemporalIndexCheck extends Logging {
-  def extractNewDTGFieldCandidate(sft:SimpleFeatureType): Option[String] = {
+  def extractNewDTGFieldCandidate(sft: SimpleFeatureType): Option[String] = {
     // check if the attribute is actually present
     val hasValidDtgField = index.getDtgDescriptor(sft).isDefined
+
     // get all attributes which may be used
     val dtgCandidates = scanForTemporalAttributes(sft)
+
     // we may wish to use the first acceptable attribute found, although we currently require just one match
     val firstDtgCandidate = dtgCandidates.headOption
     val hasValidDtgCandidate = dtgCandidates.nonEmpty
+
     // if there is a valid dtg candidate, then we can safely use it
     val dtgShouldBeSet = !hasValidDtgField && hasValidDtgCandidate
+
     // emit a warning to the user
     if (!hasValidDtgField && hasValidDtgCandidate) emitDtgWarning(dtgCandidates)
+
     // if we are going to mutate UserData, notify the user
-    if (dtgShouldBeSet) firstDtgCandidate.map { text => emitDtgNotification(text)}
+    if (dtgShouldBeSet) firstDtgCandidate.map { emitDtgNotification }
     if (dtgShouldBeSet) firstDtgCandidate else None
   }
+
   def emitDtgWarning(matches: List[String]) {
     lazy val theWarning =
-                s"""
-                    |__________Possible problem detected in the SimpleFeatureType_____________
-                    |SF_PROPERTY_START_TIME points to no existing SimpleFeature attribute, or isn't defined.
-                    |However, the following attribute(s) could be used in GeoMesa's temporal index:
-                    |${matches.mkString("\n","\n","\n")}
-                    |Please note that while queries on a temporal attribute will still work,
-                    |queries will be faster if SF_PROPERTY_START_TIME, located in the SimpleFeatureType's UserData,
-                    |points to the attribute's name
-                """.stripMargin
+      s"""
+        |__________Possible problem detected in the SimpleFeatureType_____________
+        |SF_PROPERTY_START_TIME points to no existing SimpleFeature attribute, or isn't defined.
+        |However, the following attribute(s) could be used in GeoMesa's temporal index:
+        |${matches.mkString("\n", "\n", "\n")}
+        |Please note that while queries on a temporal attribute will still work,
+        |queries will be faster if SF_PROPERTY_START_TIME, located in the SimpleFeatureType's UserData,
+        |points to the attribute's name
+      """.stripMargin
     logger.warn(theWarning)
   }
 
@@ -73,6 +78,5 @@ object TemporalIndexCheck extends Logging {
 
   def scanForTemporalAttributes(sft: SimpleFeatureType) =
     sft.getAttributeDescriptors.asScala.toList
-     .withFilter{_.getType.getBinding == classOf[java.util.Date] }
-     .map {_.getLocalName}
+      .withFilter { classOf[java.util.Date] isAssignableFrom _.getType.getBinding } .map { _.getLocalName }
 }
