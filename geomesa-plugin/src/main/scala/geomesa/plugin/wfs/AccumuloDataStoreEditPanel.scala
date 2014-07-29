725a88ff1cff9e25352f4e1afa6bdbe9162e4ab4
Hunter Provyn <hunter@ccri.com>
2014-07-29 15:57:30 -0400

    GEOMESA-236 GeoServer Accumulo Coverage Store password should be hidden

--- geomesa-plugin/src/main/scala/geomesa/plugin/wfs/AccumuloDataStoreEditPanel.scala
+++ geomesa-plugin/src/main/scala/geomesa/plugin/wfs/AccumuloDataStoreEditPanel.scala
@@ -17,19 +17,15 @@
 
 package geomesa.plugin.wfs
 
-import org.apache.wicket.behavior.SimpleAttributeModifier
+import geomesa.plugin.GeoMesaStoreEditPanel
 import org.apache.wicket.markup.html.form.validation.IFormValidator
 import org.apache.wicket.markup.html.form.{FormComponent, Form}
-import org.apache.wicket.markup.html.panel.Panel
-import org.apache.wicket.model.{ResourceModel, IModel, PropertyModel}
+import org.apache.wicket.model.PropertyModel
 import org.geoserver.catalog.DataStoreInfo
-import org.geoserver.web.data.store.StoreEditPanel
-import org.geoserver.web.data.store.panel.{ParamPanel, PasswordParamPanel, TextParamPanel}
-import org.geoserver.web.util.MapModel
 import org.geotools.data.DataAccessFactory.Param
 
 class AccumuloDataStoreEditPanel (componentId: String, storeEditForm: Form[_])
-    extends StoreEditPanel(componentId, storeEditForm) {
+    extends GeoMesaStoreEditPanel(componentId, storeEditForm) {
 
   val model = storeEditForm.getModel
   setDefaultModel(model)
@@ -51,36 +47,4 @@ class AccumuloDataStoreEditPanel (componentId: String, storeEditForm: Form[_])
 
     def validate(form: Form[_]) {}
   })
-
-  def addTextPanel(paramsModel: IModel[_], param: Param): FormComponent[_] = {
-    val paramName = param.key
-    val resourceKey = getClass.getSimpleName + "." + paramName
-    val required = param.required
-    val textParamPanel =
-      new TextParamPanel(paramName,
-        new MapModel(paramsModel, paramName).asInstanceOf[IModel[_]],
-        new ResourceModel(resourceKey, paramName), required)
-    addPanel(textParamPanel, param, resourceKey)
-  }
-
-  def addPasswordPanel(paramsModel: IModel[_], param: Param): FormComponent[_] = {
-    val paramName = param.key
-    val resourceKey = getClass.getSimpleName + "." + paramName
-    val required = param.required
-    val passParamPanel =
-      new PasswordParamPanel(paramName,
-        new MapModel(paramsModel, paramName).asInstanceOf[IModel[_]],
-        new ResourceModel(resourceKey, paramName), required)
-    addPanel(passParamPanel, param, resourceKey)
-  }
-
-  def addPanel(paramPanel: Panel with ParamPanel, param: Param, resourceKey: String): FormComponent[_] = {
-    paramPanel.getFormComponent.setType(classOf[String])
-    val defaultTitle = String.valueOf(param.description)
-    val titleModel = new ResourceModel(resourceKey + ".title", defaultTitle)
-    val title = String.valueOf(titleModel.getObject)
-    paramPanel.add(new SimpleAttributeModifier("title", title))
-    add(paramPanel)
-    paramPanel.getFormComponent
-  }
 }
