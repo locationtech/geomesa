813ff8495cd99d8ab0cd693f6e2a593cd0c11ed2
Hunter Provyn <hunter@ccri.com>
2014-07-30 11:05:45 -0400

    changed boilerplate

--- geomesa-plugin/src/main/scala/geomesa/plugin/GeoMesaStoreEditPanel.scala
+++ geomesa-plugin/src/main/scala/geomesa/plugin/GeoMesaStoreEditPanel.scala
@@ -9,13 +9,22 @@ import org.geoserver.web.data.store.panel.{ParamPanel, PasswordParamPanel, TextP
 import org.geoserver.web.util.MapModel
 import org.geotools.data.DataAccessFactory.Param
 
-/**
- * Created with IntelliJ IDEA.
- * User: fhp
- * Date: 7/29/14
- * Time: 3:30 PM
- * To change this template use File | Settings | File Templates.
+/*
+ * Copyright 2013 Commonwealth Computer Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 abstract class GeoMesaStoreEditPanel (componentId: String, storeEditForm: Form[_])
     extends StoreEditPanel(componentId, storeEditForm) {
 
