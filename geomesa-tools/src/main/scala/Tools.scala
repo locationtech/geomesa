af64bdfd4e8326420c8237d1f7d049857b713392
Andrew Annex <aannex@ccri.com>
2014-07-30 13:52:09 -0400

    updated ingestcmd to be even more in line with old ingest method

--- geomesa-tools/src/main/scala/Tools.scala
+++ geomesa-tools/src/main/scala/Tools.scala
@@ -22,9 +22,7 @@ import org.apache.commons.cli.{Option => Opt}
  *
  * ALT+SHIFT+F10, Right, E, Enter, Tab, enter your command line parameters, Enter.
  */
-class Tools {
-
-}
+class Tools {}
 
 object Tools extends App {
   val parser = new scopt.OptionParser[Config]("geomesa-tools") {
@@ -39,28 +37,43 @@ object Tools extends App {
     } text ("features is a command") children(
       )
     cmd("ingest") action { (_, c) =>
-      c.copy(mode = "ingest") } text "Ingest is a command" children (
+      c.copy(mode = "ingest") } text "Ingest a feature into GeoMesa" children (
       opt[String]('i', "instanceId").action { (s, c) =>
         c.copy(instanceId = s) } text "the ID (name) of the Accumulo instance, e.g:  mycloud" required(),
       opt[String]('z', "zookeepers").action { (s, c) =>
-        c.copy(zookeepers = s) } text "the comma-separated list of Zookeeper nodes that support your Accumulo instance, e.g.:  zoo1:2181,zoo2:2181,zoo3:2181" required(),
+        c.copy(zookeepers = s) } text "the comma-separated list of Zookeeper nodes that" +
+        " support your Accumulo instance, e.g.:  zoo1:2181,zoo2:2181,zoo3:2181" required(),
       opt[String]('u', "user").action { (s, c) =>
         c.copy(user = s) } text "the Accumulo user that will own the connection, e.g.:  root" required(),
       opt[String]('p', "password").action { (s, c) =>
-        c.copy(password = s) } text "the password for the Accumulo user that will own the connection, e.g.:  guest" required(),
+        c.copy(password = s) } text "the password for the Accumulo user that will own the connection," +
+        " e.g.:  guest" required(),
       arg[String]("<authorizations>").action { (s, c) =>
         c.copy(authorizations = s) } text "the (optional) list of comma-separated Accumulo authorizations that" +
         " should be applied to all data written or read by this Accumulo user; note that this is NOT the list of" +
         " low-level database permissions such as 'Table.READ', but more a series of text tokens that decorate cell" +
         " data, e.g.:  Accounting,Purchasing,Testing" optional(),
-      opt[String]('f', "format").action { (s, c) =>
-        c.copy(format = s) } text "format of ingest file" required(),
+      arg[String]("table").action { (s, c) =>
+        c.copy(table = s) } text "the name of the Accumulo table to use -- or create, " +
+        "if it does not already exist -- to contain the new data" optional(),
+      arg[String]("<hdfs path>").action { (s, c) =>
+        c.copy(pathHDFS = s) } text "HDFS path of file to ingest" optional(),
       opt[String]('s', "spec").action { (s, c) =>
         c.copy(spec = s) } text "the specification for the file" required(),
+      opt[String]("latitude").action { (s, c) =>
+        c.copy(latField = s) } text "Name of latitude field" required(),
+      opt[String]("longitude").action { (s, c) =>
+        c.copy(lonField = s) } text "Name of longitude field" required(),
+      opt[String]("datetime").action { (s, c) =>
+        c.copy(dtField = s) } text "Name of the datetime field" required(),
+      opt[String]("dtformat").action { (s, c) =>
+        c.copy(dtFormat = s) } text "Format of the datetime field" required(),
       opt[String]('m', "method").action { (s, c) =>
         c.copy(method = s) } text "the method used to ingest, e.g.: mapreduce" required(),
       opt[String]("file").action { (s, c) =>
-        c.copy(file = s) } text "the file you wish to ingest, e.g.: ~/capelookout.csv" required()
+        c.copy(file = s) } text "the file you wish to ingest, e.g.: ~/capelookout.csv" required(),
+      opt[String]("format").action { (s, c) =>
+        c.copy(format = s) } text "format of ingest file" required()
      )
   }
 
@@ -68,13 +81,11 @@ object Tools extends App {
     config.mode match {
       case "export" =>
         println("exporting")
-      case "feature" => 
+      case "feature" =>
         println("feature-ing")
       case "ingest" =>
         println("Ingesting...")
-        println(config.instanceId)
-        println(config.zookeepers)
-        println(config.user)
+        println(parser.showUsage)
         println(config.toString)
     }
   } getOrElse {
@@ -82,16 +93,14 @@ object Tools extends App {
   }
 }
 
-case class Config(mode: String = null,
-                  instanceId: String = null,
-                  zookeepers: String = null,
-                  user: String = null,
-                  password: String = null,
-                  authorizations: String = null,
-                  format: String = null,
-                  spec: String = null,
-                  method: String = null,
-                  file: String = null)
+case class Config(mode: String = null, instanceId: String = null,
+                  zookeepers: String = null, user: String = null,
+                  password: String = null, authorizations: String = null,
+                  table: String = null, pathHDFS: String = null, spec: String = null,
+                  latField: String = null, lonField: String = null,
+                  dtField: String = null, dtFormat: String = null,
+                  method: String = null, file: String = null,
+                  format: String = null)
 
 
 
