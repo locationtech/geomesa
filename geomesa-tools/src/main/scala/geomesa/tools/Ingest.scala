6b32d34e4b98c247de89f60d6f937772059c7970
Andrew Annex <aannex@ccri.com>
2014-08-01 17:14:03 -0400

    fixed up ingest command to now take format as a subcommand, geojson sortof in, csv sortof working

--- geomesa-tools/src/main/scala/geomesa/tools/Ingest.scala
+++ geomesa-tools/src/main/scala/geomesa/tools/Ingest.scala
@@ -1,7 +1,8 @@
 package geomesa.tools
 
 
-import java.net.URLDecoder
+import java.io.File
+import java.net.{URL, URLDecoder}
 import java.nio.charset.Charset
 import java.util.Date
 import com.google.common.hash.Hashing
@@ -10,11 +11,11 @@ import com.vividsolutions.jts.geom.Coordinate
 import geomesa.core.data.AccumuloDataStore
 import geomesa.core.index.Constants
 import geomesa.feature.AvroSimpleFeatureFactory
-import org.apache.accumulo.core.client.ZooKeeperInstance
-import org.apache.accumulo.core.client.security.tokens.PasswordToken
-import org.geotools.data.{FeatureWriter, DataStoreFinder, DataUtilities, Transaction}
+import org.geotools.data.shapefile.ShapefileDataStore
+import org.geotools.data.{DataStoreFinder, DataUtilities, FeatureWriter, Transaction}
 import org.geotools.factory.Hints
 import org.geotools.filter.identity.FeatureIdImpl
+import org.geotools.geojson.feature._
 import org.geotools.geometry.jts.JTSFactoryFinder
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
@@ -40,31 +41,57 @@ class Ingest(catalogTable: String) {
     dsConfig.put("auths", auths)
     dsConfig.put("visibilities", visibilities)
     dsConfig.put("tableName", config.table)
-    val instance = new ZooKeeperInstance(instanceId, zookeepers)
-    val connector = instance.getConnector(user, new PasswordToken(password))
-    dsConfig.put("connector", connector)
     dsConfig
   }
 
   def defineIngestJob(config: Config): Boolean = {
     val dsConfig = getAccumuloDataStoreConf(config)
-    println(dsConfig)
-    config.method match {
-      case "mapreduce" => println("go go mapreduce!")
-        println("Success")
-        true
-      case "naive" =>
-        println("go go naive!")
-        new SFTIngest(config, dsConfig)
-        true
+    config.format match {
+      case "CSV" | "TSV" =>
+        config.method match {
+          case "mapreduce" =>
+            println("Success")
+            true
+          case "naive" =>
+            new SVIngest(config, dsConfig)
+            true
+          case _ =>
+            println("Error, no such ingest method for CSV or TSV found, no data ingested")
+            false
+        }
+      case "GEOJSON" | "JSON" =>
+        config.method match {
+          case "naive" =>
+            new GeoJsonIngest(config, dsConfig)
+            true
+          case _ =>
+            println("Error, no such ingest method for GEOJSON or JSON found, no data ingested")
+            false
+        }
+      case "GML" | "KML" =>
+        config.method match {
+          case "naive" =>
+            true
+          case _ =>
+            println("Error, no such ingest method for GML or KML found, no data ingested")
+            false
+        }
+      case "SHAPEFILE" | "SHP" =>
+        config.method match {
+          case "naive" =>
+            true
+          case _ =>
+            println("Error, no such ingest method for Shapefiles found, no data ingested")
+            false
+        }
       case _ =>
-        println("Error, no such method exists, no changes made")
+        println(s"Error, format: \'${config.format}\'")
         false
     }
   }
 }
 
-class SFTIngest(config: Config, dsConfig: HashMap[String, Any]) {
+class SVIngest(config: Config, dsConfig: HashMap[String, Any]) {
 
   import scala.collection.JavaConversions._
   import scala.reflect.runtime.universe._
@@ -80,7 +107,7 @@ class SFTIngest(config: Config, dsConfig: HashMap[String, Any]) {
       case _                                    => identity
     }
 
-  lazy val table          = config.table
+  lazy val table            = config.table
   lazy val path             = config.file
   lazy val typeName         = config.typeName
   lazy val idFields         = config.idFields
@@ -106,6 +133,8 @@ class SFTIngest(config: Config, dsConfig: HashMap[String, Any]) {
     ret
   }
 
+  val builder = AvroSimpleFeatureFactory.featureBuilder(sft)
+  lazy val ds = DataStoreFinder.getDataStore(dsConfig).asInstanceOf[AccumuloDataStore]
   ds.createSchema(sft) // this will need to be removed eventually
 
   lazy val geomFactory = JTSFactoryFinder.getGeometryFactory
@@ -117,8 +146,39 @@ class SFTIngest(config: Config, dsConfig: HashMap[String, Any]) {
       .take(sft.getAttributeDescriptors.length - 2)
       .map { desc => getConverter(desc.getType.getBinding) }
 
-  val builder = AvroSimpleFeatureFactory.featureBuilder(sft)
-  lazy val ds = DataStoreFinder.getDataStore(dsConfig).asInstanceOf[AccumuloDataStore]
+  lazy val attributes = sft.getAttributeDescriptors
+  lazy val dtBuilder = buildDtBuilder
+  lazy val idBuilder = buildIDBuilder
+
+  val fw = ds.getFeatureWriterAppend(typeName, Transaction.AUTO_COMMIT)
+  Source.fromFile(path).getLines.foreach { line => parseFeature(line) }
+  def parseFeature(line: String) = {
+    try {
+      val fields = line.toString.split(delim)
+      builder.reset()
+      val id = idBuilder(fields)
+
+      builder.addAll(convertAttributes(fields))
+      val feature = builder.buildFeature(id)
+
+      val lat = feature.getAttribute(latField).asInstanceOf[Double]
+      val lon = feature.getAttribute(lonField).asInstanceOf[Double]
+      val geom = geomFactory.createPoint(new Coordinate(lon, lat))
+      val dtg = dtBuilder(feature.getAttribute(dtgField))
+
+      feature.setDefaultGeometry(geom)
+      feature.setAttribute(dtgTargetField, dtg.toDate)
+      val toWrite = fw.next()
+      sft.getAttributeDescriptors.foreach { ad =>
+        toWrite.setAttribute(ad.getName, feature.getAttribute(ad.getName))
+      }
+      toWrite.getIdentifier.asInstanceOf[FeatureIdImpl].setID(id)
+      toWrite.getUserData.put(Hints.USE_PROVIDED_FID, java.lang.Boolean.TRUE)
+      fw.write()
+    } catch {
+      case t: Throwable => Nil //beware this hides the arrayindex out of bounds exception
+    }
+  }
 
   def convertAttributes(attrs: Array[String]): Seq[AnyRef] =
     converters.zip(attrs).map { case (conv, attr) => conv.apply(attr) }
@@ -150,10 +210,34 @@ class SFTIngest(config: Config, dsConfig: HashMap[String, Any]) {
         (obj: AnyRef) => dtFormat.parseDateTime(obj.asInstanceOf[String])
 
     }.getOrElse(throw new RuntimeException("Cannot parse date"))
+  fw.close()
+}
+
+class GeoJsonIngest(config: Config, dsConfig: HashMap[String, Any]) {
+  import geomesa.utils.geotools.Conversions.RichSimpleFeatureIterator
+  import scala.collection.JavaConversions._
+  lazy val table          = config.table
+  lazy val path             = new File(config.file)
+  lazy val typeName         = config.typeName
+  lazy val dtgTargetField   = Constants.SF_PROPERTY_START_TIME // sft.getUserData.get(Constants.SF_PROPERTY_START_TIME).asInstanceOf[String]
+  lazy val zookeepers       = dsConfig.get("zookeepers")
+  lazy val user             = dsConfig.get("user")
+  lazy val password         = dsConfig.get("password")
+  lazy val auths            = dsConfig.get("auths")
+
+
+  val io = new FeatureJSON()
+  val jsonIterator =  io.streamFeatureCollection(path)
+  lazy val geomFactory = JTSFactoryFinder.getGeometryFactory
+
+  val sft = io.readFeatureCollectionSchema(path, false)
+
+  val builder = AvroSimpleFeatureFactory.featureBuilder(sft)
+  lazy val ds = DataStoreFinder.getDataStore(dsConfig).asInstanceOf[AccumuloDataStore]
+
+  ds.createSchema(sft)
 
   lazy val attributes = sft.getAttributeDescriptors
-  lazy val dtBuilder = buildDtBuilder
-  lazy val idBuilder = buildIDBuilder
 
   class CloseableFeatureWriter {
     val fw = ds.getFeatureWriterAppend(typeName, Transaction.AUTO_COMMIT)
@@ -161,35 +245,37 @@ class SFTIngest(config: Config, dsConfig: HashMap[String, Any]) {
   }
 
   val cfw = new CloseableFeatureWriter
-  //do the work
-  Source.fromFile(path).getLines().foreach { line => parseFeature(cfw.fw, line) }
 
-  def parseFeature(fw: FeatureWriter[SimpleFeatureType, SimpleFeature], line: String): Unit = {
+  jsonIterator.asInstanceOf[RichSimpleFeatureIterator].foreach{ f => parseFeature(cfw.fw, f) }
+
+  def parseFeature(fw: FeatureWriter[SimpleFeatureType, SimpleFeature], feature: SimpleFeature): Unit = {
     try {
-      val fields = line.toString.split(delim)
-      val id = idBuilder(fields)
-
-      builder.reset()
-      builder.addAll(convertAttributes(fields))
-      val feature = builder.buildFeature(id)
-
-      val lat = feature.getAttribute(latField).asInstanceOf[Double]
-      val lon = feature.getAttribute(lonField).asInstanceOf[Double]
-      val geom = geomFactory.createPoint(new Coordinate(lon, lat))
-      val dtg = dtBuilder(feature.getAttribute(dtgField))
-
-      feature.setDefaultGeometry(geom)
-      feature.setAttribute(dtgTargetField, dtg.toDate)
+      val sft = feature.getFeatureType
       val toWrite = fw.next()
       sft.getAttributeDescriptors.foreach { ad =>
         toWrite.setAttribute(ad.getName, feature.getAttribute(ad.getName))
       }
-      toWrite.getIdentifier.asInstanceOf[FeatureIdImpl].setID(id)
+      toWrite.getIdentifier.asInstanceOf[FeatureIdImpl].setID(feature.getID)
       toWrite.getUserData.put(Hints.USE_PROVIDED_FID, java.lang.Boolean.TRUE)
       fw.write()
-    } catch {
+    }catch {
       case t: Throwable => t.printStackTrace()
     }
   }
+}
+
+class ShapeFileIngest(config: Config, dsConfig: HashMap[String, Any]) {
+  lazy val table          = config.table
+  lazy val path             = new URL(config.file)
+  lazy val typeName         = config.typeName
+  lazy val dtgTargetField   = Constants.SF_PROPERTY_START_TIME // sft.getUserData.get(Constants.SF_PROPERTY_START_TIME).asInstanceOf[String]
+  lazy val zookeepers       = dsConfig.get("zookeepers")
+  lazy val user             = dsConfig.get("user")
+  lazy val password         = dsConfig.get("password")
+  lazy val auths            = dsConfig.get("auths")
+
+  val shapeStore = new ShapefileDataStore(path)
+  val name = shapeStore.getTypeNames()(0)
+
 
 }
