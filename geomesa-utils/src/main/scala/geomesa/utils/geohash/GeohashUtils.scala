f402d5ff411cd28df6e1ce276886bc6d2cac5912
Chris Eichelberger <cne1x@ccri.com>
2014-08-05 11:26:04 -0400

    GEOMESA-249 getUniqueGeohashSubstringsInPolygon fails if you don't specify a maximum number of keys less than Int.MaxValue
    Updated the unit tests to reproduce this error, and tweaked the
    routine to compute the expected result.

--- geomesa-utils/src/main/scala/geomesa/utils/geohash/GeohashUtils.scala
+++ geomesa-utils/src/main/scala/geomesa/utils/geohash/GeohashUtils.scala
@@ -749,14 +749,14 @@ object GeohashUtils
   def getUniqueGeohashSubstringsInPolygon(poly: Polygon,
                                           offset: Int,
                                           bits: Int,
-                                          MAX_KEYS_IN_LIST: Int = Int.MaxValue,
+                                          MAX_KEYS_IN_LIST: Int = Int.MaxValue - 1,
                                           includeDots: Boolean = true): Seq[String] = {
 
     val maxBits = (offset + bits) * 5
     val minBits = offset * 5
     val usedBits = bits * 5
     val allResolutions = ResolutionRange(0, Math.min(35, maxBits), 1)
-    val maxKeys = Math.min(2 << usedBits, MAX_KEYS_IN_LIST)
+    val maxKeys = Math.min(2 << usedBits, Math.min(MAX_KEYS_IN_LIST, Int.MaxValue - 1))
     val polyCentroid = poly.getCentroid
 
     // find the smallest GeoHash you can that covers the target geometry
