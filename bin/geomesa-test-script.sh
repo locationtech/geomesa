9fe4d2b4601de88ae27e52340320c8071199de14
Jake Kenneally <jdk2pq@ccri.com>
2014-08-12 09:02:47 -0400

    forgot to add test script

---
+++ bin/geomesa-test-script.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+#
+# Copyright 2014 Commonwealth Computer Research, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#
+# This script allows testing of the geomesa command line tools to ensure that
+# all commands are working properly. Change the variables at the top to suit
+# your testing needs and run from the command line.
+
+USERNAME=username
+PASSWORD=password
+CATALOG=test_catalog
+FEATURENAME=test_feature
+SPEC=id:String:indexed=true,dtg:Date,geom:Point:srid=4326
+
+geomesa -u ${USERNAME} -p ${PASSWORD} create -c ${CATALOG} -f ${FEATURENAME} -s ${SPEC}
+geomesa -u ${USERNAME} -p ${PASSWORD} list -c ${CATALOG}
+geomesa -u ${USERNAME} -p ${PASSWORD} describe -c ${CATALOG} -f ${FEATURENAME}
+geomesa -u ${USERNAME} -p ${PASSWORD} explain -c ${CATALOG} -f ${FEATURENAME} -q include
+geomesa -u ${USERNAME} -p ${PASSWORD} export -c ${CATALOG} -f ${FEATURENAME} -o csv
+geomesa -u ${USERNAME} -p ${PASSWORD} export -c ${CATALOG} -f ${FEATURENAME} -o tsv
+geomesa -u ${USERNAME} -p ${PASSWORD} export -c ${CATALOG} -f ${FEATURENAME} -o geojson
+geomesa -u ${USERNAME} -p ${PASSWORD} export -c ${CATALOG} -f ${FEATURENAME} -o gml
+geomesa -u ${USERNAME} -p ${PASSWORD} export -c ${CATALOG} -f ${FEATURENAME} -o shp
+geomesa -u ${USERNAME} -p ${PASSWORD} delete -c ${CATALOG} -f ${FEATURENAME}
+geomesa -u ${USERNAME} -p ${PASSWORD} list -c ${CATALOG}
