0055f149fcf59bd7f43b45737d4e79dca3ec5f57
Jim Hughes <jnh5y@ccri.com>
2014-08-13 16:38:40 -0400

    GEOMESA-201 Compute geometries from Geometry Filters
    * This pr makes a first pass at extracting geometries to cover from the geometry filters.
    * In order to do this, various things are refactored.
    * GeohashUtils.getUniqueGeohashSubstringsInPolygon is widened to accept Geometries.

---
+++ geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/FilterHelper.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2014 Commonwealth Computer Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.locationtech.geomesa.core.index
+
+import com.vividsolutions.jts.geom.{Geometry, MultiPolygon, Point, Polygon}
+import org.locationtech.geomesa.core.filter._
+import org.locationtech.geomesa.utils.geohash.GeohashUtils
+import org.locationtech.geomesa.utils.geohash.GeohashUtils._
+import org.locationtech.geomesa.utils.geotools.GeometryUtils
+import org.opengis.feature.simple.SimpleFeatureType
+import org.opengis.filter.Filter
+import org.opengis.filter.expression.{Literal, PropertyName}
+import org.opengis.filter.spatial._
+
+import scala.collection.JavaConversions._
+
+object FilterHelper {
+  // Let's handle special cases with topological filters.
+  def updateTopologicalFilters(filter: Filter, featureType: SimpleFeatureType) = {
+    filter match {
+      case dw: DWithin    => rewriteDwithin(dw)
+      case op: BBOX       => visitBBOX(op, featureType)
+      case op: Within     => visitBinarySpatialOp(op, featureType)
+      case op: Intersects => visitBinarySpatialOp(op, featureType)
+      case op: Overlaps   => visitBinarySpatialOp(op, featureType)
+      case _ => filter
+    }
+  }
+
+  def visitBinarySpatialOp(op: BinarySpatialOperator, featureType: SimpleFeatureType): Filter = {
+    val e1 = op.getExpression1.asInstanceOf[PropertyName]
+    val e2 = op.getExpression2.asInstanceOf[Literal]
+    val geom = e2.evaluate(null, classOf[Geometry])
+    val safeGeometry = getInternationalDateLineSafeGeometry(geom)
+    updateToIDLSafeFilter(op, safeGeometry, featureType)
+  }
+
+  def visitBBOX(op: BBOX, featureType: SimpleFeatureType): Filter = {
+    val e1 = op.getExpression1.asInstanceOf[PropertyName]
+    val e2 = op.getExpression2.asInstanceOf[Literal]
+    val geom = addWayPointsToBBOX( e2.evaluate(null, classOf[Geometry]) )
+    val safeGeometry = getInternationalDateLineSafeGeometry(geom)
+    updateToIDLSafeFilter(op, safeGeometry, featureType)
+  }
+
+  def updateToIDLSafeFilter(op: BinarySpatialOperator, geom: Geometry, featureType: SimpleFeatureType): Filter = geom match {
+    case p: Polygon =>
+      dispatchOnSpatialType(op, featureType.getGeometryDescriptor.getLocalName, p)
+    case mp: MultiPolygon =>
+      val polygonList = getGeometryListOf(geom)
+      val filterList = polygonList.map {
+        p => dispatchOnSpatialType(op, featureType.getGeometryDescriptor.getLocalName, p)
+      }
+      ff.or(filterList)
+  }
+
+  def getGeometryListOf(inMP: Geometry): Seq[Geometry] =
+    for( i <- 0 until inMP.getNumGeometries ) yield inMP.getGeometryN(i)
+
+  def dispatchOnSpatialType(op: BinarySpatialOperator, property: String, geom: Geometry): Filter = op match {
+    case op: Within     => ff.within( ff.property(property), ff.literal(geom) )
+    case op: Intersects => ff.intersects( ff.property(property), ff.literal(geom) )
+    case op: Overlaps   => ff.overlaps( ff.property(property), ff.literal(geom) )
+    case op: BBOX       => val envelope = geom.getEnvelopeInternal
+      ff.bbox( ff.property(property), envelope.getMinX, envelope.getMinY,
+        envelope.getMaxX, envelope.getMaxY, op.getSRS )
+  }
+
+  def addWayPointsToBBOX(g: Geometry): Geometry = {
+    val gf = g.getFactory
+    val geomArray = g.getCoordinates
+    val correctedGeom = GeometryUtils.addWayPoints(geomArray).toArray
+    gf.createPolygon(correctedGeom)
+  }
+
+  // Rewrites a Dwithin (assumed to express distance in meters) in degrees.
+  def rewriteDwithin(op: DWithin): Filter = {
+    val e2 = op.getExpression2.asInstanceOf[Literal]
+    val startPoint = e2.evaluate(null, classOf[Point])
+    val distance = op.getDistance
+    val distanceDegrees = GeometryUtils.distanceDegrees(startPoint, distance)
+
+    // NB: The ECQL spec doesn't allow for us to put the measurement in "degrees",
+    //  but that's how this filter will be used.
+    ff.dwithin(
+      op.getExpression1,
+      ff.literal(startPoint),
+      distanceDegrees,
+      "meters")
+  }
+
+  def extractGeometry(bso: BinarySpatialOperator) = {
+    bso.getExpression1.evaluate(null, classOf[Geometry]) match {
+      case g: Geometry => Seq(GeohashUtils.getInternationalDateLineSafeGeometry(g))
+      case _           =>
+        bso.getExpression2.evaluate(null, classOf[Geometry]) match {
+          case g: Geometry => Seq(GeohashUtils.getInternationalDateLineSafeGeometry(g))
+        }
+    }
+  }
+}
