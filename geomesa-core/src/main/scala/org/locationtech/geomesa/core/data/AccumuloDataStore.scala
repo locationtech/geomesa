60b59a9dc52fe864ddeda60bcd43a779a1780d0e
David McWhorter <mcwhorter@ccri.com>
2014-08-14 12:00:20 -0400

    GEOMESA-290 using only .removeSchema to delete features and deleting _queries tables

--- geomesa-core/src/main/scala/org/locationtech/geomesa/core/data/AccumuloDataStore.scala
+++ geomesa-core/src/main/scala/org/locationtech/geomesa/core/data/AccumuloDataStore.scala
@@ -343,30 +343,37 @@ class AccumuloDataStore(val connector: Connector,
    * Deletes the tables from Accumulo created from the Geomesa SpatioTemporal Schema, and deletes
    * metadata from the catalog. If the table is an older 0.10.x table, we throw an exception.
    *
+   * This version overrides the default geotools removeSchema function and uses 1 thread for
+   * querying during metadata deletion.
+   *
+   * @param featureName the name of the feature
+   */
+  override def removeSchema(featureName: String) = removeSchema(featureName, 1)
+
+  /**
+   * Deletes the tables from Accumulo created from the Geomesa SpatioTemporal Schema, and deletes
+   * metadata from the catalog. If the table is an older 0.10.x table, we throw an exception.
+   *
    * @param featureName the name of the feature
    * @param numThreads the number of concurrent threads to spawn for querying during metadata deletion
    */
-  def deleteSchema(featureName: String, numThreads: Int = 1) = {
+  def removeSchema(featureName: String, numThreads: Int = 1) = {
     if (readMetadataItem(featureName, ST_IDX_TABLE_CF).nonEmpty) {
-      removeSchema(featureName)
+      val featureType = getSchema(featureName)
+
+      val spatioTemporalIdxTable = formatSpatioTemporalIdxTableName(catalogTable, featureType)
+      val attributeIndexTable = formatAttrIdxTableName(catalogTable, featureType)
+      val recordTable = formatRecordTableName(catalogTable, featureType)
+      val queriesTable = formatQueriesTableName(catalogTable, featureType)
+
+      List(spatioTemporalIdxTable, attributeIndexTable, recordTable, queriesTable).foreach { t =>
+        if(tableOps.exists(t)) tableOps.delete(t)
+      }
+
       deleteMetadata(featureName, numThreads)
     } else {
       throw new RuntimeException("Cannot delete schema for this version of the data store")
     }
-  }
-
-  /**
-   * Retrieves the Geotools SpatioTemporal Schema and deletes the three previously created tables
-   *
-   * @param featureName the name of the table to query and delete from
-   */
-  override def removeSchema(featureName: String) = {
-    val featureType            = getSchema(featureName)
-    val spatioTemporalIdxTable = formatSpatioTemporalIdxTableName(catalogTable, featureType)
-    val attributeIndexTable    = formatAttrIdxTableName(catalogTable, featureType)
-    val recordTable            = formatRecordTableName(catalogTable, featureType)
-
-    List(spatioTemporalIdxTable, attributeIndexTable, recordTable).foreach { t => if (tableOps.exists(t)) tableOps.delete(t) }
   }
 
   /**
@@ -890,20 +897,45 @@ class AccumuloDataStore(val connector: Connector,
 
 object AccumuloDataStore {
 
-  // Format record table name for Accumulo...table name is stored in metadata for other usage
-  // and provide compatibility moving forward if table names change
+  /**
+   * Format record table name for Accumulo...table name is stored in metadata for other usage
+   * and provide compatibility moving forward if table names change
+   * @param catalogTable
+   * @param featureType
+   * @return
+   */
   def formatRecordTableName(catalogTable: String, featureType: SimpleFeatureType) =
     formatTableName(catalogTable, featureType, "records")
 
-  // Format record table name for Accumulo...table name is stored in metadata for other usage
-  // and provide compatibility moving forward if table names change
+  /**
+   * Format record table name for Accumulo...table name is stored in metadata for other usage
+   * and provide compatibility moving forward if table names change
+   * @param catalogTable
+   * @param featureType
+   * @return
+   */
   def formatSpatioTemporalIdxTableName(catalogTable: String, featureType: SimpleFeatureType) =
     formatTableName(catalogTable, featureType, "st_idx")
 
-  // Format record table name for Accumulo...table name is stored in metadata for other usage
-  // and provide compatibility moving forward if table names change
+  /**
+   * Format record table name for Accumulo...table name is stored in metadata for other usage
+   * and provide compatibility moving forward if table names change
+   * @param catalogTable
+   * @param featureType
+   * @return
+   */
   def formatAttrIdxTableName(catalogTable: String, featureType: SimpleFeatureType) =
     formatTableName(catalogTable, featureType, "attr_idx")
+
+  /**
+   * Format record table name for Accumulo...table name is stored in metadata for other usage
+   * and provide compatibility moving forward if table names change
+   * @param catalogTable
+   * @param featureType
+   * @return
+   */
+  def formatQueriesTableName(catalogTable: String, featureType: SimpleFeatureType) =
+    formatTableName(catalogTable, featureType, "queries")
 
   // only alphanumeric is safe
   val SAFE_FEATURE_NAME_PATTERN = "^[a-zA-Z0-9]+$"
