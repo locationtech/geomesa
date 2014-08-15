a089bfbb0a3e1b8e19b90ed93d126c5741ce8482
Anthony Fox <anthony@ccri.com>
2014-08-15 09:11:23 -0400

    GEOMESA-298 DBSCAN as a WPS service
    Implemented DBSCAN as a WPS.  Provided an example
    request that creates a new layer from the result of
    the DBSCAN clustering.

---
+++ geomesa-plugin/src/main/scala/org/locationtech/geomesa/plugin/process/DBSCANProcess.scala
@@ -0,0 +1,85 @@
+package org.locationtech.geomesa.plugin.process
+
+import java.util.UUID
+
+import org.apache.commons.math3.ml.clustering.{Clusterable, DBSCANClusterer}
+import org.apache.commons.math3.ml.distance.DistanceMeasure
+import org.geotools.data.DataUtilities
+import org.geotools.data.simple.SimpleFeatureCollection
+import org.geotools.geometry.jts.{GeometryBuilder, JTSFactoryFinder}
+import org.geotools.process.factory.{DescribeParameter, DescribeProcess, DescribeResult}
+import org.geotools.referencing.GeodeticCalculator
+import org.geotools.referencing.crs.DefaultGeographicCRS
+import org.locationtech.geomesa.feature.AvroSimpleFeatureFactory
+import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
+import org.opengis.feature.simple.SimpleFeature
+
+import scala.collection.JavaConversions._
+
+@DescribeProcess(
+  title = "Density Based Spatial Clustering of Applications with Noise",
+  description = "Clustering"
+)
+class DBSCANProcess {
+
+  import org.locationtech.geomesa.utils.geotools.Conversions._
+  private val outputSFT = SimpleFeatureTypes.createType("geomesa:dbscan", "radius:Double,geom:Polygon:srid=4326")
+  private val builder = AvroSimpleFeatureFactory.featureBuilder(outputSFT)
+  private val geomFactory = JTSFactoryFinder.getGeometryFactory
+  private val geomBuilder = new GeometryBuilder(geomFactory)
+
+  @DescribeResult(
+    name = "layerName",
+    `type` = classOf[SimpleFeatureCollection],
+    description = "Name of the new featuretype, with workspace")
+  def execute(
+               @DescribeParameter(
+                 name = "features",
+                 description = "Input feature collection")
+               features: SimpleFeatureCollection,
+
+               @DescribeParameter(
+                 name = "eps",
+                 description = "Epsilon error")
+               eps: Double,
+
+               @DescribeParameter(
+                 name = "minPts",
+                 description = "The minimum number of points to form a cluster")
+               minPts: Int
+
+               ): SimpleFeatureCollection = {
+
+    val calc = new GeodeticCalculator(DefaultGeographicCRS.WGS84)
+
+    val clusterer = new DBSCANClusterer[FeatureClusterable](eps, 3, new DistanceMeasure {
+      override def compute(p1: Array[Double], p2: Array[Double]): Double = {
+        calc.setStartingGeographicPoint(p1(0), p1(1))
+        calc.setDestinationGeographicPoint(p2(0), p2(1))
+        calc.getOrthodromicDistance
+      }
+    })
+
+    val clusters = clusterer.cluster(features.features().toList.map(new FeatureClusterable(_)))
+    val resultFeatures = clusters.map { c =>
+      val points = c.getPoints.map(_.sf.point)
+      val geom = geomFactory.buildGeometry(points)
+      val pt = geom.getCentroid
+      val radius = points.map(_.distance(pt)).max
+      val circle = geomBuilder.circle(pt.getX, pt.getY, radius, 20)
+      builder.reset()
+      builder.add(radius)
+      builder.add(circle)
+      builder.buildFeature(UUID.randomUUID().toString)
+    }
+    DataUtilities.collection(resultFeatures)
+  }
+
+  class FeatureClusterable(val sf: SimpleFeature) extends Clusterable {
+    override def getPoint: Array[Double] = {
+      val coord = sf.point.getCoordinate
+      Array(coord.x, coord.y)
+    }
+  }
+
+}
---
+++ geomesa-plugin/src/test/scala/org/locationtech/geomesa/process/DBSCANProcessTest.scala
@@ -0,0 +1,48 @@
+package org.locationtech.geomesa.process
+
+import java.util.UUID
+
+import org.geotools.data.DataUtilities
+import org.geotools.geometry.jts.GeometryBuilder
+import org.junit.runner.RunWith
+import org.locationtech.geomesa.feature.AvroSimpleFeatureFactory
+import org.locationtech.geomesa.plugin.process.DBSCANProcess
+import org.locationtech.geomesa.utils.geotools.Conversions._
+import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+import scala.collection.JavaConversions._
+
+@RunWith(classOf[JUnitRunner])
+class DBSCANProcessTest extends Specification {
+
+  "DBSCANProcess" should {
+    val sft = SimpleFeatureTypes.createType("input", "geom:Point:srid=4326")
+    val geomBuilder = new GeometryBuilder
+    
+    // cluster 1
+    val c1 = Seq((10, 10), (11, 11), (10, 11), (11, 10))
+
+    // cluster 2
+    val c2 = Seq((70, 70), (71, 71), (70, 71), (71, 70))
+
+    val sf = (c1 ++ c2).map { case (x, y) =>
+      val pt = geomBuilder.point(x, y)
+      val id = UUID.randomUUID().toString
+      val f = AvroSimpleFeatureFactory.buildAvroFeature(sft, Seq(), id)
+      f.setDefaultGeometry(pt)
+      f
+    }
+
+    val featureColl = DataUtilities.collection(sf)
+    
+    "cluster simple features" >> {
+      val proc = new DBSCANProcess
+      val procresults = proc.execute(featureColl, 200000, 3)
+      val clusters = procresults.features().toList
+      "into two clusters" >> { clusters.length must beEqualTo(2) }
+      "with non-zero radius" >> { clusters.head.getAttribute("radius").asInstanceOf[Double] must be greaterThan 0.0 }
+    }
+  }
+}
