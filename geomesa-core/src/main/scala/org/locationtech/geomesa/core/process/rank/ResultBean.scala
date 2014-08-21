1cbc663b521b94728470f21c80ea1958d8005809
Nick Hamblet <nhamblet@ccri.com>
2014-08-21 13:49:55 -0400

    More refactoring/styling changes in ResultBean

--- geomesa-core/src/main/scala/org/locationtech/geomesa/core/process/rank/ResultBean.scala
+++ geomesa-core/src/main/scala/org/locationtech/geomesa/core/process/rank/ResultBean.scala
@@ -117,17 +117,16 @@ case class ResultBean(@BeanProperty results: java.util.List[RankingValuesBean], 
   def keyMap = results.asScala.groupBy(_.key)
 
   def merge(other: ResultBean): ResultBean = {
-    val km1 = keyMap
-    val km2 = other.keyMap
-    val allKeys = km1.keySet ++ km2.keySet
-    def emptyBuffer = mutable.Buffer[RankingValuesBean]()
-    val valueLists = allKeys.map(k => (k, km1.getOrElse(k, emptyBuffer) ++ km2.getOrElse(k, emptyBuffer)))
-    ResultBean.fromRankingValues(valueLists.map {
-      case (key, resultList) =>
-        (key, resultList.foldLeft(RankingValues.emptyOne(gridSize, nRouteGridCells)) {
-          case (combined, current) => combined.merge(current.toRankingValues(gridSize, nRouteGridCells))
-        })
-    }.toMap, sortBy)
+    ResultBean.fromRankingValues(
+      (results.asScala ++ other.results.asScala) // put all result together
+        .groupBy(_.key)                          // now a Map[String, Buffer[RankingValuesBean]
+        .mapValues(buffRVB => {                  // merge all beans in each buffer independently
+          buffRVB.foldLeft(RankingValues.emptyOne(gridSize, nRouteGridCells)) { case (comb, curr) =>
+            comb.merge(curr.toRankingValues(gridSize, nRouteGridCells))
+          }
+        }),
+      sortBy
+    )
   }
 }
 
