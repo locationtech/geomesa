cf9d3b7f4a4ca42c4e9f204530b5a923c50d1e47
Jianping Wang <jw9bn@ccri.com>
2014-12-01 13:19:57 -0500

    Correct license info.

--- geomesa-raster/src/main/scala/org/locationtech/geomesa/raster/ingest/GeoserverClientService.scala
+++ geomesa-raster/src/main/scala/org/locationtech/geomesa/raster/ingest/GeoserverClientService.scala
@@ -1,29 +1,18 @@
-/**
- * Copyright (c) Commonwealth Computer Research, Inc. 2006 - 2012
- * All Rights Reserved, www.ccri.com
+/*
+ * Copyright 2014 Commonwealth Computer Research, Inc.
  *
- * Developed under contracts for:
- * US Army / RDECOM / CERDEC / I2WD and
- * SBIR Contract for US Navy / Office of Naval Research
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This code may contain SBIR protected information.  Contact CCRi prior to
- * distribution.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Begin SBIR Data Rights Statement
- * Contract No.:  N00014-08-C-0254
- * Contractor Name:  Commonwealth Computer Research, Inc
- * Contractor Address:  1422 Sachem Pl, Unit #1, Charlottesville, VA 22901
- * Expiration of SBIR Data Rights Period:  5/9/2017
- * The Government's rights to use, modify, reproduce, release, perform, display,
- * or disclose technical data or computer software marked with this legend are
- * restricted during the period shown as provided in paragraph (b)(4) of the
- * Rights in Noncommercial Technical Data and Computer Software--Small Business
- * Innovative Research (SBIR) Program clause contained in the above identified
- * contract. No restrictions apply after the expiration date shown above. Any
- * reproduction of technical data, computer software, or portions thereof
- * marked with this legend must also reproduce the markings.
- * End SBIR Data Rights Statement
- **/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.locationtech.geomesa.raster.ingest
 
