89c07fa6df04aa0a8c604b0f6d92b4621637a7d5
Jake Kenneally <jdk2pq@ccri.com>
2014-12-02 16:11:24 -0500

    GEOMESA-471 Modify existing WMS section of GeoServer to assemble chunked raster data, 473 Investigate Raster Assembly Methods for WCS
    Added GeoMesaCoverageFormat and Reader for WCS. Can return a single raster image from Accumulo in GeoServer and will be expanded to mosaicing multiple images into a single raster in a future commit(s).

---
+++ geomesa-plugin/src/main/scala/org/locationtech/geomesa/plugin/wcs/GeoMesaCoverageFormatFactory.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 Commonwealth Computer Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.locationtech.geomesa.plugin.wcs
+
+import java.awt.RenderingHints
+import java.util.Collections
+
+import org.geotools.coverage.grid.io.{AbstractGridFormat, GridFormatFactorySpi}
+
+class GeoMesaCoverageFormatFactory extends GridFormatFactorySpi {
+  def isAvailable = true
+
+  def createFormat(): AbstractGridFormat = new GeoMesaCoverageFormat
+
+  def getImplementationHints: java.util.Map[RenderingHints.Key, _] = Collections.emptyMap()
+
+}
---
+++ geomesa-plugin/src/test/scala/org/locationtech/geomesa/plugin/wcs/GeoMesaCoverageReaderTest.scala
@@ -0,0 +1,104 @@
+package org.locationtech.geomesa.plugin.wcs
+
+import java.awt.image._
+import java.io.{ByteArrayOutputStream, File}
+import javax.imageio.ImageIO
+
+import breeze.util.ArrayUtil
+import org.geotools.coverage.CoverageFactoryFinder
+import org.geotools.coverage.grid.GridCoverage2D
+import org.geotools.factory.Hints
+import org.geotools.geometry.jts.ReferencedEnvelope
+import org.geotools.referencing.crs.DefaultGeographicCRS
+import org.junit.runner.RunWith
+import org.locationtech.geomesa.plugin.ImageUtils
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+
+@RunWith(classOf[JUnitRunner])
+class GeoMesaCoverageReaderTest extends Specification {
+  val white = Array[Int] (255, 255, 255)
+  val black = Array[Int] (0, 0, 0)
+
+  def getNewImage(width: Int, height: Int, color: Array[Int]): BufferedImage = {
+    val image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY)
+    val wr = image.getRaster
+    var h = 0
+    var w = 0
+    for(h <- 1 until height) {
+      for (w <- 1 until width) {
+        wr.setPixel(w, h, color)
+      }
+    }
+    image
+  }
+
+  def getTestGridCoverages(width: Int, height: Int): Iterator[GridCoverage2D] = {
+    val whiteImage = getNewImage(width, height, white)
+    val blackImage = getNewImage(width, height, black)
+    val coverageFactory = CoverageFactoryFinder.getGridCoverageFactory(new Hints())
+    val envTR = new ReferencedEnvelope(0, width, 0, height, DefaultGeographicCRS.WGS84)
+    val envBR = new ReferencedEnvelope(0, width, height * -1, 0, DefaultGeographicCRS.WGS84)
+    val envTL = new ReferencedEnvelope(width * -1, 0, 0, height, DefaultGeographicCRS.WGS84)
+    val envBL = new ReferencedEnvelope(width * -1, 0, height * -1, 0, DefaultGeographicCRS.WGS84)
+    val coverageList = new ListBuffer[GridCoverage2D]()
+    coverageList += coverageFactory.create("image1", whiteImage, envTL)
+    coverageList += coverageFactory.create("image2", blackImage, envTR)
+    coverageList += coverageFactory.create("image3", blackImage, envBL)
+    coverageList += coverageFactory.create("image4", whiteImage, envBR)
+    coverageList.toIterator
+  }
+
+  def getImageFromGridCoverages(gridCoverageIterator: Iterator[GridCoverage2D], width: Int, height: Int): BufferedImage = {
+    val envTotal =  new ReferencedEnvelope(width * -1, width, height * -1, height, DefaultGeographicCRS.WGS84)
+    ImageUtils.mosaicGridCoverages(gridCoverageIterator, width * 2, height * 2, envTotal)
+  }
+
+  def getImageByteArray(imageName: String): Array[Byte] = {
+    val checkImage = ImageIO.read(new File(imageName))
+    val baos = new ByteArrayOutputStream()
+    ImageIO.write(checkImage, "png", baos)
+    baos.toByteArray
+  }
+
+  "GeoMesaCoverageReader" should {
+    "mosaic GridCoverages appropriately for 4 10x10 grid coverages" in {
+      val width = 10
+      val height = 10
+      val gridCoverageIterator = getTestGridCoverages(width, height)
+      val image = getImageFromGridCoverages(gridCoverageIterator, width, height)
+      val randString = Random.alphanumeric.take(5).mkString
+      ImageIO.write(image, "png", new File(s"test-$randString.png"))
+      val checkImage = getImageByteArray("src/test/resources/wcs/test-10-10.png")
+      val newImage = getImageByteArray(s"test-$randString.png")
+      ArrayUtil.equals(checkImage, newImage) must beTrue
+    }
+
+    "mosaic GridCoverages appropriately for 4 100x100 grid coverages" in {
+      val width = 100
+      val height = 100
+      val gridCoverageIterator = getTestGridCoverages(width, height)
+      val image = getImageFromGridCoverages(gridCoverageIterator, width, height)
+      val randString = Random.alphanumeric.take(5).mkString
+      ImageIO.write(image, "png", new File(s"test-$randString.png"))
+      val checkImage = getImageByteArray("src/test/resources/wcs/test-100-100.png")
+      val newImage = getImageByteArray(s"test-$randString.png")
+      ArrayUtil.equals(checkImage, newImage) must beTrue
+    }
+
+    "mosaic GridCoverages appropriately for 4 1000x1000 grid coverages" in {
+      val width = 1000
+      val height = 1000
+      val gridCoverageIterator = getTestGridCoverages(width, height)
+      val image = getImageFromGridCoverages(gridCoverageIterator, width, height)
+      val randString = Random.alphanumeric.take(5).mkString
+      ImageIO.write(image, "png", new File(s"test-$randString.png"))
+      val checkImage = getImageByteArray("src/test/resources/wcs/test-1000-1000.png")
+      val newImage = getImageByteArray(s"test-$randString.png")
+      ArrayUtil.equals(checkImage, newImage) must beTrue
+    }
+  }
+}
