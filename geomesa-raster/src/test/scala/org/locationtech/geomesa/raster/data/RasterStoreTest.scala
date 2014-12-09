7cd2a079162ce26e57c88165ace65ddc68ee6b73
Michael Ronquest <ronquest@ccri.com>
2014-12-09 15:01:07 -0500

    Fixed parts of RasterStoreTest.

--- geomesa-raster/src/test/scala/org/locationtech/geomesa/raster/data/RasterStoreTest.scala
+++ geomesa-raster/src/test/scala/org/locationtech/geomesa/raster/data/RasterStoreTest.scala
@@ -1,34 +1,97 @@
 package org.locationtech.geomesa.raster.data
 
-import java.io.File
+import java.awt._
+import java.awt.color.ColorSpace
+import java.awt.image._
+import java.io._
+import javax.imageio.ImageIO
+import javax.media.jai.remote.SerializableRenderedImage
 
-import org.geotools.coverage.grid.GridCoverage2D
+import com.vividsolutions.jts.geom.Geometry
+import org.geotools.coverage.CoverageFactoryFinder
+import org.geotools.coverage.grid.{GridCoverageFactory, GridCoverage2D}
+import org.geotools.factory.Hints
+import org.geotools.geometry.jts.ReferencedEnvelope
+import org.geotools.referencing.crs.DefaultGeographicCRS
+//import org.jaitools.imageutils.ImageUtils
 import org.joda.time.DateTime
 import org.junit.runner.RunWith
 import org.locationtech.geomesa.core.index.DecodedIndex
+//import org.locationtech.geomesa.plugin.ImageUtils
 import org.locationtech.geomesa.raster.feature.Raster
 import org.locationtech.geomesa.raster.ingest.SimpleRasterIngest._
-import org.locationtech.geomesa.utils.geohash.BoundingBox
+import org.locationtech.geomesa.utils.geohash.{GeoHash, TwoGeoHashBoundingBox, BoundingBox}
+import org.opengis.coverage.grid.GridCoverage
+import org.opengis.geometry.Envelope
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
+import scala.collection.mutable.ListBuffer
+
+
+
 @RunWith(classOf[JUnitRunner])
 class RasterStoreTest extends Specification {
-  val rs = RasterStore("user", "pass", getClass.toString, "zk", getClass.toString, "S,USA", "S,USA", true)
 
-  // Next three lines are the inputs
-  val rasterName = "testRaster"
-  val file = new File("/opt/devel/wcs/data/nlcd_DC_source_r_512.tif")
-  val ingestTime = new DateTime()
+  def createAndFillRasterStore = {
 
-  val rasterReader = getTiffReader(file)
-  val rasterGrid: GridCoverage2D = rasterReader.read(null)
+    //val rs = RasterStore("user", "pass", getClass.toString, "zk", getClass.toString, "S,USA", "S,USA", true)
+    val rs = RasterStore("user", "pass", getClass.toString, "zk", getClass.toString, "SUSA", "SUSA", true)
 
-  val envelope = rasterGrid.getEnvelope2D
-  val bbox = BoundingBox(envelope.getMinX, envelope.getMaxX, envelope.getMinY, envelope.getMaxY)
-  val rasterMetadata = rasterMetadataFromFile(rasterName, file, "TIFF", ingestTime)
-  val metadata = DecodedIndex(Raster.getRasterId(rasterName), bbox.geom, Option(ingestTime.getMillis))
-  val raster = Raster(rasterGrid.getRenderedImage, metadata)
+    val rasterName = "testRaster"
 
-  rs.putRaster(raster)
+    val ingestTime = new DateTime()
+
+    val coverageFactory = CoverageFactoryFinder.getGridCoverageFactory(new Hints())
+
+    val env = new ReferencedEnvelope(0, 50, 0, 50, DefaultGeographicCRS.WGS84)
+
+    val bbox = BoundingBox(env)
+
+    val metadata = DecodedIndex(Raster.getRasterId(rasterName), bbox.geom, Option(ingestTime.getMillis))
+
+    val image = getNewImage(500, 500, Array[Int](255, 255, 255))
+
+    val coverage = imageToCoverage(500, 500, image.getRaster(), env, coverageFactory)
+
+    val raster = new Raster(coverage.getRenderedImage, metadata)
+
+    rs.ensureTableExists()
+    rs.putRaster(raster)
+    rs
+  }
+  def generateQuery = {
+    ???
+  }
+
+  // stolen from elsewhere
+  def getNewImage(width: Int, height: Int, color: Array[Int]): BufferedImage = {
+    val image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY)
+    val wr = image.getRaster
+    var h = 0
+    var w = 0
+    for (h <- 1 until height) {
+      for (w <- 1 until width) {
+        wr.setPixel(w, h, color)
+      }
+    }
+    image
+  }
+
+  def imageToCoverage(width: Int, height: Int, img: WritableRaster, env: ReferencedEnvelope, cf: GridCoverageFactory) = {
+    cf.create("testRaster", img, env)
+  }
+
+  "RasterStore" should {
+    "create a Raster Store" in {
+      val theStore = createAndFillRasterStore
+      theStore must beAnInstanceOf[RasterStore]
+      //val theIterator = theStore.getRasters()
+    }
+  }
+
+
+
+
+
 }
