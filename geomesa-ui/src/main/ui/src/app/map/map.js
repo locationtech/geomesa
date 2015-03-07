bc950fa672299e384b55fcc069353ec64ab288b3
Kevin Wheeler <kevinwheeler90@gmail.com>
2015-03-07 10:19:46 -0600

    Implemented some pull diff suggestions.
    Also-by: Phillip Pham <pqpham90@gmail.com>
    Signed-off-by: Kevin Wheeler <kevinwheeler90@gmail.com>

--- geomesa-ui/src/main/ui/src/app/map/map.js
+++ geomesa-ui/src/main/ui/src/app/map/map.js
@@ -7,18 +7,20 @@ angular.module('geomesa.map', [])
             scope: {
                 map: '=?',
                 api: '=',
-                cards: '=',
-                selectedPoint: '='
+                selectedFeatures: '=',
             },
 
             link: function (scope, element, attrs) {
-                scope.cards = [
+                scope.selectedFeatures = [
                     {
-                        Start: ' Click a data point to view it\'s attributes.'
+                        Start: ' Click a data point to view its attributes.'
                     }
                 ];
 
-                var baseLayer = new ol.layer.Tile({
+                var projection = ol.proj.get('EPSG:3857'),
+                    extent = projection.getExtent(),
+                    baseLayer = new ol.layer.Tile({
+                        extent: extent,
                         source: new ol.source.MapQuest({layer: 'osm'})
                     }),
                     wmsSource = new ol.source.TileWMS({
@@ -26,10 +28,12 @@ angular.module('geomesa.map', [])
                         params: {LAYERS: 'QuickStart'}
                     }),
                         wmsLayer = new ol.layer.Tile({
+                        extent: extent,
                         source: wmsSource
                     }),
                     olView = new ol.View({
-                        center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+                        // center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+                        center:[-8554902.86746,-4592147.60759],
                         zoom : 4,
                         maxResolution : 40075016.68557849 / screen.width,
                     });
@@ -47,10 +51,10 @@ angular.module('geomesa.map', [])
                 };
 
                 scope.map.on('singleclick', function(evt) {
-                    var viewResolution = olView.getResolution();
-                    var url = wmsSource.getGetFeatureInfoUrl(
-                        evt.coordinate, viewResolution, 'EPSG:3857',
-                        {'INFO_FORMAT': 'application/json', FEATURE_COUNT: 50}
+                    var viewResolution = olView.getResolution(),
+                        url = wmsSource.getGetFeatureInfoUrl(
+                            evt.coordinate, viewResolution, 'EPSG:3857',
+                            {'INFO_FORMAT': 'application/json', FEATURE_COUNT: 50}
                     );
                     $http.get(url).success(function(data, status, headers, config) {
                         if (data.features.length){
