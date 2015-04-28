01711d54e040b71a31eb8080cd8f20ca5e00c6f8
Jake Kurzer <jake.kurzer@ccri.com>
2015-04-28 15:48:16 -0400

    Forward-port proper spark partitioning behavior
    This adds an rddPartPerExec function that allows the specification
    the number of parts per executor. This relies on some fairly rickety
    functionality, so I've taken the liberty of marking it pre-emptively
    with the deprecated decorator. This capability should eventually
    be firmed up and added to the core "rdd" function to better
    map to the normative pattern for spark APIs. And, you know,
    move away from hilariously tightly dependent code.

--- geomesa-compute/src/main/scala/org/locationtech/geomesa/compute/spark/GeoMesaSpark.scala
+++ geomesa-compute/src/main/scala/org/locationtech/geomesa/compute/spark/GeoMesaSpark.scala
@@ -22,25 +22,61 @@ import java.util.UUID
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.{Input, Output}
 import com.google.common.cache.{CacheBuilder, CacheLoader}
-import org.apache.accumulo.core.client.mapreduce.AccumuloInputFormat
+import org.apache.accumulo.core.client.mapreduce.{RangeInputSplit, AccumuloInputFormat}
 import org.apache.accumulo.core.client.mapreduce.lib.util.{ConfiguratorBase, InputConfigurator}
 import org.apache.accumulo.core.data.{Key, Value}
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapreduce.{InputSplit, JobContext, Job}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.KryoRegistrator
 import org.apache.spark.{SparkConf, SparkContext}
 import org.geotools.data.{DataStore, DataStoreFinder, DefaultTransaction, Query}
 import org.geotools.factory.CommonFactoryFinder
+import org.geotools.filter.text.ecql.ECQL
 import org.locationtech.geomesa.core.data._
-import org.locationtech.geomesa.core.index.{ExplainPrintln, STIdxStrategy, _}
+import org.locationtech.geomesa.core.index._
 import org.locationtech.geomesa.feature._
 import org.locationtech.geomesa.feature.kryo.{KryoFeatureSerializer, SimpleFeatureSerializer}
+import org.locationtech.geomesa.jobs.GeoMesaConfigurator
+import org.locationtech.geomesa.jobs.mapreduce.{GroupedSplit, GeoMesaInputFormat}
 import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
 import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
 
 import scala.collection.JavaConversions._
 
 object GeoMesaSpark {
+
+  class GeoSparkInputFormat extends GeoMesaInputFormat
+  {
+    private def init(conf: Configuration): Unit = if (sft == null) {
+      val params = GeoMesaConfigurator.getDataStoreInParams(conf)
+      val ds = DataStoreFinder.getDataStore(params).asInstanceOf[AccumuloDataStore]
+      sft = ds.getSchema(GeoMesaConfigurator.getFeatureType(conf))
+      encoding = ds.getFeatureEncoding(sft)
+      numShards = IndexSchema.maxShard(ds.getIndexSchemaFmt(sft.getTypeName))
+     }
+
+    override def getSplits(context: JobContext) : java.util.List[InputSplit] = {
+      init(context.getConfiguration)
+      val accumuloSplits = delegate.getSplits(context)
+      // try to create 2 mappers per node - account for case where there are less splits than shards
+      val desiredSplits = context.getConfiguration.getInt("SplitsDesired", numShards)
+      val groupSize = Math.max(desiredSplits * 2, accumuloSplits.length / (desiredSplits * 2))
+
+      // We know each range will only have a single location because of autoAdjustRanges
+      val splits = accumuloSplits.groupBy(_.getLocations()(0)).flatMap { case (location, splits) =>
+        splits.grouped(groupSize).map { group =>
+          val split = new GroupedSplit()
+          split.location = location
+          split.splits.append(group.map(_.asInstanceOf[RangeInputSplit]): _*)
+          split
+        }
+      }
+      splits.toList
+    }
+
+  }
 
   def init(conf: SparkConf, ds: DataStore): SparkConf = {
     val typeOptions = ds.getTypeNames.map { t => (t, SimpleFeatureTypes.encodeType(ds.getSchema(t))) }
@@ -54,6 +90,20 @@ object GeoMesaSpark {
 
   def typeProp(typeName: String) = s"geomesa.types.$typeName"
   def jOpt(typeName: String, spec: String) = s"-D${typeProp(typeName)}=$spec"
+
+  def rddPartPerExec(conf: Configuration, sc: SparkContext, dsParams: Map[String, String], query: Query, partsPerExec: Int): RDD[SimpleFeature] = {
+    val filter = ECQL.toCQL(query.getFilter)
+    conf.setInt("SplitsDesired", sc.getExecutorStorageStatus.length * partsPerExec)
+    val job = Job.getInstance(conf, "GeoMesa Spark")
+    GeoMesaInputFormat.configure(job, dsParams, query, Some(filter)) // this appears to be mutator for job
+    val rdd = sc.newAPIHadoopRDD(job.getConfiguration(), classOf[GeoSparkInputFormat], classOf[Text], classOf[SimpleFeature])
+
+/*    rdd.mapPartitions { iter =>
+      val sft = SimpleFeatureTypes.createType(typeName, spec)
+      val decoder = SimpleFeatureDecoder(sft, featureEncoding)
+      iter.map { case (k: Key, v: Value) => decoder.decode(v.get()) }
+    }*/
+  }
 
   def rdd(conf: Configuration, sc: SparkContext, ds: AccumuloDataStore, query: Query, useMock: Boolean = false): RDD[SimpleFeature] = {
     val typeName = query.getTypeName
@@ -75,6 +125,8 @@ object GeoMesaSpark {
     InputConfigurator.setRanges(classOf[AccumuloInputFormat], conf, qp.ranges)
     qp.iterators.foreach { is => InputConfigurator.addIterator(classOf[AccumuloInputFormat], conf, is) }
 
+    val job = Job.getInstance(conf, "GeoMesa Spark") // this line, I don't totally grok.
+    GeoMesaInputFormat.configure(job, )
     val rdd = sc.newAPIHadoopRDD(conf, classOf[AccumuloInputFormat], classOf[Key], classOf[Value])
 
     rdd.mapPartitions { iter =>
