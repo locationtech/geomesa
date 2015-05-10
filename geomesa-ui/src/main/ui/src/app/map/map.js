6dff50635370eab3865c60d1ae9d50178a1cf30a
Thien Vo <vohuythien@utexas.edu>
2015-05-10 02:01:25 -0500

    Resolved another merge conflict and added style for ui for heatmap
    Signed-off-by: Thien Vo <vohuythien@utexas.edu>

--- geomesa-ui/src/main/ui/src/app/map/map.js
+++ geomesa-ui/src/main/ui/src/app/map/map.js
@@ -17,10 +17,15 @@ angular.module('geomesa.map', [])
                         format: 'image/png',
                         transparent: true
                     });
-                var baseLayer = new ol.layer.Tile({
-                   source: new ol.source.MapQuest({layer: 'osm'})
-                });
 
+                scope.map = L.map(element[0], {
+                    center: L.latLng(-38.09, -76.85),
+                    zoom: 8,
+                    maxZoom: 18,
+                    minZoom: 3,
+                    attributionControl: false,
+                    layers: [baseLayer, wmsLayer]
+                });
             },
             link: function (scope, element, attrs) {
                 scope.selectedFeatures = [
