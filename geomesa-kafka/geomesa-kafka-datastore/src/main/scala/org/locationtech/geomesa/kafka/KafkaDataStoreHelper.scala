1c69bcaa25d35ea9be8fde3c728e0c1639e2c472
Matt Eldridge <matt.eldridge@ccri.com>
2015-05-13 08:51:43 -0400

    Adding body of KafkaDataStoreHelper.scala
    Signed-off-by: Matt Eldridge <matt.eldridge@ccri.com>

--- geomesa-kafka/geomesa-kafka-datastore/src/main/scala/org/locationtech/geomesa/kafka/KafkaDataStoreHelper.scala
+++ geomesa-kafka/geomesa-kafka-datastore/src/main/scala/org/locationtech/geomesa/kafka/KafkaDataStoreHelper.scala
@@ -1,8 +1,52 @@
 package org.locationtech.geomesa.kafka
 
-/**
- * Created by meldridge on 5/12/15.
- */
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder
+import org.opengis.feature.simple.SimpleFeatureType
+
 class KafkaDataStoreHelper {
 
+  private val TopicKey = "Topic"
+  private val ReplayConfigKey = "ReplayConfig"
+
+  def prepareForLive(sft: SimpleFeatureType, zkPath: String) : SimpleFeatureType = {
+
+    val topic = buildTopicName(zkPath, sft)
+    val sftCopy = copySft(sft)
+    sftCopy.getUserData.put(TopicKey,topic)
+
+    sftCopy
+  }
+
+  def prepareForReplay(sft: SimpleFeatureType, rConfig: ReplayConfig) : SimpleFeatureType = {
+    val rcString = ReplayConfig.encode(rConfig);
+    val sftCopy = copySft(sft);
+    sftCopy.getUserData.put(ReplayConfigKey,rcString);
+
+    sftCopy
+  }
+
+  def extractTopic(sft: SimpleFeatureType) : Option[String] =
+    Option(sft.getUserData.get(TopicKey)).map(_.asInstanceOf[String])
+
+
+  def extractReplayConfig(sft: SimpleFeatureType) : Option[ReplayConfig] = {
+    // this one is pretty cool, but not very readable :)
+    Option(sft.getUserData.get(ReplayConfigKey)).map(_.asInstanceOf[String]).map(ReplayConfig.decode(_).get)
+  }
+
+  private def buildTopicName(zkPath: String, sft: SimpleFeatureType): String = {
+    return sft.getTypeName + "_" + zkPath;
+  }
+
+  private def copySft(sft: SimpleFeatureType): SimpleFeatureType = {
+
+    val builder = new SimpleFeatureTypeBuilder()
+    builder.init(sft)
+    val preppedSft: SimpleFeatureType = builder.buildFeatureType()
+
+    // builder doesn't copy user data...
+    preppedSft.getUserData.putAll(sft.getUserData)
+
+    preppedSft
+  }
 }
