6b0436bb701146286b640564cda85f876966eb8e
John Armstrong <john.armstrong@ccri.com>
2015-05-20 15:29:03 -0400

    addressing some comments

--- geomesa-plugin/src/main/scala/org/locationtech/geomesa/plugin/security/UserNameRoles.scala
+++ geomesa-plugin/src/main/scala/org/locationtech/geomesa/plugin/security/UserNameRoles.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Commonwealth Computer Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.locationtech.geomesa.plugin.security
 
 import org.geoserver.security.GeoServerRoleService
@@ -39,11 +55,9 @@ trait UserNameRoles extends GeoServerRoleService {
   // RoleServices don't write back to their backing configuration
   // we need everything to happen as a layer on top of the configured answers
   abstract override def getRolesForUser(username: String): jSortedSet[GeoServerRole] = {
-    import collection.JavaConversions._
     val roles = new jTreeSet[GeoServerRole]()
     roles.addAll(super.getRolesForUser(username))
     roles.add(storedUserRoles(username))
-    println(s"Retrieved roles for $username: ${roles.map(_.toString).mkString(",")}")
     Collections.unmodifiableSortedSet(roles)
   }
 
