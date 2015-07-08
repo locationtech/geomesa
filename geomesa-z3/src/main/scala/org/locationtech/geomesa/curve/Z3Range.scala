5789033c3789aca00c3fd4967eaa88b1a747e203
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2015-07-08 16:11:28 -0400

    GEOMESA-819 Including size of query box in z3 range calculation
    * Start recursion at the longest common prefix of the min/max bounds
    * Depth of recursion depends on the size of our query region
    * Compared to fixed recursion depth, increases ranges for small query regions and decreases ranges for large query regions
    * Most queries will create less than 10k ranges - usually on the order of 2-4k for moderate queries and 30-500 for small queries
    
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-z3/src/main/scala/org/locationtech/geomesa/curve/Z3Range.scala
+++ geomesa-z3/src/main/scala/org/locationtech/geomesa/curve/Z3Range.scala
@@ -19,12 +19,25 @@ case class Z3Range(min: Z3, max: Z3) {
 
   def length: Int = (max.z - min.z + 1).toInt
 
+  // contains in index space (e.g. the long value)
   def contains(bits: Z3): Boolean = bits.z >= min.z && bits.z <= max.z
 
+  // contains in index space (e.g. the long value)
   def contains(r: Z3Range): Boolean = contains(r.min) && contains(r.max)
 
+  // overlap in index space (e.g. the long value)
   def overlaps(r: Z3Range): Boolean = contains(r.min) || contains(r.max)
 
+  // contains in user space - each dimension is contained
+  def containsInUserSpace(bits: Z3) = {
+    val (x, y, z) = bits.decode
+    x >= min.d0 && x <= max.d0 && y >= min.d1 && y <= max.d1 && z >= min.d2 && z <= max.d2
+  }
+
+  // contains in user space - each dimension is contained
+  def containsInUserSpace(r: Z3Range): Boolean = containsInUserSpace(r.min) && containsInUserSpace(r.max)
+
+  // overlap in user space - if any dimension overlaps
   def overlapsInUserSpace(r: Z3Range): Boolean =
     overlaps(min.d0, max.d0, r.min.d0, r.max.d0) &&
         overlaps(min.d1, max.d1, r.min.d1, r.max.d1) &&
@@ -32,15 +45,4 @@ case class Z3Range(min: Z3, max: Z3) {
 
   private def overlaps(a1: Int, a2: Int, b1: Int, b2: Int) = math.max(a1, b1) <= math.min(a2, b2)
 
-  def containsInUserSpace(bits: Z3) = {
-    val (x, y, z) = bits.decode
-    x >= min.d0 &&
-        x <= max.d0 &&
-        y >= min.d1 &&
-        y <= max.d1 &&
-        z >= min.d2 &&
-        z <= max.d2
-  }
-
-  def containsInUserSpace(r: Z3Range): Boolean = containsInUserSpace(r.min) && containsInUserSpace(r.max)
 }
