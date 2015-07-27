1af5857ba09c2f8732c1059b4d96450346952b97
Andrew Hulbert <andrew.hulbert@ccri.com>
2015-07-27 11:25:30 -0400

    GEOMESA-870 CSV deadlock fix
    Fixes issues where delimited text convert module deadlocks when processing trailing newlines are found in CSV files (which happens often)
    
    Signed-off-by: Andrew Hulbert <andrew.hulbert@ccri.com>

---
+++ geomesa-convert/geomesa-convert-text/src/test/scala/org/locationtech/geomesa/convert/text/NewLinesTest.scala
@@ -0,0 +1,73 @@
+/***********************************************************************
+* Copyright (c) 2013-2015 Commonwealth Computer Research, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0 which
+* accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*************************************************************************/
+package org.locationtech.geomesa.convert.text
+
+import com.typesafe.config.ConfigFactory
+import org.junit.runner.RunWith
+import org.locationtech.geomesa.convert.SimpleFeatureConverters
+import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class NewLinesTest extends Specification {
+
+  sequential
+
+  "NewLinesTest" should {
+
+    val conf = ConfigFactory.parseString(
+      """
+        | converter = {
+        |   type         = "delimited-text",
+        |   format       = "DEFAULT",
+        |   id-field     = "uuid()",
+        |   fields = [
+        |     { name = "lat",      transform = "$1::double" },
+        |     { name = "lon",      transform = "$2::double" },
+        |     { name = "geom",     transform = "point($lat, $lon)" }
+        |   ]
+        | }
+      """.stripMargin)
+
+    val sft = SimpleFeatureTypes.createType(ConfigFactory.parseString(
+      """
+        |{
+        |  type-name = "newlinetest"
+        |  attributes = [
+        |    { name = "lat",      type = "Double", index = false },
+        |    { name = "lon",      type = "Double", index = false },
+        |    { name = "geom",     type = "Point",  index = true, srid = 4326, default = true }
+        |  ]
+        |}
+      """.stripMargin
+    ))
+
+    "process trailing newline" >> {
+      val converter = SimpleFeatureConverters.build[String](sft, conf)
+      val data = "45.0,45.0\n55.0,55.0\n".split("\n", -1)
+      data.length mustEqual 3
+
+      val res = converter.processInput(data.toIterator)
+      res.length mustEqual 2
+      converter.close()
+      success
+    }
+
+    "process middle of data newline" >> {
+      val converter = SimpleFeatureConverters.build[String](sft, conf)
+      val data = "45.0,45.0\n\n55.0,55.0\n".split("\n", -1)
+      data.length mustEqual 4
+
+      val res = converter.processInput(data.toIterator)
+      res.length mustEqual 2
+      converter.close()
+      success
+    }
+  }
+}
