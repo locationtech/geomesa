90ab5f25bdc3717585741fd3333ce421f539f4be
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2015-08-28 11:10:11 -0400

    adding xsd validation, xml to string method for ids
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-convert/geomesa-convert-xml/src/main/scala/org/locationtech/geomesa/convert/xml/XMLConverter.scala
+++ geomesa-convert/geomesa-convert-xml/src/main/scala/org/locationtech/geomesa/convert/xml/XMLConverter.scala
@@ -1,21 +1,25 @@
 package org.locationtech.geomesa.convert.xml
 
-import java.io.ByteArrayInputStream
+import java.io.StringReader
+import javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI
 import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
 import javax.xml.xpath.{XPathConstants, XPathExpression, XPathFactory}
 
-import com.google.common.base.Charsets
 import com.typesafe.config.Config
 import org.locationtech.geomesa.convert.Transformers.{EvaluationContext, Expr}
 import org.locationtech.geomesa.convert._
 import org.opengis.feature.simple.SimpleFeatureType
 import org.w3c.dom.NodeList
+import org.xml.sax.InputSource
 
 import scala.collection.JavaConversions._
 
 class XMLConverter(val targetSFT: SimpleFeatureType,
                    val idBuilder: Expr,
                    val featurePath: Option[XPathExpression],
+                   val xsd: Option[String],
                    val inputFields: IndexedSeq[Field]) extends ToSimpleFeatureConverter[String] {
 
   private val docBuilder = {
@@ -23,10 +27,19 @@ class XMLConverter(val targetSFT: SimpleFeatureType,
     factory.setNamespaceAware(false)
     factory.newDocumentBuilder()
   }
+  private val validator = xsd.map { path =>
+    val schemaFactory = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI)
+    val xsdStream = getClass.getClassLoader.getResourceAsStream(path)
+    val schema = schemaFactory.newSchema(new StreamSource(xsdStream))
+    xsdStream.close()
+    schema.newValidator()
+  }
 
   override def fromInputType(i: String): Seq[Array[Any]] = {
+    // if a schema is defined, validate it - this will throw an exception on failure
+    validator.foreach(_.validate(new StreamSource(new StringReader(i))))
     // parse the document once, then extract each feature node and operate on it
-    val root = docBuilder.parse(new ByteArrayInputStream(i.getBytes(Charsets.UTF_8))).getDocumentElement
+    val root = docBuilder.parse(new InputSource(new StringReader(i))).getDocumentElement
     featurePath.map { path =>
       val nodeList = path.evaluate(root, XPathConstants.NODESET).asInstanceOf[NodeList]
       (0 until nodeList.getLength).map(i => Array[Any](nodeList.item(i)))
@@ -46,7 +59,8 @@ class XMLConverterFactory extends SimpleFeatureConverterFactory[String] {
     // feature path can be any xpath that resolves to a node set (or a single node)
     // it can be absolute, or relative to the root node
     val featurePath  = if (conf.hasPath("feature-path")) Some(conf.getString("feature-path")) else None
-    new XMLConverter(sft, idBuilder, featurePath.map(xpath.compile), fields)
+    val xsd = if (conf.hasPath("xsd")) Some(conf.getString("xsd")) else None
+    new XMLConverter(sft, idBuilder, featurePath.map(xpath.compile), xsd, fields)
   }
 
   override def buildFields(fields: Seq[Config]): IndexedSeq[Field] = {
--- geomesa-convert/geomesa-convert-xml/src/test/scala/org/locationtech/geomesa/convert/xml/XMLConverterTest.scala
+++ geomesa-convert/geomesa-convert-xml/src/test/scala/org/locationtech/geomesa/convert/xml/XMLConverterTest.scala
@@ -16,7 +16,7 @@ class XMLConverterTest extends Specification {
       |  attributes = [
       |    {name = "number", type = "Integer"}
       |    {name = "color",  type = "String"}
-      |    {name = "weight", type = "String"}
+      |    {name = "weight", type = "Double"}
       |    {name = "source", type = "String"}
       |  ]
       |}
@@ -41,7 +41,7 @@ class XMLConverterTest extends Specification {
           |  <Feature>
           |    <number>456</number>
           |    <color>blue</color>
-              <physical weight="w2" height="h2"/>
+              <physical weight="150" height="h2"/>
           |  </Feature>
           |</doc>
         """.stripMargin
@@ -56,7 +56,7 @@ class XMLConverterTest extends Specification {
           |     // paths can be any xpath - relative to the feature-path, or absolute
           |     { name = "number", path = "number",           transform = "$0::integer" }
           |     { name = "color",  path = "color",            transform = "trim($0)" }
-          |     { name = "weight", path = "physical/@weight", transform = "trim($0)" }
+          |     { name = "weight", path = "physical/@weight", transform = "$0::double" }
           |     { name = "source", path = "/doc/DataSource/name/text()" }
           |   ]
           | }
@@ -67,11 +67,11 @@ class XMLConverterTest extends Specification {
       features must haveLength(2)
       features.head.getAttribute("number").asInstanceOf[Integer] mustEqual 123
       features.head.getAttribute("color").asInstanceOf[String] mustEqual "red"
-      features.head.getAttribute("weight").asInstanceOf[String] mustEqual "127.5"
+      features.head.getAttribute("weight").asInstanceOf[Double] mustEqual 127.5
       features.head.getAttribute("source").asInstanceOf[String] mustEqual "myxml"
       features(1).getAttribute("number").asInstanceOf[Integer] mustEqual 456
       features(1).getAttribute("color").asInstanceOf[String] mustEqual "blue"
-      features(1).getAttribute("weight").asInstanceOf[String] mustEqual "w2"
+      features(1).getAttribute("weight").asInstanceOf[Double] mustEqual 150
       features(1).getAttribute("source").asInstanceOf[String] mustEqual "myxml"
     }
 
@@ -92,7 +92,7 @@ class XMLConverterTest extends Specification {
           |    <Feature>
           |      <number>456</number>
           |      <color>blue</color>
-          |      <physical weight="w2" height="h2"/>
+          |      <physical weight="150" height="h2"/>
           |    </Feature>
           |  </IgnoreMe>
           |</doc>
@@ -108,7 +108,7 @@ class XMLConverterTest extends Specification {
           |     // paths can be any xpath - relative to the feature-path, or absolute
           |     { name = "number", path = "number",           transform = "$0::integer" }
           |     { name = "color",  path = "color",            transform = "trim($0)" }
-          |     { name = "weight", path = "physical/@weight", transform = "trim($0)" }
+          |     { name = "weight", path = "physical/@weight", transform = "$0::double" }
           |     { name = "source", path = "/doc/DataSource/name/text()" }
           |   ]
           | }
@@ -119,11 +119,11 @@ class XMLConverterTest extends Specification {
       features must haveLength(2)
       features.head.getAttribute("number").asInstanceOf[Integer] mustEqual 123
       features.head.getAttribute("color").asInstanceOf[String] mustEqual "red"
-      features.head.getAttribute("weight").asInstanceOf[String] mustEqual "127.5"
+      features.head.getAttribute("weight").asInstanceOf[Double] mustEqual 127.5
       features.head.getAttribute("source").asInstanceOf[String] mustEqual "myxml"
       features(1).getAttribute("number").asInstanceOf[Integer] mustEqual 456
       features(1).getAttribute("color").asInstanceOf[String] mustEqual "blue"
-      features(1).getAttribute("weight").asInstanceOf[String] mustEqual "w2"
+      features(1).getAttribute("weight").asInstanceOf[Double] mustEqual 150
       features(1).getAttribute("source").asInstanceOf[String] mustEqual "myxml"
     }
 
@@ -149,9 +149,9 @@ class XMLConverterTest extends Specification {
           |   feature-path = "Feature" // can be any xpath - relative to the root, or absolute
           |   fields = [
           |     // paths can be any xpath - relative to the feature-path, or absolute
-          |     { name = "number", path = "number",                   transform = "$0::integer" }
-          |     { name = "color",  path = "color",                    transform = "trim($0)" }
-          |     { name = "weight", path = "floor(physical/@weight)", transform = "trim($0)" }
+          |     { name = "number", path = "number",                  transform = "$0::integer" }
+          |     { name = "color",  path = "color",                   transform = "trim($0)" }
+          |     { name = "weight", path = "floor(physical/@weight)", transform = "$0::double" }
           |     { name = "source", path = "/doc/DataSource/name/text()" }
           |   ]
           | }
@@ -162,8 +162,134 @@ class XMLConverterTest extends Specification {
       features must haveLength(1)
       features.head.getAttribute("number").asInstanceOf[Integer] mustEqual 123
       features.head.getAttribute("color").asInstanceOf[String] mustEqual "red"
-      features.head.getAttribute("weight").asInstanceOf[String] mustEqual "127"
+      features.head.getAttribute("weight").asInstanceOf[Double] mustEqual 127
       features.head.getAttribute("source").asInstanceOf[String] mustEqual "myxml"
+    }
+
+    "use an ID hash for each node" >> {
+      val xml =
+        """<doc>
+          |  <DataSource>
+          |    <name>myxml</name>
+          |  </DataSource>
+          |  <Feature>
+          |    <number>123</number>
+          |    <color>red</color>
+          |    <physical weight="127.5" height="5'11"/>
+          |  </Feature>
+          |  <Feature>
+          |    <number>456</number>
+          |    <color>blue</color>
+                <physical weight="150" height="h2"/>
+          |  </Feature>
+          |</doc>
+        """.stripMargin
+
+      val parserConf = ConfigFactory.parseString(
+        """
+          | converter = {
+          |   type         = "xml"
+          |   id-field     = "md5(string2bytes(xml2string($0)))"
+          |   feature-path = "Feature" // can be any xpath - relative to the root, or absolute
+          |   fields = [
+          |     // paths can be any xpath - relative to the feature-path, or absolute
+          |     { name = "number", path = "number",           transform = "$0::integer" }
+          |     { name = "color",  path = "color",            transform = "trim($0)" }
+          |     { name = "weight", path = "physical/@weight", transform = "$0::double" }
+          |     { name = "source", path = "/doc/DataSource/name/text()" }
+          |   ]
+          | }
+        """.stripMargin)
+
+      val converter = SimpleFeatureConverters.build[String](sft, parserConf)
+      val features = converter.processInput(Iterator(xml)).toList
+      features must haveLength(2)
+      features.head.getAttribute("number").asInstanceOf[Integer] mustEqual 123
+      features.head.getAttribute("color").asInstanceOf[String] mustEqual "red"
+      features.head.getAttribute("weight").asInstanceOf[Double] mustEqual 127.5
+      features.head.getAttribute("source").asInstanceOf[String] mustEqual "myxml"
+      features(1).getAttribute("number").asInstanceOf[Integer] mustEqual 456
+      features(1).getAttribute("color").asInstanceOf[String] mustEqual "blue"
+      features(1).getAttribute("weight").asInstanceOf[Double] mustEqual 150
+      features(1).getAttribute("source").asInstanceOf[String] mustEqual "myxml"
+      features.head.getID mustEqual "441dd9114a1a345fe59f0dfe461f01ca"
+      features(1).getID mustEqual "42aae6286c7204c3aa1aa99a4e8dae35"
+    }
+
+    "validate with an xsd" >> {
+      val xml =
+        """<f:doc xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:f="http://geomesa.org/test-feature">
+          |  <f:DataSource>
+          |    <f:name>myxml</f:name>
+          |  </f:DataSource>
+          |  <f:Feature>
+          |    <f:number>123</f:number>
+          |    <f:color>red</f:color>
+          |    <f:physical weight="127.5" height="5'11"/>
+          |  </f:Feature>
+          |</f:doc>
+        """.stripMargin
+
+      val parserConf = ConfigFactory.parseString(
+        """
+          | converter = {
+          |   type         = "xml"
+          |   id-field     = "uuid()"
+          |   feature-path = "Feature" // can be any xpath - relative to the root, or absolute
+          |   xsd          = "xml-feature.xsd" // looked up by class.getResource
+          |   fields = [
+          |     // paths can be any xpath - relative to the feature-path, or absolute
+          |     { name = "number", path = "number",           transform = "$0::integer" }
+          |     { name = "color",  path = "color",            transform = "trim($0)" }
+          |     { name = "weight", path = "physical/@weight", transform = "$0::double" }
+          |     { name = "source", path = "/doc/DataSource/name/text()" }
+          |   ]
+          | }
+        """.stripMargin)
+
+      val converter = SimpleFeatureConverters.build[String](sft, parserConf)
+      val features = converter.processInput(Iterator(xml)).toList
+      features must haveLength(1)
+      features.head.getAttribute("number").asInstanceOf[Integer] mustEqual 123
+      features.head.getAttribute("color").asInstanceOf[String] mustEqual "red"
+      features.head.getAttribute("weight").asInstanceOf[Double] mustEqual 127.5
+      features.head.getAttribute("source").asInstanceOf[String] mustEqual "myxml"
+    }
+
+    "invalidate with an xsd" >> {
+      val xml =
+        """<f:doc2 xmlns:f="http://geomesa.org/test-feature" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+          |  <f:DataSource>
+          |    <f:name>myxml</f:name>
+          |  </f:DataSource>
+          |  <f:Feature>
+          |    <f:number>123</f:number>
+          |    <f:color>red</f:color>
+          |    <f:physical weight="127.5" height="5'11"/>
+          |  </f:Feature>
+          |</f:doc2>
+        """.stripMargin
+
+      val parserConf = ConfigFactory.parseString(
+        """
+          | converter = {
+          |   type         = "xml"
+          |   id-field     = "uuid()"
+          |   feature-path = "Feature" // can be any xpath - relative to the root, or absolute
+          |   xsd          = "xml-feature.xsd" // looked up by class.getResource
+          |   fields = [
+          |     // paths can be any xpath - relative to the feature-path, or absolute
+          |     { name = "number", path = "number",           transform = "$0::integer" }
+          |     { name = "color",  path = "color",            transform = "trim($0)" }
+          |     { name = "weight", path = "physical/@weight", transform = "$0::double" }
+          |     { name = "source", path = "/doc/DataSource/name/text()" }
+          |   ]
+          | }
+        """.stripMargin)
+
+      val converter = SimpleFeatureConverters.build[String](sft, parserConf)
+      val features = converter.processInput(Iterator(xml)).toList
+      features must haveLength(0)
     }
   }
 }
