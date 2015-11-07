5dcd26702cae249205662908249bcad715c6a07b
Jim Hughes <jnh5y@ccri.com>
2015-11-07 16:52:38 -0500

    Fiddling with Scalatra returns.

--- geomesa-blobstore/geomesa-blobstore-api/src/main/scala/org/locationtech/geomesa/blob/api/BlobstoreServlet.scala
+++ geomesa-blobstore/geomesa-blobstore-api/src/main/scala/org/locationtech/geomesa/blob/api/BlobstoreServlet.scala
@@ -1,9 +1,12 @@
 package org.locationtech.geomesa.blob.api
 
+import java.io.{File, FileOutputStream}
+
+import com.google.common.io.Files
 import org.locationtech.geomesa.accumulo.data.{AccumuloDataStore, AccumuloDataStoreFactory}
 import org.locationtech.geomesa.blob.core.AccumuloBlobStore
 import org.locationtech.geomesa.web.core.GeoMesaScalatraServlet
-import org.scalatra.{BadRequest, Ok}
+import org.scalatra.{NotFound, BadRequest, Ok}
 
 import scala.collection.JavaConversions._
 
@@ -20,7 +23,7 @@ class BlobstoreServlet extends GeoMesaScalatraServlet {
     val ds = new AccumuloDataStoreFactory().createDataStore(dsParams).asInstanceOf[AccumuloDataStore]
 
     if (ds == null) {
-      BadRequest(reason = "Could not load data store using the provided parameters.")
+      NotFound(reason = "Could not load data store using the provided parameters.")
     } else {
       abs = new AccumuloBlobStore(ds)
       Ok()
@@ -32,13 +35,16 @@ class BlobstoreServlet extends GeoMesaScalatraServlet {
     println(s"In ID method, trying to retrieve id $id")
 
     if (abs == null) {
-      BadRequest(reason = "AccumuloBlobStore is not initialized.")
+      NotFound(reason = "AccumuloBlobStore is not initialized.")
     } else {
-      val returnBytes = abs.get(id)._1
+      val (returnBytes, filename) = abs.get(id)
       if (returnBytes == null) {
-        BadRequest(reason = s"Unknown ID $id")
+        NotFound(reason = s"Unknown ID $id")
       } else {
-        returnBytes
+        contentType = "application/octet-stream"
+        response.setHeader("Content-Disposition", "attachment; filename=" + filename)
+
+        Ok(returnBytes)
       }
     }
   }
