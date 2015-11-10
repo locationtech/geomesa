e7331db09b4b5215a8c059dd581580cab86c432a
Alex Tatusko <atatusko@ccri.com>
2015-11-10 15:13:29 -0500

    GEOMESA-964 Exposing java api for KyroFeatureSerializer
    Signed-off-by: Alex Tatusko <atatusko@ccri.com>

---
+++ geomesa-features/geomesa-feature-common/src/main/java/org/locationtech/geomesa/features/interop/SerializationOptions.java
@@ -0,0 +1,17 @@
+/***********************************************************************
+ * Copyright (c) 2013-2015 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0 which
+ * accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ *************************************************************************/
+
+package org.locationtech.geomesa.features.interop;
+
+import org.locationtech.geomesa.features.SerializationOption;
+
+public class SerializationOptions {
+    public static SerializationOption.SerializationOptions withUserData() {
+        return SerializationOption.SerializationOptions$.MODULE$.withUserData();
+    }
+}
