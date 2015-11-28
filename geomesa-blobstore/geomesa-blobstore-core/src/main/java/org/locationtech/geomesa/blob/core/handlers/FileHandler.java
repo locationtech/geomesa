c114e7eaef7e56701fb329bf5b826d41f4cd330e
Jim Hughes <jnh5y@ccri.com>
2015-11-28 16:14:38 -0500

    Adding README.md

--- geomesa-blobstore/geomesa-blobstore-core/src/main/java/org/locationtech/geomesa/blob/core/handlers/FileHandler.java
+++ geomesa-blobstore/geomesa-blobstore-core/src/main/java/org/locationtech/geomesa/blob/core/handlers/FileHandler.java
@@ -13,8 +13,23 @@ import org.opengis.feature.simple.SimpleFeature;
 import java.io.File;
 import java.util.Map;
 
+/**
+ * An interface to define how to build SimpleFeatures from a File and a map of parameters
+ */
 public interface FileHandler {
+    /**
+     * Indicates whether or not the class can handle the file with the associated parameters.
+     * @param file   File to Store
+     * @param params Map of parameters indicating or hinting how to processing should work.
+     * @return       Whether or not the class can handle the given input
+     */
     public Boolean canProcess(File file, Map<String, String> params);
 
+    /**
+     * This method builds the SimpleFeature indexing the input file.
+     * @param file   File to Store
+     * @param params Map of parameters indicating or hinting how to processing should work.
+     * @return       SimpleFeature indexing the file.  Must contain a unique ID.
+     */
     public SimpleFeature buildSF(File file, Map<String, String> params);
 }
