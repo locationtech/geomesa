6678d9d606c10ac3cc2897d258d354d9d69bdfff
Jim Hughes <jnh5y@ccri.com>
2016-03-29 17:48:47 -0400

    GEOMESA-1154 - Improve KDS consumer install

---
+++ geomesa-tools/bin/install-kafka.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0 which
+# accompanies this distribution and is available at
+# http://www.opensource.org/licenses/apache2.0.php.
+#
+
+if [[ (-z "$1") ]]; then
+    echo "Error: Provide one arg which is the target directory (e.g. /opt/jboss/standalone/deployments/geoserver.war/WEB-INF/lib)"
+    exit
+else
+    install_dir=$1
+    NL=$'\n'
+    read -r -p "Install Kafka DataStore dependencies to ${install_dir}?${NL}Confirm? [Y/n]" confirm
+    confirm=${confirm,,} #lowercasing
+    if [[ $confirm =~ ^(yes|y) ]]; then
+        # get stuff
+        declare -a urls=(
+            "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka-clients/0.8.2.1/kafka-clients-0.8.2.1.jar"
+            "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka_2.11/0.8.2.1/kafka_2.11-0.8.2.1.jar"
+            "https://search.maven.org/remotecontent?filepath=com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
+            "https://search.maven.org/remotecontent?filepath=com/101tec/zkclient/0.3/zkclient-0.3.jar"
+            "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/3.4.5/zookeeper-3.4.5.jar"
+            )
+
+        for x in "${urls[@]}"; do
+            fname=$(basename "$x");
+            echo "fetching ${x}";
+            wget -O "${1}/${fname}" "$x";
+         done
+    else
+        echo "Installation cancelled"
+    fi
+fi
