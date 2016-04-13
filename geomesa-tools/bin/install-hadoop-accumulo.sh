a6705b55affe27902e9474384b2e37e2c1ab8392
Andrew Hulbert <andrew.hulbert@ccri.com>
2016-04-13 12:55:21 -0400

    GEOMESA-1157 Add commons-logging and guava to install script
    * These are needed for command line tools too work (e.g. ingest)
    
    Signed-off-by: Andrew Hulbert <andrew.hulbert@ccri.com>

--- geomesa-tools/bin/install-hadoop-accumulo.sh
+++ geomesa-tools/bin/install-hadoop-accumulo.sh
@@ -7,6 +7,21 @@
 # http://www.opensource.org/licenses/apache2.0.php.
 #
 
+# This script will attempt to install the client dependencies for hadoop and accumulo
+# into a given directory. Usually this is used to install the deps into either the
+# geomesa tools lib dir or the WEB-INF/lib dir of geoserver.
+
+accumulo_version="${accumulo.version}"
+hadoop_version="${hadoop.version}"
+zookeeper_version="${zookeeper.version}"
+thrift_version="${thrift.version}"
+
+# for hadoop 2.5 and 2.6 to work we need these
+guava_version="11.0.2"
+com_log_version="1.1.3"
+
+base_url="https://search.maven.org/remotecontent?filepath="
+
 if [[ (-z "$1") ]]; then
     echo "Error: Provide one arg which is the target directory (e.g. /opt/jboss/standalone/deployments/geoserver.war/WEB-INF/lib)"
     exit
@@ -18,16 +33,18 @@ else
     if [[ $confirm =~ ^(yes|y) ]]; then
         # get stuff
         declare -a urls=(
-            "https://search.maven.org/remotecontent?filepath=org/apache/accumulo/accumulo-core/${accumulo.version}/accumulo-core-${accumulo.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/accumulo/accumulo-fate/${accumulo.version}/accumulo-fate-${accumulo.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/accumulo/accumulo-trace/${accumulo.version}/accumulo-trace-${accumulo.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/thrift/libthrift/${thrift.version}/libthrift-${thrift.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/${zookeeper.version}/zookeeper-${zookeeper.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=commons-configuration/commons-configuration/1.6/commons-configuration-1.6.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/hadoop/hadoop-auth/${hadoop.version}/hadoop-auth-${hadoop.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/hadoop/hadoop-client/${hadoop.version}/hadoop-client-${hadoop.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/hadoop/hadoop-common/${hadoop.version}/hadoop-common-${hadoop.version}.jar"
-            "https://search.maven.org/remotecontent?filepath=org/apache/hadoop/hadoop-hdfs/${hadoop.version}/hadoop-hdfs-${hadoop.version}.jar"
+            "${base_url}org/apache/accumulo/accumulo-core/${accumulo_version}/accumulo-core-${accumulo_version}.jar"
+            "${base_url}org/apache/accumulo/accumulo-fate/${accumulo_version}/accumulo-fate-${accumulo_version}.jar"
+            "${base_url}org/apache/accumulo/accumulo-trace/${accumulo_version}/accumulo-trace-${accumulo_version}.jar"
+            "${base_url}org/apache/thrift/libthrift/${thrift_version}/libthrift-${thrift_version}.jar"
+            "${base_url}org/apache/zookeeper/zookeeper/${zookeeper_version}/zookeeper-${zookeeper_version}.jar"
+            "${base_url}commons-configuration/commons-configuration/1.6/commons-configuration-1.6.jar"
+            "${base_url}org/apache/hadoop/hadoop-auth/${hadoop_version}/hadoop-auth-${hadoop_version}.jar"
+            "${base_url}org/apache/hadoop/hadoop-client/${hadoop_version}/hadoop-client-${hadoop_version}.jar"
+            "${base_url}org/apache/hadoop/hadoop-common/${hadoop_version}/hadoop-common-${hadoop_version}.jar"
+            "${base_url}org/apache/hadoop/hadoop-hdfs/${hadoop_version}/hadoop-hdfs-${hadoop_version}.jar"
+            "${base_url}commons-logging/commons-logging/${com_log_version}/commons-logging-${com_log_version}.jar"
+            "${base_url}com/google/guava/guava/${guava_version}/guava-${guava_version}.jar"
             )
 
         for x in "${urls[@]}"; do
