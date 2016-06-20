badf324d67ff2c25157edeb048e919a63f669215
Alex Tatusko <atatusko@ccri.com>
2016-06-20 12:25:27 -0400

    General improvements

--- geomesa-web/geomesa-web-stats/src/main/scala/org/locationtech/geomesa/web/stats/GeoMesaStatsEndpoint.scala
+++ geomesa-web/geomesa-web-stats/src/main/scala/org/locationtech/geomesa/web/stats/GeoMesaStatsEndpoint.scala
@@ -9,43 +9,72 @@
 package org.locationtech.geomesa.web.stats
 
 import com.typesafe.scalalogging.LazyLogging
+import org.json4s.{DefaultFormats, Formats}
 import org.locationtech.geomesa.utils.stats.MinMax
 import org.locationtech.geomesa.web.core.GeoMesaServletCatalog.GeoMesaLayerInfo
 import org.locationtech.geomesa.web.core.{GeoMesaScalatraServlet, GeoMesaServletCatalog}
 import org.scalatra.BadRequest
+import org.scalatra.json._
 
 import scala.collection.JavaConversions._
 
-class GeoMesaStatsEndpoint extends GeoMesaScalatraServlet with LazyLogging {
+class GeoMesaStatsEndpoint extends GeoMesaScalatraServlet with LazyLogging with NativeJsonSupport {
   override def root: String = "stats"
 
-  logger.warn("*** Starting the stats REST API endpoint!")
+  override def defaultFormat: Symbol = 'json
+  override protected implicit def jsonFormats: Formats = DefaultFormats
+
+  logger.info("*** Starting the stats REST API endpoint!")
+
+  before() {
+    contentType = formats(format)
+  }
 
   // Endpoint for debugging
-  get("/:workspace/:layer") {
+  get("/:workspace/:layer/?") {
     retrieveInfo("get") match {
-      case Some(statInfo) => logger.warn("Found a GeoMesa datastore for the layer")
+      case Some(statInfo) => logger.info("Found a GeoMesa datastore for the layer")
         val sft = statInfo.sft
-        logger.warn(s"SFT for layer is $sft.")
+        logger.info(s"SFT for layer is $sft.")
 
-      case _ => logger.warn("Didn't find a GeoMesa datastore.  Available ws/layer combinations are:")
+      case _ => logger.info("Didn't find a GeoMesa datastore.  Available ws/layer combinations are:")
         GeoMesaServletCatalog.getKeys.foreach { case (ws, layer) =>
-          logger.warn(s"Workspace: $ws Layer: $layer.")
+          logger.info(s"Workspace: $ws Layer: $layer.")
         }
+    }
+  }
+
+  /**
+    * Histogram
+    */
+  get("/:workspace/:layer/histogram") {
+    retrieveInfo("histogram") match {
+      case Some(statInfo) => logger.info("Found a GeoMesa datastore for the layer")
+        val sft = statInfo.sft
+        logger.info(s"SFT for layer is $sft.")
+
+        statInfo.ads.stats.getCount(sft) match {
+          case Some(count) => logger.info(s"Got count $count")
+            count
+          case None =>        logger.info(s"No estimated count for ${sft.getTypeName}")
+            BadRequest(s"Estimated count for ${sft.getTypeName} is not available.")
+        }
+
+      case _ => BadRequest("No registered datastore.")
     }
   }
 
   // Returns an estimated count for the entire layer.
   get("/:workspace/:layer/count") {
     retrieveInfo("count") match {
-      case Some(statInfo) => logger.warn("Found a GeoMesa datastore for the layer")
+      case Some(statInfo) => logger.info("Found a GeoMesa datastore for the layer")
         val sft = statInfo.sft
-        logger.warn(s"SFT for layer is $sft.")
+        logger.info(s"SFT for layer is $sft.")
 
         statInfo.ads.stats.getCount(sft) match {
-          case Some(count) => logger.warn(s"Got count $count")
+          case Some(count) => logger.info(s"Got count $count")
             count
-          case None =>        logger.warn(s"No estimated count for ${sft.getTypeName}")
+          case None =>        logger.info(s"No estimated count for ${sft.getTypeName}")
             BadRequest(s"Estimated count for ${sft.getTypeName} is not available.")
         }
 
@@ -56,9 +85,9 @@ class GeoMesaStatsEndpoint extends GeoMesaScalatraServlet with LazyLogging {
   // Returns a spatial envelope for the layer.
   get("/:workspace/:layer/envelope") {
     retrieveInfo("envelope") match {
-      case Some(statInfo) => logger.warn("Found a GeoMesa datastore for the layer")
+      case Some(statInfo) => logger.info("Found a GeoMesa datastore for the layer")
         val sft = statInfo.sft
-        logger.warn(s"SFT for layer is $sft.")
+        logger.info(s"SFT for layer is $sft.")
 
         statInfo.ads.stats.getBounds(sft)
 
@@ -69,7 +98,7 @@ class GeoMesaStatsEndpoint extends GeoMesaScalatraServlet with LazyLogging {
   // Gets the attribute bounds for a layer.
   get("/:workspace/:layer/bounds") {
     retrieveInfo("envelope") match {
-      case Some(statInfo) => logger.warn("Found a GeoMesa datastore for the layer")
+      case Some(statInfo) => logger.info("Found a GeoMesa datastore for the layer")
         val sft = statInfo.sft
         val attrNames = sft.getAttributeDescriptors.map(_.getLocalName)
 
@@ -82,7 +111,7 @@ class GeoMesaStatsEndpoint extends GeoMesaScalatraServlet with LazyLogging {
   def retrieveInfo(call: String): Option[GeoMesaLayerInfo] = {
     val workspace = params("workspace")
     val layer     = params("layer")
-    logger.warn(s"Received $call request for workspace: $workspace and layer: $layer.")
+    logger.info(s"Received $call request for workspace: $workspace and layer: $layer.")
     GeoMesaServletCatalog.getGeoMesaLayerInfo(workspace, layer)
   }
 }
