378186ed7ab313ed3e5be7556590d76202dd91ab
Emilio <elahrvivaz@ccri.com>
2016-06-22 16:51:48 -0400

    GEOMESA-1271 Adding density iterator to record strategy (#952)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-accumulo/geomesa-accumulo-datastore/src/test/scala/org/locationtech/geomesa/accumulo/iterators/DensityIteratorTest.scala
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/test/scala/org/locationtech/geomesa/accumulo/iterators/DensityIteratorTest.scala
@@ -21,6 +21,8 @@ import org.joda.time.{DateTime, DateTimeZone}
 import org.junit.runner.RunWith
 import org.locationtech.geomesa.accumulo.TestWithMultipleSfts
 import org.locationtech.geomesa.accumulo.index.QueryHints
+import org.locationtech.geomesa.accumulo.index.Strategy.StrategyType
+import org.locationtech.geomesa.accumulo.index.Strategy.StrategyType.StrategyType
 import org.locationtech.geomesa.features.ScalaSimpleFeature
 import org.locationtech.geomesa.utils.geotools.Conversions._
 import org.opengis.feature.simple.SimpleFeatureType
@@ -44,12 +46,16 @@ class DensityIteratorTest extends Specification with TestWithMultipleSfts {
 
   def spec(binding: String) = s"an_id:java.lang.Integer,attr:java.lang.Double,dtg:Date,*geom:$binding:srid=4326"
 
-  def getDensity(sftName: String, query: String): List[(Double, Double, Double)] = {
+  def getDensity(sftName: String,
+                 query: String,
+                 envelope: Option[Envelope] = None,
+                 strategy: Option[StrategyType] = None): List[(Double, Double, Double)] = {
     val q = new Query(sftName, ECQL.toFilter(query))
-    val geom = q.getFilter.accept(ExtractBoundsFilterVisitor.BOUNDS_VISITOR, null).asInstanceOf[Envelope]
+    val geom = envelope.getOrElse(q.getFilter.accept(ExtractBoundsFilterVisitor.BOUNDS_VISITOR, null).asInstanceOf[Envelope])
     q.getHints.put(QueryHints.DENSITY_BBOX_KEY, new ReferencedEnvelope(geom, DefaultGeographicCRS.WGS84))
     q.getHints.put(QueryHints.WIDTH_KEY, 500)
     q.getHints.put(QueryHints.HEIGHT_KEY, 500)
+    strategy.foreach(s => q.getHints.put(QueryHints.QUERY_STRATEGY_KEY, s))
     val decode = KryoLazyDensityIterator.decodeResult(geom, 500, 500)
     ds.getFeatureSource(sftName).getFeatures(q).features().flatMap(decode).toList
   }
@@ -91,6 +97,18 @@ class DensityIteratorTest extends Specification with TestWithMultipleSfts {
         val q = "BBOX(geom, -1, 33, 6, 40) AND " +
             "dtg between '2012-01-01T18:00:00.000Z' AND '2012-01-01T23:00:00.000Z'"
         val density = getDensity(sft.getTypeName, q)
+
+        density.length must beLessThan(150)
+        density.map(_._3).sum mustEqual 150
+        val compiled = density.groupBy(d => (d._1, d._2)).map { case (pt, group) => group.map(_._3).sum }
+        // should be 5 bins of 30
+        compiled must haveLength(5)
+        forall(compiled)(_ mustEqual 30)
+      }
+
+      "with record index" >> {
+        val q = "INCLUDE"
+        val density = getDensity(sft.getTypeName, q, Some(new Envelope(-180, 180, -90, 90)), Some(StrategyType.RECORD))
 
         density.length must beLessThan(150)
         density.map(_._3).sum mustEqual 150
