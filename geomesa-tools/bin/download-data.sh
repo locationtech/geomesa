da579288a578c1a5f7400991c1bc4fa6cf7dcc16
Andrew Hulbert <ahulbert@ccri.com>
2016-06-21 21:52:51 -0400

    Adding header to download-data script
    Signed-off-by: Andrew Hulbert <ahulbert@ccri.com>

--- geomesa-tools/bin/download-data.sh
+++ geomesa-tools/bin/download-data.sh
@@ -1,4 +1,11 @@
 #! /usr/bin/env bash
+#
+# Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0 which
+# accompanies this distribution and is available at
+# http://www.opensource.org/licenses/apache2.0.php.
+#
 
 type=$1
 
