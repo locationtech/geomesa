c679e16e643a6d0e6ef810c5307a1fc22c3c5b22
Matthew Zimmerman <matt.zimmerman@ccri.com>
2016-07-06 09:40:13 -0400

    GEOMESA-1297 Fix LaTeX build
    (Mostly) fixes LaTeX build of docs, which was not changed when we
    reorganized how the different manuals were built with HTML.
    
    Signed-off-by: Matthew Zimmerman <matt.zimmerman@ccri.com>

--- docs/common.py
+++ docs/common.py
@@ -289,7 +289,7 @@ latex_documents = [
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = True
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
