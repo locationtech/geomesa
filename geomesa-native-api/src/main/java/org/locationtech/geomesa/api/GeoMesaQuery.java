fc84c5a3492a207753a5f13ffaf4acee6c8151fb
Chris Eichelberger <cne1x@ccri.com>
2016-08-03 11:29:37 -0400

    GEOMESA-1356 The Native API's query-builder should allow temporal bou… (#1020)
    * GEOMESA-1356 The Native API's query-builder should allow temporal bounds to be (in|ex)clusive
    
    Added a new builder function and corresponding unit tests.
    
    Signed-off-by: Chris Eichelberger <cne1x@ccri.com>

--- geomesa-native-api/src/main/java/org/locationtech/geomesa/api/GeoMesaQuery.java
+++ geomesa-native-api/src/main/java/org/locationtech/geomesa/api/GeoMesaQuery.java
@@ -12,7 +12,7 @@ import org.apache.hadoop.classification.InterfaceStability;
 import org.geotools.factory.CommonFactoryFinder;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
-import org.opengis.filter.PropertyIsBetween;
+import org.opengis.filter.expression.PropertyName;
 import org.opengis.filter.spatial.BBOX;
 
 import java.util.Date;
@@ -27,6 +27,7 @@ public class GeoMesaQuery {
 
     public static class GeoMesaQueryBuilder {
         private static final FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+        private static PropertyName DTGProperty = ff.property("dtg");
         private Double minx, miny, maxx, maxy;
         private Date start;
         private Date end;
@@ -44,10 +45,82 @@ public class GeoMesaQuery {
             return this;
         }
 
-        public GeoMesaQueryBuilder during(Date start, Date end) {
-            this.start = start;
-            this.end = end;
+        public GeoMesaQueryBuilder before(Date end) {
+            return during(null, false, end, false);
+        }
+
+        public GeoMesaQueryBuilder after(Date start) {
+            return during(start, false, null, false);
+        }
+
+        public GeoMesaQueryBuilder allTime() {
+            this.start = null;
+            this.end = null;
             return this;
+        }
+
+        public GeoMesaQueryBuilder during(Date start, Date end) {
+            return during(start, true, end, true);
+        }
+
+        /**
+         * We implement the "during" as a GeoTools "between" predicate, which includes the endpoints
+         * by default; see:
+         *   https://github.com/geotools/geotools/blob/15.x/modules/library/main/src/main/java/org/geotools/filter/IsBetweenImpl.java#L144
+         *
+         * If the temporal bounds are set to non-sensical values, that will be handled elsewhere.
+         *
+         * WARNING:  NULL values do *NOT* overwrite existing start/end values for the query builder.
+         * To clear out the start/end values, you must use the "allTime" method.
+         *
+         * @param start the start date
+         * @param isStartInclusive whether the start date should be included in the interval
+         * @param end the end date
+         * @param isEndInclusive whether the end date should be included in the interval
+         * @return the query object updated for the effective temporal bounds
+         */
+        public GeoMesaQueryBuilder during(Date start, boolean isStartInclusive, Date end, boolean isEndInclusive) {
+            if (start != null) {
+                if (isStartInclusive) {
+                    this.start = start;
+                } else {
+                    this.start = new Date(start.getTime() + 1L);
+                }
+            }
+
+            if (end != null) {
+                if (isEndInclusive) {
+                    this.end = end;
+                } else {
+                    this.end = new Date(end.getTime() - 1L);
+                }
+            }
+
+            return this;
+        }
+
+        /**
+         * Converts the current start- and end-point into a valid temporal filter.
+         *
+         * @return the net temporal filter that results from the current temporal bounds
+         */
+        public Filter getTemporalFilter() {
+            // if neither end-point is set, there is no valid filter
+            if (start == null && end == null) return Filter.INCLUDE;
+
+            // if the start-point alone is defined, this is an AFTER query
+            if (end == null) return ff.after(DTGProperty, ff.literal(start));
+
+            // if the end-point alone is defined, this is a BEFORE query
+            if (start == null) return ff.before(DTGProperty, ff.literal(end));
+
+            // both are defined; if they are ordered correctly, this is a DURING query
+            if (start.getTime() <= end.getTime()) return ff.between(DTGProperty, ff.literal(start), ff.literal(end));
+
+            // if you get this far, you have non-null temporal bounds that are improperly ordered;
+            // this is equivalent to having no valid temporal bounds
+            // TODO:  log this occurrence, because it is unexpected, and the developer should be informed
+            return Filter.INCLUDE;
         }
 
         public GeoMesaQueryBuilder filter(Filter filter) {
@@ -59,8 +132,7 @@ public class GeoMesaQuery {
             // TODO: need to respect the SimpleFeatureView
             GeoMesaQuery query = new GeoMesaQuery();
             BBOX geoFilter = ff.bbox("geom", minx, miny, maxx, maxy, "EPSG:4326");
-            PropertyIsBetween dtgFilter = ff.between(ff.property("dtg"), ff.literal(start), ff.literal(end));
-            query.filter = ff.and(extraFilter, ff.and(geoFilter, dtgFilter));
+            query.filter = ff.and(extraFilter, ff.and(geoFilter, getTemporalFilter()));
             return query;
         }
     }
