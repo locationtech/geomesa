90aa8e77431c46771d60c3b3c31245c7a9e79875
Matthew Zimmerman <matt.zimmerman@ccri.com>
2016-08-11 09:58:52 -0400

    GEOMESA-1108 Pull version for docs from pom.xml properly (#1035)
    Setting versions via Sphinx command line was not doing all of the
    other substitions properly. This solution sets the versions via
    the maven-resources-plugin prior to Sphinx invocation.
    
    Signed-off-by: Matthew Zimmerman <matt.zimmerman@ccri.com>

---
+++ docs/versions.py
@@ -0,0 +1,8 @@
+# NB: This file is preprocessed by Maven; the version that is used
+# by Sphinx is built in target/versions.py
+
+# version: The short X.Y version.
+# release: The full version, including alpha/beta/rc tags.
+release = '${project.version}'
+version = '${project.version}'
+version_devel = '${geomesa.devel.version}'
