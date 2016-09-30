bc8807afa00f2456f6d8455547b79f6c4e8bca00
Charles Kelly <charles.kelly@ccri.com>
2016-09-30 16:58:33 -0400

    GEOMESA-1220 Don't freeze on bad formats
    Signed-off-by: Charles Kelly <charles.kelly@ccri.com>

--- geomesa-accumulo/geomesa-accumulo-tools/src/test/scala/org/locationtech/geomesa/tools/accumulo/commands/IngestCommandTest.scala
+++ geomesa-accumulo/geomesa-accumulo-tools/src/test/scala/org/locationtech/geomesa/tools/accumulo/commands/IngestCommandTest.scala
@@ -75,6 +75,62 @@ class IngestCommandTest extends Specification {
       features.map(_.get[String]("name")) must containTheSameElementsAs(Seq("Hermione", "Harry", "Severus"))
     }
 
+    "not ingest csv to tsv " >> {
+      val id = nextId
+
+      val confFile = new File(this.getClass.getClassLoader.getResource("examples/example2.conf").getFile)
+      val dataFile = new File(this.getClass.getClassLoader.getResource("examples/example1.csv").getFile)
+
+      val args = Array("ingest", "--mock", "-i", id, "-u", "foo", "-p", "bar", "-c", id,
+        "--converter", confFile.getPath, "-s", confFile.getPath, dataFile.getPath)
+      args.length mustEqual 15
+
+      val command = AccumuloRunner.createCommand(args)
+      command.execute()
+
+      val ds = DataStoreParamsHelper.createDataStore(command.params.asInstanceOf[GeoMesaConnectionParams])
+      import Conversions._
+      val features = ds.getFeatureSource("renegades2").getFeatures.features().toList
+      features.size mustEqual 0
+    }
+
+    "ingest mysql to tsv" >> {
+      val id = nextId
+
+      val confFile = new File(this.getClass.getClassLoader.getResource("examples/city1.conf").getFile)
+      val dataFile = new File(this.getClass.getClassLoader.getResource("examples/city.mysql").getFile)
+
+      val args = Array("ingest", "--mock", "-i", id, "-u", "foo", "-p", "bar", "-c", id,
+        "--converter", confFile.getPath, "-s", confFile.getPath, dataFile.getPath)
+      args.length mustEqual 15
+
+      val command = AccumuloRunner.createCommand(args)
+      command.execute()
+
+      val ds = DataStoreParamsHelper.createDataStore(command.params.asInstanceOf[GeoMesaConnectionParams])
+      import Conversions._
+      val features = ds.getFeatureSource("geonames").getFeatures.features().toList
+      features.size mustEqual 3
+    }
+
+     "not ingest tsv to mysql" >> {
+      val id = nextId
+
+      val confFile = new File(this.getClass.getClassLoader.getResource("examples/city2.conf").getFile)
+      val dataFile = new File(this.getClass.getClassLoader.getResource("examples/city.tsv").getFile)
+
+      val args = Array("ingest", "--mock", "-i", id, "-u", "foo", "-p", "bar", "-c", id,
+        "--converter", confFile.getPath, "-s", confFile.getPath, dataFile.getPath)
+      args.length mustEqual 15
+
+      val command = AccumuloRunner.createCommand(args)
+      command.execute()
+
+      val ds = DataStoreParamsHelper.createDataStore(command.params.asInstanceOf[GeoMesaConnectionParams])
+      import Conversions._
+      val features = ds.getFeatureSource("geonames").getFeatures.features().toList
+      features.size mustEqual 0
+    }
     // TODO GEOMESA-529 more testing of explicit commands
 
   }
