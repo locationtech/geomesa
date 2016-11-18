967d79098889eb98d7e8e44a10bf392fb6031cd3
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2016-11-18 13:28:48 -0500

    GEOMESA-1531 Kafka tools for 09, 10
    * Modules are copy-pasted from 08 tools
    
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-kafka/geomesa-kafka-tools/bin/install-kafka.sh
+++ geomesa-kafka/geomesa-kafka-tools/bin/install-kafka.sh
@@ -7,42 +7,38 @@
 # http://www.opensource.org/licenses/apache2.0.php.
 #
 
-kafka_major_version="0.8"
+kafka_major_version="%%kafka.maj.version%%"
 
 if [[ (-z "$1") ]]; then
   echo "Error: Provide one arg which is the target directory (e.g. /opt/jboss/standalone/deployments/geoserver.war/WEB-INF/lib)"
-  echo "You may also use 0.9 as an optional second parameter if using kafka 0.9"
   exit
 fi
-if [[ (! -z "$2") ]]; then
-  kafka_major_version=$2
-fi
+
 install_dir=$1
 NL=$'\n'
 read -r -p "Install Kafka ${kafka_major_version} DataStore dependencies to ${install_dir}?${NL}Confirm? [Y/n]" confirm
 confirm=${confirm,,} #lowercasing
 if [[ $confirm =~ ^(yes|y) || $confirm == "" ]]; then
   # get stuff
-  if [[ $kafka_major_version = "0.8" ]]; then
-    declare -a urls=(
-      "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka-clients/${kafka.version}/kafka-clients-${kafka.version}.jar"
-      "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka_2.11/${kafka.version}/kafka_2.11-${kafka.version}.jar"
-      "https://search.maven.org/remotecontent?filepath=com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
-      "https://search.maven.org/remotecontent?filepath=com/101tec/zkclient/0.3/zkclient-0.3.jar"
-      "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/${zookeeper.version}/zookeeper-${zookeeper.version}.jar"
-      )
-  elif [[ $kafka_major_version = "0.9" ]]; then
-    declare -a urls=(
-      "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka-clients/0.9.0.1/kafka-clients-0.9.0.1.jar"
-      "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka_2.11/0.9.0.1/kafka_2.11-0.9.0.1.jar"
-      "https://search.maven.org/remotecontent?filepath=com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
-      "https://search.maven.org/remotecontent?filepath=com/101tec/zkclient/0.7/zkclient-0.7.jar"
-      "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/${zookeeper.version}/zookeeper-${zookeeper.version}.jar"
-      )
+
+  if [[ $kafka_major_version = "08" ]]; then
+    ZK_CLIENT_VERSION="0.3"
+  elif [[ $kafka_major_version = "09" ]]; then
+    ZK_CLIENT_VERSION="0.7"
+  elif [[ $kafka_major_version = "10" ]]; then
+    ZK_CLIENT_VERSION="0.8"
   else
-    echo "Error: Kafka version ${kafka_major_version} is not supported.  Please select 0.9 as second arg or omit to use default (0.8)."
+    echo "Error: Kafka version ${kafka_major_version} is not supported."
     exit
   fi
+
+  declare -a urls=(
+    "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka-clients/%%kafka.min.version%%/kafka-clients-%%kafka.min.version%%.jar"
+    "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka_2.11/%%kafka.min.version%%/kafka_2.11-%%kafka.min.version%%.jar"
+    "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/%%zookeeper.version%%/zookeeper-%%zookeeper.version%%.jar"
+    "https://search.maven.org/remotecontent?filepath=com/101tec/zkclient/${ZK_CLIENT_VERSION}/zkclient-${ZK_CLIENT_VERSION}.jar"
+    "https://search.maven.org/remotecontent?filepath=com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
+  )
 
   for x in "${urls[@]}"; do
     fname=$(basename "$x");
---
+++ geomesa-kafka/geomesa-kafka-tools/geomesa-kafka-10-tools/src/main/scala/org/locationtech/geomesa/kafka/tools/status/GetNamesCommand.scala
@@ -0,0 +1,79 @@
+/***********************************************************************
+* Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0
+* which accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*************************************************************************/
+
+package org.locationtech.geomesa.kafka.tools.status
+
+import com.beust.jcommander.Parameters
+import com.typesafe.scalalogging.LazyLogging
+import org.I0Itec.zkclient.ZkClient
+import org.I0Itec.zkclient.exception.ZkNoNodeException
+import org.locationtech.geomesa.kafka.tools.{KafkaDataStoreCommand, OptionalZkPathParams}
+import org.locationtech.geomesa.kafka10.KafkaUtils10
+
+class KafkaGetTypeNamesCommand extends KafkaDataStoreCommand with LazyLogging {
+
+  override val name = "get-names"
+  override val params = new KafkaGetTypeNamesParams()
+
+  override def execute() = {
+    if (params.zkPath == null) {
+      logger.info(s"Running List Features without zkPath...")
+      logger.info(s"zkPath - schema")
+
+      val zkUtils = KafkaUtils10.createZkUtils(params.zookeepers, Int.MaxValue, Int.MaxValue)
+      try {
+        zkUtils.getAllTopics.filter(_.contains('-')).foreach(printZkPathAndTopicString(zkUtils.zkClient, _))
+      } finally {
+        zkUtils.close()
+      }
+    } else {
+      logger.info(s"Running List Features using zkPath ${params.zkPath}...")
+      withDataStore(_.getTypeNames.foreach(println))
+    }
+  }
+
+  /**
+    * Fetches schema info from zookeeper to check if the topic is one created by GeoMesa.
+    * Prints zkPath and SFT name if valid.
+    *
+    * @param topic The kafka topic
+    */
+  def printZkPathAndTopicString(zkClient: ZkClient, topic: String): Unit = {
+    val sb = new StringBuilder()
+
+    var tokenizedTopic = topic.split("-")
+    var tokenizedTopicCount = tokenizedTopic.length
+
+    while (tokenizedTopicCount > 1) {
+      try {
+        val topicName = zkClient.readData[String](getTopicNamePath(tokenizedTopic)) // throws ZkNoNodeException if not valid
+        if (topicName.equals(topic)) {
+          println(s"/${tokenizedTopic.take(tokenizedTopicCount-1).mkString("/")} - ${tokenizedTopic.last}")
+          return
+        }
+      } catch {
+        case e: ZkNoNodeException =>
+          // wrong zkPath and schema name combo
+      } finally {
+        tokenizedTopicCount -= 1
+        tokenizedTopic = topic.split("-", tokenizedTopicCount)
+      }
+    }
+  }
+
+  private def getTopicNamePath(tokenizedTopic: Array[String]): String = {
+    s"/${tokenizedTopic.mkString("/")}/Topic"
+  }
+}
+
+@Parameters(commandDescription = "List GeoMesa features for a given zkPath")
+class KafkaGetTypeNamesParams extends OptionalZkPathParams {
+  override val isProducer: Boolean = false
+  override var partitions: String = null
+  override var replication: String = null
+}
