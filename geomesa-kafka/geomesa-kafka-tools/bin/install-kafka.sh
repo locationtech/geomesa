29aa0ca5ad2f1efe4587eee39ca0bfa5b05472e1
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2016-11-21 10:18:31 -0500

    moving default jar versions into pom, moving download urls to top of install-kafka file

--- geomesa-kafka/geomesa-kafka-tools/bin/install-kafka.sh
+++ geomesa-kafka/geomesa-kafka-tools/bin/install-kafka.sh
@@ -9,6 +9,14 @@
 
 kafka_major_version="%%kafka.maj.version%%"
 
+declare -a urls=(
+  "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka_2.11/%%kafka.version%%/kafka_2.11-%%kafka.version%%.jar"
+  "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka-clients/%%kafka.version%%/kafka-clients-%%kafka.version%%.jar"
+  "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/%%zookeeper.version%%/zookeeper-%%zookeeper.version%%.jar"
+  "https://search.maven.org/remotecontent?filepath=com/101tec/zkclient/%%zkclient.version%%/zkclient-%%zkclient.version%%.jar"
+  "https://search.maven.org/remotecontent?filepath=com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
+)
+
 if [[ (-z "$1") ]]; then
   echo "Error: Provide one arg which is the target directory (e.g. /opt/jboss/standalone/deployments/geoserver.war/WEB-INF/lib)"
   exit
@@ -16,30 +24,10 @@ fi
 
 install_dir=$1
 NL=$'\n'
-read -r -p "Install Kafka ${kafka_major_version} DataStore dependencies to ${install_dir}?${NL}Confirm? [Y/n]" confirm
+read -r -p "Install Kafka ${kafka_major_version} DataStore dependencies to '${install_dir}' (y/n)? " confirm
 confirm=${confirm,,} #lowercasing
 if [[ $confirm =~ ^(yes|y) || $confirm == "" ]]; then
   # get stuff
-
-  if [[ $kafka_major_version = "08" ]]; then
-    ZK_CLIENT_VERSION="0.3"
-  elif [[ $kafka_major_version = "09" ]]; then
-    ZK_CLIENT_VERSION="0.7"
-  elif [[ $kafka_major_version = "10" ]]; then
-    ZK_CLIENT_VERSION="0.8"
-  else
-    echo "Error: Kafka version ${kafka_major_version} is not supported."
-    exit
-  fi
-
-  declare -a urls=(
-    "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka-clients/%%kafka.min.version%%/kafka-clients-%%kafka.min.version%%.jar"
-    "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka_2.11/%%kafka.min.version%%/kafka_2.11-%%kafka.min.version%%.jar"
-    "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/%%zookeeper.version%%/zookeeper-%%zookeeper.version%%.jar"
-    "https://search.maven.org/remotecontent?filepath=com/101tec/zkclient/${ZK_CLIENT_VERSION}/zkclient-${ZK_CLIENT_VERSION}.jar"
-    "https://search.maven.org/remotecontent?filepath=com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
-  )
-
   for x in "${urls[@]}"; do
     fname=$(basename "$x");
     echo "fetching ${x}";
