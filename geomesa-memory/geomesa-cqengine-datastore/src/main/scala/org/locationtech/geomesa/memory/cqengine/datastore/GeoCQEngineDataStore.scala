ce150c50fcaa6ce395f476d6b4e518d1ca87a169
James Hughes <jnh5y@ccri.com>
2016-12-12 15:53:30 -0500

    GEOMESA-1576 Implement a CQEngine backed GeoTools DataStore (#1274)
    * Initially this DataStore will be used for unit tests. Its scope may increase later.
    
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

---
+++ geomesa-memory/geomesa-cqengine-datastore/src/main/scala/org/locationtech/geomesa/memory/cqengine/datastore/GeoCQEngineDataStore.scala
@@ -0,0 +1,43 @@
+/***********************************************************************
+* Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0
+* which accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*************************************************************************/
+
+package org.locationtech.geomesa.memory.cqengine.datastore
+
+import java.util
+
+import org.geotools.data.Query
+import org.geotools.data.store.{ContentDataStore, ContentEntry, ContentFeatureSource}
+import org.geotools.feature.NameImpl
+import org.locationtech.geomesa.memory.cqengine.GeoCQEngine
+import org.opengis.feature.`type`.Name
+import org.opengis.feature.simple.SimpleFeatureType
+
+import scala.collection.JavaConversions._
+
+class GeoCQEngineDataStore extends ContentDataStore {
+  val namesToEngine = new java.util.concurrent.ConcurrentHashMap[String, GeoCQEngine]()
+
+  override def createFeatureSource(entry: ContentEntry): ContentFeatureSource = {
+    val engine = namesToEngine.get(entry.getTypeName)
+    if (engine != null) {
+      new GeoCQEngineFeatureStore(engine, entry, Query.ALL)
+    } else {
+      null
+    }
+  }
+
+  override def createTypeNames(): util.List[Name] = { namesToEngine.keys().toList.map { new NameImpl(_) } }
+
+  override def createSchema(featureType: SimpleFeatureType): Unit = {
+    namesToEngine.putIfAbsent(featureType.getTypeName, new GeoCQEngine(featureType))
+  }
+}
+
+object GeoCQEngineDataStore {
+  lazy val engine = new GeoCQEngineDataStore
+}
