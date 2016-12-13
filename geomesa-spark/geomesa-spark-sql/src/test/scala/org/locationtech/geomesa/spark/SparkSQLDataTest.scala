3aa82d40f639f730683d04a7d721d6930f703a75
Matthew Zimmerman <matt.zimmerman@ccri.com>
2016-12-13 13:25:48 -0500

    GEOMESA-1573 Implement Spark SQL test framework with in-memory database.
    Now uses the new in-memory CQEngine-backed data store to speed up
    testing.
    
    Stacks on top of: https://github.com/locationtech/geomesa/pull/1275
    
    This PR replaces: https://github.com/locationtech/geomesa/pull/1270
    
    Signed-off-by: Matthew Zimmerman <matt.zimmerman@ccri.com>

---
+++ geomesa-spark/geomesa-spark-sql/src/test/scala/org/locationtech/geomesa/spark/SparkSQLDataTest.scala
@@ -0,0 +1,81 @@
+/***********************************************************************
+* Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0
+* which accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*************************************************************************/
+
+package org.locationtech.geomesa.spark
+
+import java.util.{Map => JMap}
+
+import com.typesafe.scalalogging.LazyLogging
+import com.vividsolutions.jts.geom.{Coordinate, Point}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
+import org.geotools.data.{DataStore, DataStoreFinder}
+import org.geotools.geometry.jts.JTSFactoryFinder
+import org.junit.runner.RunWith
+import org.locationtech.geomesa.utils.interop.WKTUtils
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+import scala.collection.JavaConversions._
+
+@RunWith(classOf[JUnitRunner])
+class SparkSQLDataTest extends Specification with LazyLogging {
+  val createPoint = JTSFactoryFinder.getGeometryFactory.createPoint(_: Coordinate)
+
+  "sql data tests" should {
+    sequential
+
+    val dsParams: JMap[String, String] = Map("cqengine" -> "true", "geotools" -> "true")
+    var ds: DataStore = null
+    var spark: SparkSession = null
+    var sc: SQLContext = null
+
+    var df: DataFrame = null
+
+    // before
+    step {
+      ds = DataStoreFinder.getDataStore(dsParams)
+      spark = SparkSQLTestUtils.createSparkSession()
+      sc = spark.sqlContext
+    }
+
+    "ingest chicago" >> {
+      SparkSQLTestUtils.ingestChicago(ds)
+
+      df = spark.read
+        .format("geomesa")
+        .options(dsParams)
+        .option("geomesa.feature", "chicago")
+        .load()
+      logger.info(df.schema.treeString)
+      df.createOrReplaceTempView("chicago")
+
+      df.collect.length mustEqual 3
+    }
+
+    "basic sql 1" >> {
+      val r = sc.sql("select * from chicago where case_number = 1")
+      val d = r.collect
+
+      d.length mustEqual 1
+      d.head.getAs[Point]("geom") mustEqual createPoint(new Coordinate(-76.5, 38.5))
+    }
+
+    "st_translate" >> {
+      val r = sc.sql(
+        """
+          |select ST_Translate(st_geomFromWKT('POINT(0 0)'), 5, 12)
+        """.stripMargin)
+
+      r.collect().head.getAs[Point](0) mustEqual WKTUtils.read("POINT(5 12)")
+    }
+
+    // after
+    step {
+    }
+  }
+}
