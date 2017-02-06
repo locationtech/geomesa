85155e27571c67cb82903e80eed8677ad32a301d
Matthew Zimmerman <matt.zimmerman@ccri.com>
2017-02-06 12:48:19 -0500

    GEOMESA-1664 - Spark SQL: add antimeridian handling (#1355)
    * Add ``st_antimeridianSafeGeom`` to split geometries that span the antimeridian.
    * Modified ``st_bufferPoint`` to produce antimeridian-safe geometries (required change
      of return type from Polygon to Geometry, as it now may return MultiPolygons).
    
    Signed-off-by: Matthew Zimmerman <matt.zimmerman@ccri.com>

--- geomesa-spark/geomesa-spark-sql/src/main/scala/org/apache/spark/sql/SQLGeometryProcessingFunctions.scala
+++ geomesa-spark/geomesa-spark-sql/src/main/scala/org/apache/spark/sql/SQLGeometryProcessingFunctions.scala
@@ -8,15 +8,19 @@
 
 package org.apache.spark.sql
 
-import com.vividsolutions.jts.geom.{Coordinate, Point, Polygon}
+import com.typesafe.scalalogging.LazyLogging
+import com.vividsolutions.jts.geom.{Geometry, Coordinate, Point, Polygon}
 import com.vividsolutions.jts.util.GeometricShapeFactory
 import org.geotools.geometry.jts.JTSFactoryFinder
+import org.locationtech.geomesa.utils.geohash.GeohashUtils
 import org.locationtech.spatial4j.context.jts.JtsSpatialContext
 import org.locationtech.spatial4j.distance.DistanceUtils
 import org.locationtech.spatial4j.shape.Circle
 import org.locationtech.spatial4j.shape.jts.JtsPoint
 
-object SQLGeometryProcessingFunctions {
+import scala.util.{Failure, Success}
+
+object SQLGeometryProcessingFunctions extends LazyLogging {
 
   @transient private lazy val spatialContext = JtsSpatialContext.GEO
   @transient private lazy val shapeFactory   = spatialContext.getShapeFactory
@@ -28,25 +32,32 @@ object SQLGeometryProcessingFunctions {
       }
     }
 
-  private def fastCircleToPoly(circle: Circle) = {
-    if (circle.getBoundingBox.getCrossesDateLine)
-      throw new IllegalArgumentException("Doesn't support dateline cross yet: "+circle)
-
+  private def fastCircleToGeom(circle: Circle): Geometry = {
     val gsf = geometricShapeFactory.get()
     gsf.setSize(circle.getBoundingBox.getWidth)
     gsf.setNumPoints(4*25) //multiple of 4 is best
     gsf.setCentre(new Coordinate(circle.getCenter.getX, circle.getCenter.getY))
-    gsf.createCircle()
-
+    ST_antimeridianSafeGeom(gsf.createCircle())
   }
 
-  val ST_BufferPoint: (Point, Double) => Polygon = (p, d) => {
+  val ST_antimeridianSafeGeom: Geometry => Geometry = geom => {
+    GeohashUtils.getInternationalDateLineSafeGeometry(geom) match {
+      case Success(g) => g
+      case Failure(e) =>
+        logger.warn(s"Error splitting geometry on anti-meridian for $geom", e)
+        geom
+    }
+  }
+
+  val ST_BufferPoint: (Point, Double) => Geometry = (p, d) => {
     val degrees = DistanceUtils.dist2Degrees(d/1000.0, DistanceUtils.EARTH_MEAN_RADIUS_KM)
-    fastCircleToPoly(new JtsPoint(p, spatialContext).getBuffered(degrees, spatialContext))
+    fastCircleToGeom(new JtsPoint(p, spatialContext).getBuffered(degrees, spatialContext))
   }
 
   def registerFunctions(sqlContext: SQLContext): Unit = {
+    sqlContext.udf.register("st_antimeridianSafeGeom", ST_antimeridianSafeGeom)
     sqlContext.udf.register("st_bufferPoint"         , ST_BufferPoint)
+    sqlContext.udf.register("st_idlSafeGeom"         , ST_antimeridianSafeGeom)
   }
 
 }
