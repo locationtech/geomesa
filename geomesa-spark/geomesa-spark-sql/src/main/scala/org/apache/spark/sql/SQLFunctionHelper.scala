7bac2255abf0df3f8cb4a0ac8439ed40f499dcea
Matthew Zimmerman <matt.zimmerman@ccri.com>
2017-02-14 08:49:28 -0500

    Respond to comments from @aheyne.
    Signed-off-by: Matthew Zimmerman <matt.zimmerman@ccri.com>

--- geomesa-spark/geomesa-spark-sql/src/main/scala/org/apache/spark/sql/SQLFunctionHelper.scala
+++ geomesa-spark/geomesa-spark-sql/src/main/scala/org/apache/spark/sql/SQLFunctionHelper.scala
@@ -1,8 +1,13 @@
+/***********************************************************************
+* Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License, Version 2.0
+* which accompanies this distribution and is available at
+* http://www.opensource.org/licenses/apache2.0.php.
+*************************************************************************/
+
 package org.apache.spark.sql
 
-/**
-  * Created by mzimmerman on 2/8/17.
-  */
 object SQLFunctionHelper {
   def nullableUDF[A1, RT](f: A1 => RT): A1 => RT = {
     in1 => in1 match {
--- geomesa-spark/geomesa-spark-sql/src/test/scala/org/locationtech/geomesa/spark/SparkSQLGeometricCastTest.scala
+++ geomesa-spark/geomesa-spark-sql/src/test/scala/org/locationtech/geomesa/spark/SparkSQLGeometricCastTest.scala
@@ -1,3 +1,11 @@
+/***********************************************************************
+  * Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+  * All rights reserved. This program and the accompanying materials
+  * are made available under the terms of the Apache License, Version 2.0
+  * which accompanies this distribution and is available at
+  * http://www.opensource.org/licenses/apache2.0.php.
+  *************************************************************************/
+
 package org.locationtech.geomesa.spark
 
 import java.util.{Map => JMap}
@@ -13,9 +21,6 @@ import org.specs2.runner.JUnitRunner
 
 import scala.collection.JavaConversions._
 
-/**
-  * Created by mzimmerman on 2/13/17.
-  */
 @RunWith(classOf[JUnitRunner])
 class SparkSQLGeometricCastTest extends Specification with LazyLogging {
 
