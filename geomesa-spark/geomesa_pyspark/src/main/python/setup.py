c778721f5980f7b29bc72ffd605b4ea5f3cc3429
Tom Kunicki <tom.kunicki@ccri.com>
2017-02-28 12:22:47 -0500

    GEOMESA-1635 modify geomesa_pyspark module metadata per review
    Signed-off-by: Tom Kunicki <tom.kunicki@ccri.com>

--- geomesa-spark/geomesa_pyspark/src/main/python/setup.py
+++ geomesa-spark/geomesa_pyspark/src/main/python/setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='geomesa_pyspark',
-    author='Tom Kunicki, Gerard C. Briones',
     version='${python.version}',
-    author_email='tkunicki@ccri.com, gbriones@ccri.com',
+    url='http://www.geomesa.org',
     packages=find_packages(),
     install_requires=['pytz']
 )
