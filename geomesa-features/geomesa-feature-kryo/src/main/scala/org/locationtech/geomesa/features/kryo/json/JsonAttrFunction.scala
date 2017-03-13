6c6cb72c24c9e12cdd4613efbd21f2bd2281f9c6
Austin Heyne <aheyne@ccri.com>
2017-03-13 13:26:06 -0400

    Wip from friday
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

--- geomesa-features/geomesa-feature-kryo/src/main/scala/org/locationtech/geomesa/features/kryo/json/JsonAttrFunction.scala
+++ geomesa-features/geomesa-feature-kryo/src/main/scala/org/locationtech/geomesa/features/kryo/json/JsonAttrFunction.scala
@@ -7,13 +7,13 @@ import org.geotools.filter.capability.FunctionNameImpl.parameter
 import org.geotools.filter.expression.PropertyAccessors
 import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
 import org.opengis.feature.simple.SimpleFeature
-import org.opengis.filter.expression.PropertyName
+import org.opengis.filter.expression.{PropertyName, VolatileFunction}
 
 class JsonAttrFunction extends FunctionExpressionImpl(
   new FunctionNameImpl("jsonattr",
     parameter("propertyName", classOf[PropertyName]),
     parameter("string", classOf[String]))
-  ) with LazyLogging {
+  ) with LazyLogging with VolatileFunction {
 
   override def evaluate(obj: java.lang.Object): AnyRef = {
     //new AttributeExpressionImpl("$.json.[foo path]")
@@ -27,17 +27,17 @@ class JsonAttrFunction extends FunctionExpressionImpl(
     // some mojo to ensure our property accessor is picked up -
     // our accumulo iterators are not generally available in the system classloader
     // instead, we can set the context classloader (as that will be checked if set)
-    val contextClassLoader = Thread.currentThread.getContextClassLoader
-    if (contextClassLoader != null) {
-      logger.warn(s"Bypassing context classloader $contextClassLoader for PropertyAccessor loading")
-    }
-    Thread.currentThread.setContextClassLoader(classOf[JsonAttrFunction].getClassLoader)
+//    val contextClassLoader = Thread.currentThread.getContextClassLoader
+//    if (contextClassLoader != null) {
+//      logger.warn(s"Bypassing context classloader $contextClassLoader for PropertyAccessor loading")
+//    }
+//    Thread.currentThread.setContextClassLoader(classOf[JsonAttrFunction].getClassLoader)
     val accessor = try {
       import scala.collection.JavaConversions._
       PropertyAccessors.findPropertyAccessors(sf, path, null, null).find(_.canHandle(sf, path, classOf[AnyRef]))
     } finally {
       // reset the classloader after loading the accessors
-      Thread.currentThread.setContextClassLoader(contextClassLoader)
+//      Thread.currentThread.setContextClassLoader(contextClassLoader)
     }
     accessor match {
       case Some(a) => a.get(sf, path, classOf[AnyRef])
