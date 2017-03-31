2abc4cce1190933d9b75814df7105df4e16d60a4
Jim Hughes <jnh5y@ccri.com>
2017-03-31 16:21:13 -0400

    Skip Kafka 8/9 builds in Travis.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- .travisbuild.sh
+++ .travisbuild.sh
@@ -14,7 +14,7 @@ bash -c "while true; do sleep $PING_SLEEP; echo [INFO] \$(date -u '+%F %T UTC') 
 PING_LOOP_PID=$!
 
 # build using the maven executable, not the zinc maven compiler (which uses too much memory)
-mvn clean license:check install -Phbase,travis-ci 2>&1 | tee -a $BUILD_OUTPUT | grep -e '^\[INFO\] Building GeoMesa' -e '^\[INFO\] --- \(maven-surefire-plugin\|maven-install-plugin\|scala-maven-plugin.*:compile\)'
+mvn clean license:check install -Phbase,travis-ci -pl '!geomesa-kafka,!geomesa-kafka/geomesa-kafka-datastore,!geomesa-kafka/geomesa-kafka-datastore/geomesa-kafka-datastore-common,!geomesa-kafka/geomesa-kafka-datastore/geomesa-kafka-08-datastore,!geomesa-kafka/geomesa-kafka-datastore/geomesa-kafka-09-datastore,!geomesa-kafka/geomesa-kafka-tools,!geomesa-kafka/geomesa-kafka-tools/geomesa-kafka-08-tools,!geomesa-kafka/geomesa-kafka-gs-plugin,!geomesa-kafka/geomesa-kafka-gs-plugin/geomesa-kafka-08-gs-plugin,!geomesa-kafka/geomesa-kafka-dist,!geomesa-kafka/geomesa-kafka-dist/geomesa-kafka-08-dist,!geomesa-kafka/geomesa-kafka-tools/geomesa-kafka-09-tools,!geomesa-kafka/geomesa-kafka-gs-plugin/geomesa-kafka-09-gs-plugin,!geomesa-kafka/geomesa-kafka-dist/geomesa-kafka-09-dist' 2>&1 | tee -a $BUILD_OUTPUT | grep -e '^\[INFO\] Building GeoMesa' -e '^\[INFO\] --- \(maven-surefire-plugin\|maven-install-plugin\|scala-maven-plugin.*:compile\)'
 RESULT=${PIPESTATUS[0]} # capture the status of the maven build
 
 # dump out the end of the build log, to show success or errors
