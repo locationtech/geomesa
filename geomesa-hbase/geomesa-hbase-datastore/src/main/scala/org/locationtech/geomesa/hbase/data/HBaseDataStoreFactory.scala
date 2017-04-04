715b7d11e48951102dc551ee1262e27daec0dd74
Andrew Hulbert <ahulbert@ccri.com>
2017-04-04 15:32:26 -0400

    boolean param

--- geomesa-hbase/geomesa-hbase-datastore/src/main/scala/org/locationtech/geomesa/hbase/data/HBaseDataStoreFactory.scala
+++ geomesa-hbase/geomesa-hbase-datastore/src/main/scala/org/locationtech/geomesa/hbase/data/HBaseDataStoreFactory.scala
@@ -87,7 +87,7 @@ object HBaseDataStoreFactory {
   object Params {
     val BigTableNameParam  = new Param("bigtable.table.name", classOf[String], "Table name", true)
     val ConnectionParam    = new Param("connection", classOf[Connection], "Connection", false)
-    val RemoteParam        = new Param("remote.filtering", classOf[String], "Remote filtering", false)
+    val RemoteParam        = new Param("remote.filtering", classOf[java.lang.Boolean], "Remote filtering", false)
     val LooseBBoxParam     = GeoMesaDataStoreFactory.LooseBBoxParam
     val QueryThreadsParam  = GeoMesaDataStoreFactory.QueryThreadsParam
     val GenerateStatsParam = GeoMesaDataStoreFactory.GenerateStatsParam
