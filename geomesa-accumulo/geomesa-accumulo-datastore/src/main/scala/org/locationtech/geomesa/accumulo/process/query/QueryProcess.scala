944f9022ae93dd27b3fd57469db0d8fd63b0c5cc
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2017-05-01 17:16:25 -0400

    GEOMESA-1798 Fixing SecuredFeatureCollection for geomesa:Query process
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/process/query/QueryProcess.scala
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/process/query/QueryProcess.scala
@@ -14,12 +14,13 @@ import org.geotools.data.simple.{SimpleFeatureCollection, SimpleFeatureSource}
 import org.geotools.data.store.ReTypingFeatureCollection
 import org.geotools.factory.CommonFactoryFinder
 import org.geotools.feature.DefaultFeatureCollection
-import org.geotools.feature.visitor.{AbstractCalcResult, CalcResult, FeatureCalc}
+import org.geotools.feature.visitor.{AbstractCalcResult, CalcResult, FeatureAttributeVisitor, FeatureCalc}
 import org.geotools.process.factory.{DescribeParameter, DescribeProcess, DescribeResult}
 import org.geotools.util.NullProgressListener
 import org.opengis.feature.Feature
 import org.opengis.feature.simple.SimpleFeature
 import org.opengis.filter.Filter
+import org.opengis.filter.expression.Expression
 
 @DescribeProcess(
   title = "Geomesa Query",
@@ -38,8 +39,14 @@ class QueryProcess extends LazyLogging {
                  name = "filter",
                  min = 0,
                  description = "The filter to apply to the features collection")
-               filter: Filter
-               ): SimpleFeatureCollection = {
+               filter: Filter,
+
+               @DescribeParameter(
+                 name = "properties",
+                 min = 0,
+                 description = "The lists of properties and transform definitions to apply")
+               properties: String = null
+             ): SimpleFeatureCollection = {
 
     logger.debug("Attempting Geomesa query on type " + features.getClass.getName)
 
@@ -47,38 +54,55 @@ class QueryProcess extends LazyLogging {
       logger.warn("WARNING: layer name in geoserver must match feature type name in geomesa")
     }
 
-    val visitor = new QueryVisitor(features, Option(filter).getOrElse(Filter.INCLUDE))
+    val arrayString = Option(properties).map(_.split(";")).orNull
+
+    val visitor = new QueryVisitor(features, Option(filter).getOrElse(Filter.INCLUDE), arrayString)
     features.accepts(visitor, new NullProgressListener)
     visitor.getResult.asInstanceOf[QueryResult].results
   }
 }
 
 class QueryVisitor(features: SimpleFeatureCollection,
-                   filter: Filter)
-  extends FeatureCalc
-          with LazyLogging {
+                   filter: Filter,
+                   properties: Array[String] = null) extends FeatureCalc with FeatureAttributeVisitor with LazyLogging {
 
-  val manualVisitResults = new DefaultFeatureCollection(null, features.getSchema)
-  val ff  = CommonFactoryFinder.getFilterFactory2
+  private val manualVisitResults = new DefaultFeatureCollection(null, features.getSchema)
+  private val ff  = CommonFactoryFinder.getFilterFactory2
 
   // Called for non AccumuloFeactureCollections
-  def visit(feature: Feature): Unit = {
+  override def visit(feature: Feature): Unit = {
+    // TODO:  GEOMESA-1755 Add any necessary transform support to the QueryProcess
     val sf = feature.asInstanceOf[SimpleFeature]
     if(filter.evaluate(sf)) {
       manualVisitResults.add(sf)
     }
   }
 
+  // allows us to accept visitors from retyping feature collections
+  override def getExpressions: java.util.List[Expression] = {
+    import scala.collection.JavaConversions._
+    Option(properties).map(_.toSeq)
+        .getOrElse(features.getSchema.getAttributeDescriptors.map(_.getLocalName))
+        .map(ff.property)
+  }
+
   var resultCalc: QueryResult = new QueryResult(manualVisitResults)
 
   override def getResult: CalcResult = resultCalc
 
-  def setValue(r: SimpleFeatureCollection) = resultCalc = QueryResult(r)
+  def setValue(r: SimpleFeatureCollection): Unit = resultCalc = QueryResult(r)
 
-  def query(source: SimpleFeatureSource, query: Query) = {
+  def query(source: SimpleFeatureSource, query: Query): SimpleFeatureCollection = {
     logger.debug("Running Geomesa query on source type "+source.getClass.getName)
     val combinedFilter = ff.and(query.getFilter, filter)
-    source.getFeatures(combinedFilter)
+    query.setFilter(combinedFilter)
+    if (properties != null) {
+      if (query.getProperties != Query.ALL_PROPERTIES) {
+        logger.warn(s"Overriding inner query's properties (${query.getProperties}) with properties / transforms $properties.")
+      }
+      query.setPropertyNames(properties)
+    }
+    source.getFeatures(query)
   }
 
 }
