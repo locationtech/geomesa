ab29f52fb63e873221a0f7e7e35b3feb486fae08
Andrew <ahulbert@ccri.com>
2017-05-22 14:42:28 -0400

    GEOMESA-1831 clear evaluation context between records (#1524)
    Signed-off-by: Andrew Hulbert <ahulbert@ccri.com>

--- geomesa-convert/geomesa-convert-fixedwidth/src/test/scala/org/locationtech/geomesa/convert/fixedwidth/FixedWidthConverterTest.scala
+++ geomesa-convert/geomesa-convert-fixedwidth/src/test/scala/org/locationtech/geomesa/convert/fixedwidth/FixedWidthConverterTest.scala
@@ -19,6 +19,7 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class FixedWidthConverterTest extends Specification {
 
+  sequential
   "FixedWidthConverter" >> {
 
     val data =
@@ -52,6 +53,40 @@ class FixedWidthConverterTest extends Specification {
       res.size must be equalTo 2
       res(0).getDefaultGeometry.asInstanceOf[Point].getCoordinate must be equalTo new Coordinate(55.0, 45.0)
       res(1).getDefaultGeometry.asInstanceOf[Point].getCoordinate must be equalTo new Coordinate(65.0, 65.0)
+    }
+
+    "set null values on out of order converter components until GEOMESA-1833 is completed" >> {
+      val conf = ConfigFactory.parseString(
+        """
+          | {
+          |   type      = "fixed-width"
+          |   id-field  = "uuid()"
+          |   options {
+          |     validating = false
+          |   }
+          |   fields = [
+          |     { name = "anotherLat", transform = "$lat" },
+          |     { name = "lat",  transform = "$0::double", start = 1, width = 2 },
+          |     { name = "lon",  transform = "$0::double", start = 3, width = 2 },
+          |     { name = "geom", transform = "point($lon, $lat)" }
+          |   ]
+          | }
+        """.stripMargin)
+
+      val sft = SimpleFeatureTypes.createType(ConfigFactory.load("sft_testsft.conf"))
+      val converter = SimpleFeatureConverters.build[String](sft, conf)
+
+      converter must not beNull
+      val res = converter.processInput(data.split("\n").toIterator.filterNot( s => "^\\s*$".r.findFirstIn(s).size > 0)).toList
+      res.size must be equalTo 2
+      res(0).getDefaultGeometry.asInstanceOf[Point].getCoordinate must be equalTo new Coordinate(55.0, 45.0)
+      res(1).getDefaultGeometry.asInstanceOf[Point].getCoordinate must be equalTo new Coordinate(65.0, 65.0)
+
+      // This should really be set to 65.0 but there is a bug in the converters
+      // We need to build an attribute graph and evaluate them in that order GEOMESA-1833
+      //res(1).getAttribute("anotherLat").asInstanceOf[Double] must be equalTo 65.0D
+      res(1).getAttribute("anotherLat") must beNull // wrong for now
+      res(1).getAttribute("anotherLat").asInstanceOf[Double] must be equalTo 0.0D // wrong for now
     }
 
     "process with validation on" >> {
