7408f3ef6571b7ba7ca2b8f4841e771535e8eba6
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2017-07-07 14:18:10 -0400

    GEOMESA-1908 Support for loading configs from distributed file systems
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-utils/src/main/scala/org/locationtech/geomesa/utils/conf/ArgResolver.scala
+++ geomesa-utils/src/main/scala/org/locationtech/geomesa/utils/conf/ArgResolver.scala
@@ -11,31 +11,18 @@ package org.locationtech.geomesa.utils.conf
 import com.typesafe.config.ConfigParseOptions
 import com.typesafe.scalalogging.LazyLogging
 
-import scala.annotation.tailrec
-
 /**
  * Trait for resolving sft/config parameters and handling errors around
  * parsing.
  */
 trait ArgResolver[ReturnType, ParseMethodArgs] extends LazyLogging {
 
-  // Object for holding relevant error to return
-  class ErrorData {
-    var message: String = _
-    var error: Throwable = _
-    def apply(msg: String, e: Throwable) = { message = msg; error = e }
-  }
+  import ArgResolver.ArgTypes.ArgTypes
 
-  // Type Names for arg type guesses
-  object ArgTypes extends Enumeration {
-    val  NAME, SPECSTR, CONFSTR, PATH = Value
-  }
-  import ArgTypes._
-
-  type ResEither = Either[(String, Throwable, Value), ReturnType]
+  type ResEither = Either[(String, Throwable, ArgTypes), ReturnType]
 
   // Important to setAllowMissing to false bc else you'll get a config but it will be empty
-  val parseOpts =
+  protected val parseOpts =
     ConfigParseOptions.defaults()
       .setAllowMissing(false)
       .setClassLoader(null)
@@ -44,50 +31,43 @@ trait ArgResolver[ReturnType, ParseMethodArgs] extends LazyLogging {
       .setSyntax(null)
 
   // Should return a guess of the argType without using the parsers (e.g. regex, contains, etc)
-  def argType(args: ParseMethodArgs): ArgTypes.Value
+  protected def argType(args: ParseMethodArgs): ArgTypes
+
   // (Ordered) List of parse methods to attempt
-  def parseMethodList: List[ParseMethodArgs => ResEither]
+  protected def parseMethodList: Seq[ParseMethodArgs => ResEither]
 
   /**
    * @return the instance of ReturnType parsed from the Args
    */
   def getArg(args: ParseMethodArgs): Either[Throwable, ReturnType] = {
-    val errorData = new ErrorData
-    val res = parseMethods(args, None, parseMethodList, errorData)
+    var error: Exception = null
 
-    if (res.isDefined) Right(res.get)
-    else {
-      val e = new Throwable(errorData.message + "\n" + errorData.error.getMessage, errorData.error)
-      e.setStackTrace(errorData.error.getStackTrace)
-      Left(e)
-    }
+    var methods = parseMethodList
+    assert(methods.nonEmpty, "subclass did not define any parse methods")
+
+    do {
+      methods.head.apply(args) match {
+        case Right(res) => return Right(res) // parse method succeeded, re-cast and return result
+
+        case Left((msg, e, value)) =>
+          logger.debug(msg, e)
+          if (error == null || argType(args) == value) {
+            error = new RuntimeException(s"$msg\n${e.getMessage}", e)
+            error.setStackTrace(e.getStackTrace)
+          }
+      }
+      methods = methods.tail
+    } while (methods.nonEmpty)
+
+    // nothing passed, return the most relevant error
+    Left(error)
   }
+}
 
-  /**
-   * Recursively run through parseMethodList attempting to parse the sft.
-   * The most relevant error message is saved in ErrorData and all error are sent to log.
-   * ErrorData is sent back to the CLArgResolver for display to user.
-   */
-  @tailrec
-  private def parseMethods(args: ParseMethodArgs,
-                           tryMethod: Option[ParseMethodArgs => ResEither] = None,
-                           methodArray: List[ParseMethodArgs => ResEither],
-                           errorData: ErrorData): Option[ReturnType] = {
-    tryMethod match {
-      case Some(method) =>
-        method(args) match {
-          case Right(res) => Some(res) // parse method succeeded, return result
-          case Left((msg, error, value)) =>
-            logger.debug(msg, error)
-            if (argType(args) == value) { errorData(msg, error) }
-            methodArray.length match {
-              case 0 => None // no more parse methods to try, return None
-              case _ => parseMethods(args, Some(methodArray.head), methodArray.drop(1), errorData)
-            }
-        }
-      case None =>
-        require(methodArray != null, "Empty method array given to parseMethods. No parseMethod to run.")
-        parseMethods(args, Some(methodArray.head), methodArray.drop(1), errorData)
-    }
+object ArgResolver {
+  // Type Names for arg type guesses
+  object ArgTypes extends Enumeration {
+    val NAME, SPECSTR, CONFSTR, PATH = Value
+    type ArgTypes = Value
   }
 }
