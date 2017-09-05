c512811e600285b8702ed2d0c5b17f059a08fe40
jmglenn <jglenn@ccri.com>
2017-09-05 17:28:52 -0400

    GEOMESA-1986,GEOMESA-1987,GEOMESA-1993 IDL Safe TubeSelect, Interpolated GapFill, Binning Fix (#1665)
    * Added new interpolated gap fill, with unit test
    * Handle single object lines in tube select
    * Can specify zero bins in order to get every feature in it's own bin
    
    Signed-off-by: jmglenn <jeff.glenn@ccri.com>

---
+++ geomesa-utils/src/test/scala/org/locationtech/geomesa/utils/geotools/GeometryUtilsTest.scala
@@ -0,0 +1,34 @@
+/***********************************************************************
+ * Copyright (c) 2013-2017 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.locationtech.geomesa.utils.geotools
+
+import com.vividsolutions.jts.geom.Coordinate
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class GeometryUtilsTest extends Specification {
+
+  "Geometry Utils" should {
+
+    "calculate intercept of anti-meridian" >> {
+      val p1 = new Coordinate(179, 5, 0)
+      val p2 = new Coordinate(-179, 7, 0)
+      (GeometryUtils.calcIDLIntercept(p1, p2).toInt, GeometryUtils.calcIDLIntercept(p2,p1).toInt) mustEqual (6, 6)
+    }
+
+    "calculate intercept of arbitrary longitude" >> {
+      val p1 = new Coordinate(170, 5, 0)
+      val p2 = new Coordinate(172, 7, 0)
+      (GeometryUtils.calcCrossLat(p1, p2, 171).toInt, GeometryUtils.calcCrossLat(p2, p1, 171).toInt) mustEqual (6, 6)
+    }
+
+  }
+}
