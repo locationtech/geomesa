5280c7b09b1eafcfc470bfb8a873a96c4c0f69ca
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2017-09-21 09:20:19 -0400

    adding kafka 10 versions in install script, adding nexus server variable

--- geomesa-kafka/geomesa-kafka-tools/bin/install-kafka.sh
+++ geomesa-kafka/geomesa-kafka-tools/bin/install-kafka.sh
@@ -7,18 +7,27 @@
 # http://www.opensource.org/licenses/apache2.0.php.
 #
 
+maven_server="https://search.maven.org/remotecontent?filepath="
+
+zookeeper_version="%%zookeeper.version.recommended%%"
+
+# kafka 9 versions
 kafka_version="%%kafka.version%%"
 zkclient_version="%%zkclient.version%%"
-zookeeper_version="%%zookeeper.version%%"
 jopt_version="%%jopt.version%%"
 
+# kafka 10 versions
+# kafka_version="0.10.2.1"
+# zkclient_version="0.10"
+# jopt_version="4.9"
+
 declare -a urls=(
-  "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka_2.11/$kafka_version/kafka_2.11-$kafka_version.jar"
-  "https://search.maven.org/remotecontent?filepath=org/apache/kafka/kafka-clients/$kafka_version/kafka-clients-$kafka_version.jar"
-  "https://search.maven.org/remotecontent?filepath=org/apache/zookeeper/zookeeper/$zookeeper_version/zookeeper-$zookeeper_version.jar"
-  "https://search.maven.org/remotecontent?filepath=com/101tec/zkclient/$zkclient_version/zkclient-$zkclient_version.jar"
-  "https://search.maven.org/remotecontent?filepath=net/sf/jopt-simple/jopt-simple/$jopt_version/jopt-simple-$jopt_version.jar"
-  "https://search.maven.org/remotecontent?filepath=com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
+  "${maven_server}org/apache/kafka/kafka_2.11/$kafka_version/kafka_2.11-$kafka_version.jar"
+  "${maven_server}org/apache/kafka/kafka-clients/$kafka_version/kafka-clients-$kafka_version.jar"
+  "${maven_server}org/apache/zookeeper/zookeeper/$zookeeper_version/zookeeper-$zookeeper_version.jar"
+  "${maven_server}com/101tec/zkclient/$zkclient_version/zkclient-$zkclient_version.jar"
+  "${maven_server}net/sf/jopt-simple/jopt-simple/$jopt_version/jopt-simple-$jopt_version.jar"
+  "${maven_server}com/yammer/metrics/metrics-core/2.2.0/metrics-core-2.2.0.jar"
 )
 
 if [[ (-z "$1") ]]; then
