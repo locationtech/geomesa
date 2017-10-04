59a0f1f763e53131cb5aedcecb0c9f758acf9c1d
Emilio <elahrvivaz@ccri.com>
2017-10-04 15:58:13 -0400

    spacing

--- geomesa-kafka/geomesa-kafka-datastore/geomesa-kafka-08-datastore/src/main/scala/org/locationtech/geomesa/kafka08/LiveKafkaConsumerFeatureSource.scala
+++ geomesa-kafka/geomesa-kafka-datastore/geomesa-kafka-08-datastore/src/main/scala/org/locationtech/geomesa/kafka08/LiveKafkaConsumerFeatureSource.scala
@@ -39,7 +39,7 @@ class LiveKafkaConsumerFeatureSource(e: ContentEntry,
                                     (implicit ticker: Ticker = Ticker.systemTicker())
   extends KafkaConsumerFeatureSource(e, sft, q, monitor) with Runnable with Closeable with LazyLogging {
 
-  private [kafka08] val featureCache: LiveFeatureCache = if (useCQCache) {
+  private[kafka08] val featureCache: LiveFeatureCache = if (useCQCache) {
     new LiveFeatureCacheCQEngine(sft, expirationPeriod)
   } else {
     new LiveFeatureCacheGuava(sft, expirationPeriod, consistencyCheck)
