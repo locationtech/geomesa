20ec7cc0b2343713eb6354fc2f9d5c84a214bb96
Jim Hughes <jnh5y@ccri.com>
2017-10-09 17:53:47 -0400

    Initial dateoffset WPS.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

---
+++ geomesa-process/geomesa-process-vector/src/main/scala/org/locationtech/geomesa/process/transform/DateOffsetProcess.scala
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * Copyright (c) 2013-2017 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.locationtech.geomesa.process.transform
+
+import java.time.{Period, ZoneId}
+import java.util.Date
+
+import org.geotools.data.collection.ListFeatureCollection
+import org.geotools.data.simple.SimpleFeatureCollection
+import org.geotools.process.ProcessException
+import org.geotools.process.factory.{DescribeParameter, DescribeProcess, DescribeResult}
+import org.locationtech.geomesa.process.GeoMesaProcess
+import org.locationtech.geomesa.utils.collection.CloseableIterator
+import org.opengis.feature.simple.SimpleFeature
+
+import scala.collection.JavaConversions._
+
+@DescribeProcess(
+  title = "Date Offset Process",
+  description = "Modifies the specified date field in a feature collection by an input time period."
+)
+class DateOffsetProcess extends GeoMesaProcess {
+
+  @throws(classOf[ProcessException])
+  @DescribeResult(name = "result", description = "Output collection")
+  def execute(@DescribeParameter(name = "data", description = "Input features")
+              obsFeatures: SimpleFeatureCollection,
+              @DescribeParameter(name = "dateField", description = "The date attribute to modify")
+              dateField: String,
+              @DescribeParameter(name = "timeOffset", description = "Time offset (e.g. P1D)")
+              timeOffset: String): SimpleFeatureCollection = {
+
+    val iter = CloseableIterator(obsFeatures.features())
+    // TODO: Throw exception if dateField is not in SFT.
+    val period = Period.parse(timeOffset)
+
+    val iter2: List[SimpleFeature] = iter.map { sf =>
+
+      val dtg = sf.getAttribute(dateField).asInstanceOf[Date]
+      val ld = dtg.toInstant.atZone(ZoneId.systemDefault()).toLocalDate
+      val ld2 = ld.plus(period)
+      val newDtg = Date.from(ld2.atStartOfDay(ZoneId.systemDefault()).toInstant)
+
+      sf.setAttribute(dateField, newDtg)
+      sf
+    }.toList
+
+    new ListFeatureCollection(obsFeatures.getSchema, iter2)
+  }
+}
---
+++ geomesa-process/geomesa-process-vector/src/test/scala/org/locationtech/geomesa/process/transform/DateOffsetProcessTest.scala
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * Copyright (c) 2013-2017 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.locationtech.geomesa.process.transform
+
+import java.util.Date
+
+import org.geotools.data.collection.ListFeatureCollection
+import org.joda.time.DateTime
+import org.junit.runner.RunWith
+import org.locationtech.geomesa.features.ScalaSimpleFeature
+import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+import scala.collection.JavaConversions._
+import scala.util.Random
+
+@RunWith(classOf[JUnitRunner])
+class DateOffsetProcessTest extends Specification {
+
+  val sft = SimpleFeatureTypes.createType("arrow", "name:String,dtg:Date,*geom:Point:srid=4326")
+
+  def features = (0 until 10).map { i =>
+    ScalaSimpleFeature.create(sft, s"0$i", s"name${i % 2}", s"2017-02-20T00:00:0$i.000Z", s"POINT(40 ${50 + i})")
+  }
+
+  val process = new DateOffsetProcess
+
+  "DateOffSet Process" should {
+    "move a feature collection back in time one day" in {
+      val collection = new ListFeatureCollection(sft, new Random(-1L).shuffle(features))
+
+
+      val result = process.execute(collection, "dtg", "P-1D")
+      val date = result.features().next.getAttribute("dtg").asInstanceOf[Date]
+      val dt = new DateTime(date)
+      dt.withSecondOfMinute(0) mustEqual new DateTime("2017-02-19T00:00:00.000Z")
+    }
+
+    "move a feature collection forward in time one day" in {
+      val collection = new ListFeatureCollection(sft, new Random(-1L).shuffle(features))
+
+
+      val result = process.execute(collection, "dtg", "P1D")
+      val date = result.features().next.getAttribute("dtg").asInstanceOf[Date]
+      val dt = new DateTime(date)
+      dt.withSecondOfMinute(0) mustEqual new DateTime("2017-02-21T00:00:00.000Z")
+    }
+  }
+}
