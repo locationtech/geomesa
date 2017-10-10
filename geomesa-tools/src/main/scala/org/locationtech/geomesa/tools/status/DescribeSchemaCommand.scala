9825af2bf2688eab0ec186dd2335ccca5d78b61d
Emilio <elahrvivaz@ccri.com>
2017-10-10 16:08:42 -0400

    GEOMESA-2055 Adding dictionary output to arrow tools (#1736)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-tools/src/main/scala/org/locationtech/geomesa/tools/status/DescribeSchemaCommand.scala
+++ geomesa-tools/src/main/scala/org/locationtech/geomesa/tools/status/DescribeSchemaCommand.scala
@@ -12,42 +12,57 @@ import com.beust.jcommander.ParameterException
 import org.geotools.data.DataStore
 import org.locationtech.geomesa.tools.{Command, DataStoreCommand, TypeNameParam}
 import org.locationtech.geomesa.utils.stats.IndexCoverage
+import org.opengis.feature.simple.SimpleFeatureType
 
 import scala.collection.mutable.ArrayBuffer
-import scala.util.control.NonFatal
 
 trait DescribeSchemaCommand[DS <: DataStore] extends DataStoreCommand[DS] {
 
   override val name: String = "describe-schema"
-  override def params: TypeNameParam
 
-  override def execute(): Unit = withDataStore(describe)
+  protected def hasSpatialIndex: Boolean = true
+  protected def hasSpatioTemporalIndex: Boolean = true
+  protected def hasAttributeIndex: Boolean = true
 
-  protected def describe(ds: DS): Unit = {
+  override def execute(): Unit = withDataStore { ds =>
+    val sft = getSchema(ds)
+    if (sft == null) {
+      val msg = params match {
+        case p: TypeNameParam => s"Feature '${p.featureName}' not found"
+        case _ => s"Feature type not found"
+      }
+      throw new ParameterException(msg)
+    }
+    Command.user.info(s"Describing attributes of feature '${sft.getTypeName}'")
+    describe(ds, sft, Command.output.info)
+  }
+
+  protected def getSchema(ds: DS): SimpleFeatureType = params match {
+    case p: TypeNameParam => ds.getSchema(p.featureName)
+  }
+
+  protected def describe(ds: DS, sft: SimpleFeatureType, output: (String) => Unit): Unit = {
     import org.locationtech.geomesa.utils.geotools.RichAttributeDescriptors.RichAttributeDescriptor
     import org.locationtech.geomesa.utils.geotools.RichSimpleFeatureType._
 
     import scala.collection.JavaConversions._
 
-    Command.user.info(s"Describing attributes of feature '${params.featureName}'")
-
-    val sft = ds.getSchema(params.featureName)
-    if (sft == null) {
-      throw new ParameterException(s"Feature '${params.featureName}' not found")
-    }
-
     val namesAndDescriptions = sft.getAttributeDescriptors.map { descriptor =>
       val name = descriptor.getLocalName
       val description = ArrayBuffer.empty[String]
       if (descriptor == sft.getGeometryDescriptor) {
-        description.append("(Spatially indexed)")
-      } else if (sft.getDtgField.exists(_ == name)) {
+        if (hasSpatialIndex) {
+          description.append("(Spatially indexed)")
+        }
+      } else if (sft.getDtgField.contains(name) && hasSpatioTemporalIndex) {
         description.append("(Spatio-temporally indexed)")
       }
-      descriptor.getIndexCoverage() match {
-        case IndexCoverage.JOIN => description.append("(Attribute indexed - join)")
-        case IndexCoverage.FULL => description.append("(Attribute indexed - full)")
-        case _ => // no-op
+      if (hasAttributeIndex) {
+        descriptor.getIndexCoverage() match {
+          case IndexCoverage.JOIN => description.append("(Attribute indexed - join)")
+          case IndexCoverage.FULL => description.append("(Attribute indexed)")
+          case _ => // no-op
+        }
       }
       Option(descriptor.getDefaultValue).foreach(v => description.append(s"Default Value: $v"))
       (name, descriptor.getType.getBinding.getSimpleName, description)
@@ -56,13 +71,13 @@ trait DescribeSchemaCommand[DS <: DataStore] extends DataStoreCommand[DS] {
     val maxName = namesAndDescriptions.map(_._1.length).max
     val maxType = namesAndDescriptions.map(_._2.length).max
     namesAndDescriptions.foreach { case (n, t, d) =>
-      Command.output.info(s"${n.padTo(maxName, ' ')} | ${t.padTo(maxType, ' ')} ${d.mkString(" ")}")
+      output(s"${n.padTo(maxName, ' ')} | ${t.padTo(maxType, ' ')} ${d.mkString(" ")}")
     }
 
     val userData = sft.getUserData
     if (!userData.isEmpty) {
       import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes.Configs.KEYWORDS_KEY
-      Command.output.info("\nUser data:")
+      output("\nUser data:")
       val namesAndValues = userData.map { case (k, v) =>
         if (k == KEYWORDS_KEY) {
           (KEYWORDS_KEY, sft.getKeywords.mkString("[\"", "\", \"", "\"]"))
@@ -72,7 +87,7 @@ trait DescribeSchemaCommand[DS <: DataStore] extends DataStoreCommand[DS] {
       }
       val maxName = namesAndValues.map(_._1.length).max
       namesAndValues.toSeq.sortBy(_._1).foreach { case (n, v) =>
-        Command.output.info(s"  ${n.padTo(maxName, ' ')} | $v")
+        output(s"  ${n.padTo(maxName, ' ')} | $v")
       }
     }
   }
