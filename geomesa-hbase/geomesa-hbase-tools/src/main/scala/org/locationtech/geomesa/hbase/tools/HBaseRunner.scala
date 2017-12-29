5abdf7249cf04aa57af2ccc7d86b4e99aea23391
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2017-12-29 11:00:21 -0500

    bulk load command

--- geomesa-hbase/geomesa-hbase-tools/src/main/scala/org/locationtech/geomesa/hbase/tools/HBaseRunner.scala
+++ geomesa-hbase/geomesa-hbase-tools/src/main/scala/org/locationtech/geomesa/hbase/tools/HBaseRunner.scala
@@ -11,7 +11,7 @@ package org.locationtech.geomesa.hbase.tools
 import com.beust.jcommander.JCommander
 import org.locationtech.geomesa.hbase.tools.data._
 import org.locationtech.geomesa.hbase.tools.export.HBaseExportCommand
-import org.locationtech.geomesa.hbase.tools.ingest.HBaseIngestCommand
+import org.locationtech.geomesa.hbase.tools.ingest.{HBaseBulkLoadCommand, HBaseIngestCommand}
 import org.locationtech.geomesa.hbase.tools.stats._
 import org.locationtech.geomesa.hbase.tools.status._
 import org.locationtech.geomesa.tools.export.GenerateAvroSchemaCommand
@@ -23,6 +23,7 @@ object HBaseRunner extends Runner {
   override val name: String = "geomesa-hbase"
 
   override def createCommands(jc: JCommander): Seq[Command] = Seq(
+    new HBaseBulkLoadCommand,
     new HBaseCreateSchemaCommand,
     new HBaseDeleteCatalogCommand,
     new HBaseDeleteFeaturesCommand,
