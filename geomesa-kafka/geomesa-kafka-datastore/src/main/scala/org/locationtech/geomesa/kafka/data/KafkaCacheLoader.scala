bdead50c96bfe2ef693bcf7df6b984866de6de9b
Emilio <elahrvivaz@ccri.com>
2018-03-09 14:12:42 -0500

    GEOMESA-2204 Fixing Kafka consumer sleep while more data to read (#1886)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-kafka/geomesa-kafka-datastore/src/main/scala/org/locationtech/geomesa/kafka/data/KafkaCacheLoader.scala
+++ geomesa-kafka/geomesa-kafka-datastore/src/main/scala/org/locationtech/geomesa/kafka/data/KafkaCacheLoader.scala
@@ -10,16 +10,15 @@ package org.locationtech.geomesa.kafka.data
 
 import java.io.Closeable
 import java.util.Collections
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.{ConcurrentHashMap, Executors, TimeUnit}
+import java.util.concurrent.ConcurrentHashMap
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.kafka.clients.consumer.Consumer
-import org.apache.kafka.common.errors.WakeupException
+import org.apache.kafka.clients.consumer.{Consumer, ConsumerRecord}
 import org.geotools.data.simple.SimpleFeatureSource
 import org.geotools.data.{FeatureEvent, FeatureListener}
+import org.locationtech.geomesa.kafka.consumer.ThreadedConsumer
 import org.locationtech.geomesa.kafka.index.KafkaFeatureCache
-import org.locationtech.geomesa.kafka.utils.GeoMessage.{Clear, Change, Delete}
+import org.locationtech.geomesa.kafka.utils.GeoMessage.{Change, Clear, Delete}
 import org.locationtech.geomesa.kafka.utils.{GeoMessageSerializer, KafkaFeatureEvent}
 import org.locationtech.geomesa.utils.conf.GeoMesaSystemProperties.SystemProperty
 import org.opengis.feature.simple.SimpleFeatureType
@@ -86,75 +85,31 @@ object KafkaCacheLoader {
 
   class KafkaCacheLoaderImpl(sft: SimpleFeatureType,
                              val cache: KafkaFeatureCache,
-                             consumers: Seq[Consumer[Array[Byte], Array[Byte]]])
-      extends KafkaCacheLoader with LazyLogging {
+                             override protected val consumers: Seq[Consumer[Array[Byte], Array[Byte]]])
+      extends ThreadedConsumer with KafkaCacheLoader {
 
-    private val running = new AtomicInteger(0)
+    override protected val topic: String = KafkaDataStore.topic(sft)
+    override protected val frequency: Long =
+      SystemProperty("geomesa.kafka.load.interval").toDuration.map(_.toMillis).getOrElse(100L)
 
     private val serializer = new GeoMessageSerializer(sft)
 
-    private val topic = KafkaDataStore.topic(sft)
-    private val frequency = SystemProperty("geomesa.kafka.load.interval").toDuration.map(_.toMillis).getOrElse(100L)
-
-    private val executor = Executors.newScheduledThreadPool(consumers.length)
-
-    private val schedules = consumers.zipWithIndex.map { case (c, i) =>
-      executor.scheduleWithFixedDelay(new ConsumerRunnable(c, i), frequency, frequency, TimeUnit.MILLISECONDS)
-    }
-
-    logger.debug(s"Started ${consumers.length} consumer(s) on topic $topic")
-
     override def close(): Unit = {
-      consumers.foreach(_.wakeup())
-      schedules.foreach(_.cancel(true))
-      executor.shutdownNow()
-      // ensure run has finished so that we don't get ConcurrentAccessExceptions closing the consumer
-      while (running.get > 0) {
-        Thread.sleep(10)
+      try {
+        super.close()
+      } finally {
+        cache.close()
       }
-      consumers.foreach(_.close())
-      cache.close()
-      executor.awaitTermination(1, TimeUnit.SECONDS)
     }
 
-    class ConsumerRunnable(val consumer: Consumer[Array[Byte], Array[Byte]], i: Int) extends Runnable {
-
-      private var errorCount = 0
-
-      override def run(): Unit = {
-        running.getAndIncrement()
-        try {
-          val result = consumer.poll(0)
-          logger.trace(s"Consumer poll received ${result.count()} records from topic $topic")
-          val records = result.iterator
-          while (records.hasNext) {
-            val record = records.next()
-            errorCount = 0 // reset error count
-            val message = serializer.deserialize(record.key, record.value)
-            logger.debug(s"Consumed message [$topic:${record.partition}:${record.offset}] $message")
-            message match {
-              case m: Change => fireEvent(m); cache.put(m.feature)
-              case m: Delete => fireEvent(m); cache.remove(m.id)
-              case m: Clear  => fireEvent(m); cache.clear()
-              case m => throw new IllegalArgumentException(s"Unknown message: $m")
-            }
-          }
-          logger.trace(s"Consumer finished processing ${result.count()} records from topic $topic")
-          // we commit the offsets so that the next poll doesn't return the same records
-          consumer.commitSync()
-          logger.trace(s"Consumer committed offsets")
-        } catch {
-          case _: WakeupException | _: InterruptedException => // ignore
-          case NonFatal(e) =>
-            logger.warn(s"Error receiving message from topic $topic:", e)
-            errorCount += 1
-            if (errorCount < 300) { Thread.sleep(1000) } else {
-              logger.error("Shutting down due to too many errors")
-              schedules(i).cancel(false)
-            }
-        } finally {
-          running.decrementAndGet()
-        }
+    override protected def consume(record: ConsumerRecord[Array[Byte], Array[Byte]]): Unit = {
+      val message = serializer.deserialize(record.key, record.value)
+      logger.trace(s"Consumed message [$topic:${record.partition}:${record.offset}] $message")
+      message match {
+        case m: Change => fireEvent(m); cache.put(m.feature)
+        case m: Delete => fireEvent(m); cache.remove(m.id)
+        case m: Clear  => fireEvent(m); cache.clear()
+        case m => throw new IllegalArgumentException(s"Unknown message: $m")
       }
     }
   }
