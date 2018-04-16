5f7fc7f7e8a231edf3ba05cfcd88d4e5921d0480
Emilio <elahrvivaz@ccri.com>
2018-04-16 11:46:56 -0400

    GEOMESA-2259 Bigtable - fixing tools classpath/parameters (#1934)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

---
+++ geomesa-bigtable/geomesa-bigtable-tools/src/main/scala/org/locationtech/geomesa/bigtable/tools/BigtableDataStoreCommand.scala
@@ -0,0 +1,26 @@
+/***********************************************************************
+ * Copyright (c) 2013-2018 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.locationtech.geomesa.bigtable.tools
+
+import org.locationtech.geomesa.bigtable.data.BigtableDataStoreFactory
+import org.locationtech.geomesa.hbase.data.HBaseDataStoreParams
+import org.locationtech.geomesa.hbase.tools.HBaseDataStoreCommand
+
+trait BigtableDataStoreCommand extends HBaseDataStoreCommand {
+
+  override def connection: Map[String, String] = {
+    Map(
+      BigtableDataStoreFactory.BigtableCatalogParam.getName -> params.catalog,
+      HBaseDataStoreParams.ZookeeperParam.getName           -> params.zookeepers,
+      HBaseDataStoreParams.RemoteFilteringParam.getName     -> "false",
+      HBaseDataStoreParams.EnableSecurityParam.getName      -> params.secure.toString,
+      HBaseDataStoreParams.AuthsParam.getName               -> params.auths
+    ).filter(_._2 != null)
+  }
+}
