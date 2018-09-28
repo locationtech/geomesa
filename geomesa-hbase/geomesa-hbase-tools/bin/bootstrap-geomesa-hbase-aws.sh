e1157f20ad948682e512f87132e331ae47897487
Austin Heyne <aheyne@ccri.com>
2018-09-28 14:02:46 -0400

    GEOMESA-2422 Fix HBase AWS Bootstrap Upload Path for S3
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

--- geomesa-hbase/geomesa-hbase-tools/bin/bootstrap-geomesa-hbase-aws.sh
+++ geomesa-hbase/geomesa-hbase-tools/bin/bootstrap-geomesa-hbase-aws.sh
@@ -58,8 +58,8 @@ echo "# Auto-registration for geomesa coprocessors ${NL}export CUSTOM_JAVA_OPTS=
 
 # Deploy the GeoMesa HBase distributed runtime to the HBase root directory
 if [[ "$ROOTDIR" = s3* ]]; then
-  aws s3 cp /opt/geomesa/dist/hbase/$DISTRIBUTED_JAR_NAME $ROOTDIR/lib/ && \
-  echo "Installed GeoMesa distributed runtime to $ROOTDIR/lib/"
+  aws s3 cp /opt/geomesa/dist/hbase/$DISTRIBUTED_JAR_NAME ${ROOTDIR}lib/ && \
+  echo "Installed GeoMesa distributed runtime to ${ROOTDIR}lib/"
 elif [[ "$ROOTDIR" = hdfs* ]]; then
   local libdir="${ROOTDIR}/lib"
   (sudo -u $GMUSER hadoop fs -test -d $libdir || sudo -u $GMUSER hadoop fs -mkdir $libdir) && \
