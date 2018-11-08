13955092d6190b71d0569c9d178e7f71de903ecb
Emilio <elahrvivaz@ccri.com>
2018-11-08 11:35:40 -0500

    GEOMESA-2459 Remove guava dependency in ResourceLoadingCache (#2098)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-convert/geomesa-convert-common/src/main/scala/org/locationtech/geomesa/convert/EnrichmentCache.scala
+++ geomesa-convert/geomesa-convert-common/src/main/scala/org/locationtech/geomesa/convert/EnrichmentCache.scala
@@ -8,12 +8,10 @@
 
 package org.locationtech.geomesa.convert
 
-import java.io.Closeable
+import java.io.{Closeable, InputStreamReader}
 import java.nio.charset.StandardCharsets
 import java.util.ServiceLoader
 
-import com.google.common.collect.Maps
-import com.google.common.io.Resources
 import com.typesafe.config.Config
 import org.apache.commons.csv.{CSVFormat, CSVParser}
 import org.locationtech.geomesa.utils.io.WithClose
@@ -38,14 +36,14 @@ object EnrichmentCache {
 }
 
 // For testing purposes
-class SimpleEnrichmentCache(val cache: java.util.Map[String, java.util.HashMap[String, AnyRef]] = Maps.newHashMap[String, java.util.HashMap[String, AnyRef]]())
+class SimpleEnrichmentCache(val cache: java.util.Map[String, java.util.HashMap[String, AnyRef]] = new java.util.HashMap[String, java.util.HashMap[String, AnyRef]]())
     extends EnrichmentCache {
 
   override def get(args: Array[String]): Any = Option(cache.get(args(0))).map(_.get(args(1))).orNull
 
   override def put(args: Array[String], value: Any): Unit = {
     import scala.collection.JavaConversions._
-    cache.getOrElseUpdate(args(0), Maps.newHashMap()).put(args(1), value.asInstanceOf[AnyRef])
+    cache.getOrElseUpdate(args(0), new java.util.HashMap[String, AnyRef]()).put(args(1), value.asInstanceOf[AnyRef])
   }
 
   override def clear(): Unit = cache.clear()
@@ -61,10 +59,17 @@ class SimpleEnrichmentCacheFactory extends EnrichmentCacheFactory {
 
 class ResourceLoadingCache(path: String, idField: String, headers: Seq[String]) extends EnrichmentCache {
   import scala.collection.JavaConversions._
-  private val file = Resources.asCharSource(Resources.getResource(path), StandardCharsets.UTF_8)
-  private val format = CSVFormat.DEFAULT.withHeader(headers: _*)
-  private val data = WithClose(new CSVParser(file.openBufferedStream(), format)) { reader =>
-    reader.getRecords.map(rec => rec.get(idField) -> rec.toMap).toMap
+
+  private val data = {
+    val stream = getClass.getClassLoader.getResourceAsStream(path)
+    if (stream == null) {
+      throw new IllegalArgumentException(s"Could not load the resources at '$path'")
+    }
+    val reader = new InputStreamReader(stream, StandardCharsets.UTF_8)
+    val format = CSVFormat.DEFAULT.withHeader(headers: _*)
+    WithClose(new CSVParser(reader, format)) { reader =>
+      reader.getRecords.map(rec => rec.get(idField) -> rec.toMap).toMap
+    }
   }
 
   override def get(args: Array[String]): Any = data.get(args(0)).map(_.get(args(1))).orNull
