243665302d7667bede8898a326c57892cac5f5ed
Tom Kunicki <tom@kunicki.net>
2019-01-17 11:54:58 -0500

    GEOMESA-2521 Handle null typeName in FeatureSource queries (#2171)
    * Support queries from the geoserver querylayer extension
    
    Signed-off-by: Tom Kunicki <tom.kunicki@ccri.com>

--- geomesa-kafka/geomesa-kafka-datastore/src/test/scala/org/locationtech/geomesa/kafka/data/KafkaDataStoreTest.scala
+++ geomesa-kafka/geomesa-kafka-datastore/src/test/scala/org/locationtech/geomesa/kafka/data/KafkaDataStoreTest.scala
@@ -421,6 +421,18 @@ class KafkaDataStoreTest extends Specification with Mockito with LazyLogging {
     }
   }
 
+  "KafkaFeatureSource" should {
+    "handle Query instances with null TypeName (GeoServer querylayer extension implementation nuance)" >> {
+      val (p, c, sft) = createStorePair("querylayer")
+      p.createSchema(sft)
+      val fs = c.getFeatureSource(sft.getTypeName)
+      val q = new Query(null, Filter.INCLUDE)
+      def check = fs.getFeatures(q).features().close()
+      // induce issue
+      check must not throwA[NullPointerException]()
+    }
+  }
+
   step {
     logger.info("Stopping embedded kafka/zk")
     kafka.close()
