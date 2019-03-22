5788bd5773acf90231d7982228ce8bba8f2b656d
Emilio <elahrvivaz@ccri.com>
2019-03-22 19:37:14 -0400

    GEOMESA-2568 Ensure cancelled scans terminate all threads (#2247)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-kudu/geomesa-kudu-datastore/src/main/scala/org/locationtech/geomesa/kudu/utils/KuduBatchScan.scala
+++ geomesa-kudu/geomesa-kudu-datastore/src/main/scala/org/locationtech/geomesa/kudu/utils/KuduBatchScan.scala
@@ -14,6 +14,9 @@ import org.apache.kudu.Schema
 import org.apache.kudu.client._
 import org.apache.kudu.util.Slice
 import org.locationtech.geomesa.index.utils.AbstractBatchScan
+import org.locationtech.geomesa.kudu.utils.KuduBatchScan.KuduAbstractBatchScan
+import org.locationtech.geomesa.utils.collection.CloseableIterator
+
 import scala.collection.JavaConverters._
 
 /**
@@ -30,19 +33,19 @@ import scala.collection.JavaConverters._
   * @param threads number of threads
   * @param buffer size of output buffer
   */
-class KuduBatchScan(client: KuduClient, table: String, columns: Seq[String],
-                    ranges: Seq[(Option[PartialRow], Option[PartialRow])], predicates: Seq[KuduPredicate],
-                    threads: Int, buffer: Int) extends {
-      // use early initialization to ensure table is open before scans kick off
-      private val kuduTable = client.openTable(table)
-      private val cols = columns.map(kuduTable.getSchema.getColumnIndex).asJava.asInstanceOf[java.util.List[Integer]]
-    } with AbstractBatchScan[(Option[PartialRow], Option[PartialRow]), RowResultIterator](ranges, threads, buffer) {
-
-  override protected def singletonSentinel: RowResultIterator = KuduBatchScan.Sentinel
+private class KuduBatchScan(
+    client: KuduClient,
+    table: KuduTable,
+    columns: java.util.List[Integer],
+    ranges: Seq[(Option[PartialRow], Option[PartialRow])],
+    predicates: Seq[KuduPredicate],
+    threads: Int,
+    buffer: Int
+  ) extends KuduAbstractBatchScan(ranges, threads, buffer, KuduBatchScan.Sentinel) {
 
   override protected def scan(range: (Option[PartialRow], Option[PartialRow]),
                               out: BlockingQueue[RowResultIterator]): Unit = {
-    val builder = client.newScannerBuilder(kuduTable).setProjectedColumnIndexes(cols)
+    val builder = client.newScannerBuilder(table).setProjectedColumnIndexes(columns)
     range._1.foreach(builder.lowerBound)
     range._2.foreach(builder.exclusiveUpperBound)
     predicates.foreach(builder.addPredicate)
@@ -58,6 +61,9 @@ class KuduBatchScan(client: KuduClient, table: String, columns: Seq[String],
 }
 
 object KuduBatchScan {
+
+  type KuduAbstractBatchScan = AbstractBatchScan[(Option[PartialRow], Option[PartialRow]), RowResultIterator]
+
   private val Sentinel: RowResultIterator = {
     // use reflection to access the private constructor
     // private RowResultIterator(long ellapsedMillis, String tsUUID, Schema schema, int numRows, Slice bs, Slice indirectBs)
@@ -65,5 +71,17 @@ object KuduBatchScan {
       classOf[Schema], classOf[Int], classOf[Slice], classOf[Slice])
     constructor.setAccessible(true)
     constructor.newInstance(Long.box(0L), null, null, Int.box(0), null, null)
+  }
+
+  def apply(
+      client: KuduClient,
+      table: String,
+      columns: Seq[String],
+      ranges: Seq[(Option[PartialRow], Option[PartialRow])],
+      predicates: Seq[KuduPredicate],
+      threads: Int): CloseableIterator[RowResultIterator] = {
+    val kuduTable = client.openTable(table)
+    val cols = columns.map(kuduTable.getSchema.getColumnIndex).asJava.asInstanceOf[java.util.List[Integer]]
+    new KuduBatchScan(client, kuduTable, cols, ranges, predicates, threads, 1000).start()
   }
 }
