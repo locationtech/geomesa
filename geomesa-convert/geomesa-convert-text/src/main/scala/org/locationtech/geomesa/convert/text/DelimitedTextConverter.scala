49edff038d40243df1b0e4f3d49faa9a114f5f3b
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2019-06-14 15:07:53 -0400

    GEOMESA-2650 CLI - Fix auto ingest for exported CSV files
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-convert/geomesa-convert-text/src/main/scala/org/locationtech/geomesa/convert/text/DelimitedTextConverter.scala
+++ geomesa-convert/geomesa-convert-text/src/main/scala/org/locationtech/geomesa/convert/text/DelimitedTextConverter.scala
@@ -202,7 +202,7 @@ object DelimitedTextConverter {
   )
 
   // check quoted before default - if values are quoted, we don't want the quotes to be captured as part of the value
-  private [text] val inferences = Seq(Formats.Tabs, Formats.Quoted, Formats.Default)
+  private [text] val inferences = Stream(Formats.Tabs, Formats.Quoted, Formats.Default)
 
   case class DelimitedTextConfig(
       `type`: String,
