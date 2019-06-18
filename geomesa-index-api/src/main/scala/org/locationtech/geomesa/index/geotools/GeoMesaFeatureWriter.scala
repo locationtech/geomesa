1aaf54be5832b35b4e883a0af46e667c5ae6e3ac
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2019-06-18 09:12:07 -0400

    GEOMESA-2654 ModifyFeatures can fail due to timestamp collision
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-index-api/src/main/scala/org/locationtech/geomesa/index/geotools/GeoMesaFeatureWriter.scala
+++ geomesa-index-api/src/main/scala/org/locationtech/geomesa/index/geotools/GeoMesaFeatureWriter.scala
@@ -351,6 +351,7 @@ object GeoMesaFeatureWriter extends LazyLogging {
       // comparison of feature ID and attributes - doesn't consider concrete class used
       if (!ScalaSimpleFeature.equalIdAndAttributes(live, original)) {
         removeFeature(original)
+        flush() // ensure the remove is written so we don't get key collisions with the insert
         writeFeature(live)
       }
       original = null
