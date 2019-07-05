3e3a4943e7a2d0d64cc31b92743565339d25d84a
Emilio <elahrvivaz@ccri.com>
2019-07-05 12:48:38 -0400

    GEOMESA-2656 Cache writers during local ingest (#2304)
    * Flush each writer every 20k features
    * Configurable via `geomesa.ingest.local.batch.size`
    * New scala-friendly object pool impl:
      `org.locationtech.geomesa.utils.io.CloseablePool`
    
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-index-api/src/test/scala/org/locationtech/geomesa/index/InMemoryMetadata.scala
+++ geomesa-index-api/src/test/scala/org/locationtech/geomesa/index/InMemoryMetadata.scala
@@ -31,6 +31,8 @@ class InMemoryMetadata[T] extends GeoMesaMetadata[T] {
     schemas.get(typeName).foreach(_.remove(key))
   }
 
+  override def remove(typeName: String, keys: Seq[String]): Unit = keys.foreach(remove(typeName, _))
+
   override def read(typeName: String, key: String, cache: Boolean): Option[T] = synchronized {
     schemas.get(typeName).flatMap(_.get(key))
   }
