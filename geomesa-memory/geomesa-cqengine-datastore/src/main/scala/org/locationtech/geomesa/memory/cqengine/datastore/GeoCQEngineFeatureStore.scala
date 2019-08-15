f18cb12d7873764593e192d0038c4f966ecf5a04
Emilio <elahrvivaz@ccri.com>
2019-08-15 11:01:07 -0400

    GEOMESA-2641 Return immutable features from CQEngineDataStore (#2341)
    * Clean up spark serialization classes and schema loading
    
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-memory/geomesa-cqengine-datastore/src/main/scala/org/locationtech/geomesa/memory/cqengine/datastore/GeoCQEngineFeatureStore.scala
+++ geomesa-memory/geomesa-cqengine-datastore/src/main/scala/org/locationtech/geomesa/memory/cqengine/datastore/GeoCQEngineFeatureStore.scala
@@ -8,6 +8,7 @@
 
 package org.locationtech.geomesa.memory.cqengine.datastore
 
+import java.util.UUID
 import java.util.concurrent.atomic.AtomicLong
 
 import org.geotools.data.collection.DelegateFeatureReader
@@ -15,40 +16,21 @@ import org.geotools.data.store.{ContentEntry, ContentFeatureStore}
 import org.geotools.data.{FeatureReader, FeatureWriter, Query, QueryCapabilities}
 import org.geotools.feature.collection.DelegateFeatureIterator
 import org.geotools.geometry.jts.ReferencedEnvelope
+import org.geotools.util.factory.Hints
 import org.locationtech.geomesa.features.ScalaSimpleFeature
+import org.locationtech.geomesa.features.ScalaSimpleFeature.ImmutableSimpleFeature
 import org.locationtech.geomesa.memory.cqengine.GeoCQEngine
+import org.locationtech.geomesa.memory.cqengine.datastore.GeoCQEngineFeatureStore.{GeoCQEngineFeatureWriter, GeoCQEngineQueryCapabilities}
 import org.locationtech.geomesa.utils.collection.SelfClosingIterator
 import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
 
 class GeoCQEngineFeatureStore(engine: GeoCQEngine, entry: ContentEntry, query: Query) extends
   ContentFeatureStore(entry, query) {
 
-  override def getWriterInternal(query: Query, flags: Int): FeatureWriter[SimpleFeatureType, SimpleFeature] = {
-         new FeatureWriter[SimpleFeatureType, SimpleFeature] {
-           private val tempFeatureIds = new AtomicLong(0)
+  import scala.collection.JavaConverters._
 
-           var currentFeature: SimpleFeature = null
-
-           override def remove(): Unit = ???
-
-           override def next(): SimpleFeature = {
-             currentFeature = new ScalaSimpleFeature(engine.sft, tempFeatureIds.getAndIncrement().toString)
-             currentFeature
-           }
-
-           override def hasNext: Boolean = false
-
-           override def write(): Unit = {
-             if (currentFeature != null) {
-               engine.update(currentFeature)
-             }
-           }
-
-           override def getFeatureType: SimpleFeatureType = engine.sft
-
-           override def close(): Unit = { }
-         }
-  }
+  override def getWriterInternal(query: Query, flags: Int): FeatureWriter[SimpleFeatureType, SimpleFeature] =
+    new GeoCQEngineFeatureWriter(engine)
 
   override def buildFeatureType(): SimpleFeatureType = engine.sft
 
@@ -57,17 +39,62 @@ class GeoCQEngineFeatureStore(engine: GeoCQEngine, entry: ContentEntry, query: Q
   override def getCountInternal(query: Query): Int =
     SelfClosingIterator(engine.query(query.getFilter)).length
 
-  override def getReaderInternal(query: Query): FeatureReader[SimpleFeatureType, SimpleFeature] = {
-    import scala.collection.JavaConversions._
-    new DelegateFeatureReader(engine.sft, new DelegateFeatureIterator(engine.query(query.getFilter)))
-  }
+  override def getReaderInternal(query: Query): FeatureReader[SimpleFeatureType, SimpleFeature] =
+    new DelegateFeatureReader(engine.sft, new DelegateFeatureIterator(engine.query(query.getFilter).asJava))
+
+  override def getQueryCapabilities: QueryCapabilities = GeoCQEngineQueryCapabilities
+
+  override def canEvent: Boolean = true
+}
+
+object GeoCQEngineFeatureStore {
 
   object GeoCQEngineQueryCapabilities extends QueryCapabilities {
     override def isUseProvidedFIDSupported = true
   }
 
-  override def getQueryCapabilities = GeoCQEngineQueryCapabilities
+  class GeoCQEngineFeatureWriter(engine: GeoCQEngine) extends FeatureWriter[SimpleFeatureType, SimpleFeature] {
 
-  override def canEvent: Boolean = true
+    private val tempFeatureIds = new AtomicLong(0)
+    private val currentFeature: ScalaSimpleFeature = new ScalaSimpleFeature(engine.sft, "")
+    private var queued = false
 
+    override def getFeatureType: SimpleFeatureType = engine.sft
+
+    override def hasNext: Boolean = false
+
+    override def next(): SimpleFeature = {
+      var i = 0
+      while (i < engine.sft.getAttributeCount) {
+        currentFeature.setAttributeNoConvert(i, null)
+        i += 1
+      }
+      currentFeature.getUserData.clear()
+      currentFeature.setId(tempFeatureIds.getAndIncrement().toString)
+      queued = true
+      currentFeature
+    }
+
+    override def write(): Unit = {
+      if (!queued) {
+        throw new IllegalStateException("Must call 'next' before 'write'")
+      }
+      val values = Array.tabulate(engine.sft.getAttributeCount)(currentFeature.getAttribute)
+      val id = if (currentFeature.getUserData.containsKey(Hints.PROVIDED_FID)) {
+        currentFeature.getUserData.get(Hints.PROVIDED_FID).toString
+      } else if (currentFeature.getUserData.containsKey(Hints.USE_PROVIDED_FID) &&
+          currentFeature.getUserData.get(Hints.USE_PROVIDED_FID).asInstanceOf[Boolean]) {
+        currentFeature.getID
+      } else {
+        UUID.randomUUID.toString
+      }
+      val userData = new java.util.HashMap[AnyRef, AnyRef](currentFeature.getUserData)
+      engine.update(new ImmutableSimpleFeature(engine.sft, id, values, userData))
+      queued = false
+    }
+
+    override def remove(): Unit = throw new NotImplementedError()
+
+    override def close(): Unit = {}
+  }
 }
