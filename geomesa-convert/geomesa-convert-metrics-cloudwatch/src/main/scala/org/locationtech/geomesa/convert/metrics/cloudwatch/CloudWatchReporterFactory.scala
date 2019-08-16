8846935334afb720b4a86c08730c8a76319ae2b6
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2019-08-16 08:41:42 -0400

    Updating cloudwatch metrics to use pureconfig

--- geomesa-convert/geomesa-convert-metrics-cloudwatch/src/main/scala/org/locationtech/geomesa/convert/metrics/cloudwatch/CloudWatchReporterFactory.scala
+++ geomesa-convert/geomesa-convert-metrics-cloudwatch/src/main/scala/org/locationtech/geomesa/convert/metrics/cloudwatch/CloudWatchReporterFactory.scala
@@ -9,28 +9,56 @@
 package org.locationtech.geomesa.convert.metrics.cloudwatch
 
 import java.util.concurrent.TimeUnit
-import org.locationtech.geomesa.convert2.metrics.ReporterFactory
+
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClientBuilder
 import com.codahale.metrics.{MetricRegistry, ScheduledReporter}
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
 import io.github.azagniotov.metrics.reporter.cloudwatch.CloudWatchReporter
+import org.locationtech.geomesa.convert2.metrics.ReporterFactory
+import pureconfig.ConfigReader
 
 class CloudWatchReporterFactory extends ReporterFactory {
+
+  import CloudWatchReporterFactory.{CloudWatchConfig, CloudWatchDefaults, Type}
 
   override def apply(
       conf: Config,
       registry: MetricRegistry,
       rates: TimeUnit,
       durations: TimeUnit): Option[ScheduledReporter] = {
-    if (!conf.hasPath("type") || !conf.getString("type").equalsIgnoreCase("cloudwatch")) { None } else {
-      val namespace = if(!conf.hasPath("namespace")) { "geomesa" } else { conf.getString("namespace") }
+    if (!conf.hasPath("type") || !conf.getString("type").equalsIgnoreCase(Type)) { None } else {
+      val cloudwatch = pureconfig.loadConfigOrThrow[CloudWatchConfig](conf.withFallback(CloudWatchDefaults))
+      val client = AmazonCloudWatchAsyncClientBuilder.defaultClient
       val reporter =
-        CloudWatchReporter.forRegistry(registry, AmazonCloudWatchAsyncClientBuilder.defaultClient, namespace)
+        CloudWatchReporter.forRegistry(registry, client, cloudwatch.namespace)
           .convertRatesTo(rates)
           .convertDurationsTo(durations)
-      if(conf.hasPath("raw-counts") && conf.getBoolean("raw-counts")) { reporter.withReportRawCountValue }
-      if(conf.hasPath("zero-values") && conf.getBoolean("zero-values")) { reporter.withZeroValuesSubmission }
+
+      if (cloudwatch.rawCounts) {
+        reporter.withReportRawCountValue()
+      }
+      if (cloudwatch.zeroValues) {
+        reporter.withZeroValuesSubmission()
+      }
+
       Some(reporter.build)
     }
   }
+}
+
+object CloudWatchReporterFactory {
+
+  import pureconfig.generic.semiauto._
+
+  val Type = "cloudwatch"
+
+  val CloudWatchDefaults: Config =
+    ConfigFactory.empty
+        .withValue("namespace", ConfigValueFactory.fromAnyRef("geomesa"))
+        .withValue("raw-counts", ConfigValueFactory.fromAnyRef(false))
+        .withValue("zero-values", ConfigValueFactory.fromAnyRef(false))
+
+  implicit val CloudWatchConfigReader: ConfigReader[CloudWatchConfig] = deriveReader[CloudWatchConfig]
+
+  case class CloudWatchConfig(namespace: String, rawCounts: Boolean, zeroValues: Boolean)
 }
