1d46573b275802361f31369903c084d6a3255674
Emilio <elahrvivaz@ccri.com>
2019-09-20 17:12:07 -0400

    GEOMESA-2707 Converters - check for nulls in date parsing methods (#2360)
    * Date parsing methods pass through nulls instead of throwing exceptions
      * Fix the anti-pattern of `try(date($0), null)`, which is expensive
    * `require` function added to preserve old behavior if desired
    
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-convert/geomesa-convert-common/src/main/scala/org/locationtech/geomesa/convert2/transforms/DateFunctionFactory.scala
+++ geomesa-convert/geomesa-convert-common/src/main/scala/org/locationtech/geomesa/convert2/transforms/DateFunctionFactory.scala
@@ -32,11 +32,21 @@ class DateFunctionFactory extends TransformerFunctionFactory {
   }
 
   private val millisToDate = TransformerFunction.pure("millisToDate") { args =>
-    new Date(args(0).asInstanceOf[Long])
+    args(0) match {
+      case null => null
+      case d: Long => new Date(d)
+      case d: Int  => new Date(d)
+      case d => throw new IllegalArgumentException(s"Invalid millisecond: $d")
+    }
   }
 
   private val secsToDate = TransformerFunction.pure("secsToDate") { args =>
-    new Date(args(0).asInstanceOf[Long] * 1000L)
+    args(0) match {
+      case null => null
+      case d: Long => new Date(d * 1000L)
+      case d: Int  => new Date(d * 1000L)
+      case d => throw new IllegalArgumentException(s"Invalid second: $d")
+    }
   }
 
   // yyyy-MM-dd'T'HH:mm:ss.SSSZZ (ZZ is time zone with colon)
@@ -151,8 +161,13 @@ object DateFunctionFactory {
 
   abstract class StandardDateParser(names: Seq[String]) extends NamedTransformerFunction(names, pure = true) {
     val format: DateTimeFormatter
-    override def eval(args: Array[Any])(implicit ctx: EvaluationContext): Any =
-      DateParsing.parseDate(args(0).toString, format)
+    override def eval(args: Array[Any])(implicit ctx: EvaluationContext): Any = {
+      args(0) match {
+        case null => null
+        case d: String => DateParsing.parseDate(d, format)
+        case d => DateParsing.parseDate(d.toString, format)
+      }
+    }
   }
 
   class CustomFormatDateParser extends NamedTransformerFunction(Seq("date"), pure = true) {
@@ -165,7 +180,11 @@ object DateFunctionFactory {
       if (format == null) {
         format = DateTimeFormatter.ofPattern(args(0).asInstanceOf[String]).withZone(ZoneOffset.UTC)
       }
-      DateParsing.parseDate(args(1).toString, format)
+      args(1) match {
+        case null => null
+        case d: String => DateParsing.parseDate(d, format)
+        case d => DateParsing.parseDate(d.toString, format)
+      }
     }
   }
 
