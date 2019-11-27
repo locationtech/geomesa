fc06719461094e8217a7d690462afbe7f34f0101
Emilio <elahrvivaz@ccri.com>
2019-11-27 13:32:01 -0500

    GEOMESA-2746 Stats - Include bounds on Histogram json (#2412)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-utils/src/main/scala/org/locationtech/geomesa/utils/stats/Histogram.scala
+++ geomesa-utils/src/main/scala/org/locationtech/geomesa/utils/stats/Histogram.scala
@@ -157,18 +157,14 @@ class Histogram[T](
 
   override def toJsonObject: Map[String, Any] = {
     val binSeq = Seq.tabulate(bins.length) { bin =>
-      val builder = ListMap.newBuilder[String, Any]
-      builder.sizeHint(if (bin == 0) { 4 } else { 3 })
-      builder += "index" -> bin
-      builder += "lower-bound" -> bounds(bin)._1
-      if (bin == 0) {
-        builder += "upper-bound" -> bounds(bin)._2
-      }
-      builder += "count" -> bins.counts(bin)
-      builder.result
+      ListMap[String, Any](
+        "index"       -> bin,
+        "lower-bound" -> bounds(bin)._1,
+        "upper-bound" -> bounds(bin)._2,
+        "count"       -> bins.counts(bin)
+      )
     }
     ListMap("lower-bound" -> bounds._1, "upper-bound" -> bounds._2, "bins" -> binSeq)
-
   }
 
   override def isEmpty: Boolean = bins.counts.forall(_ == 0)
