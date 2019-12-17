517bd96c1d713e17ecfa6dd865164aa8324d3f7b
Sotirios Roussos <sroussos@urbanit.gr>
2019-12-17 17:15:38 +0200

    Cassandra Spark fixes
    Signed-off-by: Sotirios Roussos <sroussos@urbanit.gr>

--- geomesa-cassandra/geomesa-cassandra-spark/src/main/scala/org/locationtech/geomesa/spark/cassandra/GeoMesaCassandraInputFormat.scala
+++ geomesa-cassandra/geomesa-cassandra-spark/src/main/scala/org/locationtech/geomesa/spark/cassandra/GeoMesaCassandraInputFormat.scala
@@ -1,11 +1,12 @@
-  /***********************************************************************
-    * Copyright (c) 2013-2019 Commonwealth Computer Research, Inc.
-    * All rights reserved. This program and the accompanying materials
-    * are made available under the terms of the Apache License, Version 2.0
-    * which accompanies this distribution and is available at
-    * http://www.opensource.org/licenses/apache2.0.php.
-    ***********************************************************************/
-  package org.locationtech.geomesa.spark.cassandra
+/***********************************************************************
+ * Copyright (c) 2013-2019 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.locationtech.geomesa.spark.cassandra
 
 import com.typesafe.scalalogging.LazyLogging
   import org.apache.cassandra.hadoop.ConfigHelper
