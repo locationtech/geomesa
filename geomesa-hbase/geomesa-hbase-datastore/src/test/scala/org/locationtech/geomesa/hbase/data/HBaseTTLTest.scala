45525eb0be2faf87089c89a936c16695604aba75
Alex Shen <shen.alexander@gmail.com>
2020-08-04 17:09:57 -0400

    TTL now uses preexisting geomesa.feature.expiry key
    Signed-off-by: Alex Shen <shen.alexander@gmail.com>

--- geomesa-hbase/geomesa-hbase-datastore/src/test/scala/org/locationtech/geomesa/hbase/data/HBaseTTLTest.scala
+++ geomesa-hbase/geomesa-hbase-datastore/src/test/scala/org/locationtech/geomesa/hbase/data/HBaseTTLTest.scala
@@ -1,11 +1,9 @@
 package org.locationtech.geomesa.hbase.data
 
-import java.io.File
-import java.util.{Collections, Date}
 import java.text.SimpleDateFormat
+import java.util.Date
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.hadoop.hbase.TableName
 import org.geotools.data._
 import org.geotools.data.collection.ListFeatureCollection
 import org.geotools.data.simple.SimpleFeatureStore
@@ -13,23 +11,11 @@ import org.geotools.filter.text.ecql.ECQL
 import org.geotools.util.factory.Hints
 import org.junit.runner.RunWith
 import org.locationtech.geomesa.features.ScalaSimpleFeature
-import org.locationtech.geomesa.filter.function.ProxyIdFunction
-import org.locationtech.geomesa.hbase.HBaseSystemProperties
 import org.locationtech.geomesa.hbase.data.HBaseDataStoreParams._
-import org.locationtech.geomesa.index.api.GeoMesaFeatureIndex
-import org.locationtech.geomesa.index.conf.{QueryHints, QueryProperties, SchemaProperties}
-import org.locationtech.geomesa.index.index.attribute.AttributeIndex
-import org.locationtech.geomesa.index.index.id.IdIndex
-import org.locationtech.geomesa.index.index.z3.Z3Index
-import org.locationtech.geomesa.process.query.ProximitySearchProcess
-import org.locationtech.geomesa.process.tube.TubeSelectProcess
 import org.locationtech.geomesa.utils.collection.SelfClosingIterator
-import org.locationtech.geomesa.utils.conf.{GeoMesaProperties, SemanticVersion}
-import org.locationtech.geomesa.utils.geotools.{FeatureUtils, SimpleFeatureTypes}
-import org.locationtech.geomesa.utils.io.WithClose
-import org.opengis.feature.simple.SimpleFeature
-import org.opengis.filter.Filter
-import org.specs2.matcher.MatchResult
+import org.locationtech.geomesa.utils.conf.FeatureExpiration
+import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
+import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
@@ -45,38 +31,32 @@ class HBaseTTLTest extends Specification with LazyLogging {
   val params = Map(
     ConnectionParam.getName -> MiniCluster.connection,
     HBaseCatalogParam.getName -> getClass.getSimpleName)
-  val ttl = 10000
-  val sft = SimpleFeatureTypes.createType(typeName, f"name:String:index=true,dtg:Date;geomesa.feature.ttl=${ttl}")
 
+  val expiration = "4 seconds"
+  val sft: SimpleFeatureType = SimpleFeatureTypes.createType(typeName, f"name:String:index=true,dtg:Date;geomesa.feature.expiry=$expiration")
+  // NOTE: expiration date is set simply by adding expiration time to when the feature is created
+  val ttl: Long = FeatureExpiration.apply(sft, expiration).expires(new ScalaSimpleFeature(sft, "blank")) - System.currentTimeMillis()
 
+  val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
   "HBase TTL" should {
-    "remove based on TTL" >> {
+    "remove X features based on TTL" >> {
+      val numFeatures = 3
       val ds = DataStoreFinder.getDataStore(params).asInstanceOf[HBaseDataStore]
       ds must not(beNull)
       ds.createSchema(sft)
 
       try {
-        //        ds.getSchema(typeName) must beNull
-        //        val sft = ds.getSchema(typeName)
-
-        sft must not(beNull)
-
-        //        val ns = DataStoreFinder.getDataStore(params ++ Map(NamespaceParam.key -> "ns0")).getSchema(typeName).getName
-        //        ns.getNamespaceURI mustEqual "ns0"
-        //        ns.getLocalPart mustEqual typeName
-
         val fs = ds.getFeatureSource(typeName).asInstanceOf[SimpleFeatureStore]
 
-        val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
         val startTime = new Date(System.currentTimeMillis())
-        val numFeatures = 1
+
         val toAdd = (0 until numFeatures).map { i =>
           val sf = new ScalaSimpleFeature(sft, i.toString)
           // don't know what this line does but it's needed
           sf.getUserData.put(Hints.USE_PROVIDED_FID, java.lang.Boolean.TRUE)
           sf.setAttribute(0, s"name$i")
 
-          val featureTime = new Date(startTime.getTime + 5000 * i)
+          val featureTime = new Date(startTime.getTime + (ttl / 2) * i)
           sf.setAttribute(1, dateFormat.format(featureTime))
           sf
         }
@@ -84,44 +64,43 @@ class HBaseTTLTest extends Specification with LazyLogging {
         val ids = fs.addFeatures(new ListFeatureCollection(sft, toAdd))
         ids.asScala.map(_.getID) must containTheSameElementsAs((0 until numFeatures).map(_.toString))
 
-        getElements(ds,typeName).size mustEqual numFeatures
+        getElements(ds, typeName).size mustEqual numFeatures
 
-        Thread.sleep(10000)
+        Thread.sleep(ttl) // wait until one feature times out
         (1 until numFeatures).map { i =>
-          logger.info(f"running ${i}")
-          getElements(ds,typeName).size mustEqual numFeatures - i
-          Thread.sleep(5000)
+          getElements(ds, typeName).size mustEqual numFeatures - i
+          Thread.sleep(ttl / 2) // wait for next feature to time out
         }
         true
       } finally {
         ds.dispose()
       }
     }
+
     "refuse to add already-expired features" >> {
+      val numFeatures = 2; // one expired, other not
       val ds = DataStoreFinder.getDataStore(params).asInstanceOf[HBaseDataStore]
       ds must not(beNull)
       ds.createSchema(sft)
 
       try {
-        sft must not(beNull)
-
         val fs = ds.getFeatureSource(typeName).asInstanceOf[SimpleFeatureStore]
 
-        val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-
-        val toAdd = (0 until 1).map { i =>
+        val toAdd = (0 until numFeatures).map { i =>
           val sf = new ScalaSimpleFeature(sft, i.toString)
           sf.getUserData.put(Hints.USE_PROVIDED_FID, java.lang.Boolean.TRUE)
           sf.setAttribute(0, s"name$i")
 
-          val featureTime = new Date(System.currentTimeMillis - 15000)
+          val featureTime = new Date(System.currentTimeMillis() - ttl * i)
+          //          logger.info(f"to be deleted at ${featureTime.getTime}, 10 secs from now is ${System.currentTimeMillis() + 10000}")
           sf.setAttribute(1, dateFormat.format(featureTime))
           sf
         }
         val ids = fs.addFeatures(new ListFeatureCollection(sft, toAdd))
-//        ids.asScala.map(_.getID) must containTheSameElementsAs((0 until numFeatures).map(_.toString))
+        ids.asScala.map(_.getID) must containTheSameElementsAs((0 until numFeatures).map(_.toString))
 
-        getElements(ds,typeName).size mustEqual 0
+        // one element should've been rejected
+        getElements(ds, typeName).size mustEqual 1
       } finally {
         ds.dispose()
       }
@@ -136,16 +115,7 @@ class HBaseTTLTest extends Specification with LazyLogging {
   def runQuery(ds: HBaseDataStore,
                typeName: String,
                filter: String): List[SimpleFeature] = {
-    //    var query: Query = null; // don't think this is a a great way to do it
-    //    if (filter != null) {
-    //      val test: Array[String] = null
-    //      query = new Query(typeName, ECQL.toFilter(filter), test)
-    val query = new Query(typeName, ECQL.toFilter(filter))
-    //    }
-    //    else {
-    //      query = new Query(typeName)
-    //    }
-    runQuery(ds, query)
+    runQuery(ds, new Query(typeName, ECQL.toFilter(filter)))
   }
 
   def runQuery(ds: HBaseDataStore,
