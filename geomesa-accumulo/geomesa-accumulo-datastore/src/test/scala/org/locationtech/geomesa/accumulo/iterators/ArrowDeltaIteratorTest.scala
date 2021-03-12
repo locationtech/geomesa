e07a809d3a75f819da11e15ad13cce27c88c2068
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2021-03-12 17:43:39 -0500

    Unit test showing arrow transforms
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-accumulo/geomesa-accumulo-datastore/src/test/scala/org/locationtech/geomesa/accumulo/iterators/ArrowDeltaIteratorTest.scala
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/test/scala/org/locationtech/geomesa/accumulo/iterators/ArrowDeltaIteratorTest.scala
@@ -21,6 +21,7 @@ import org.locationtech.geomesa.features.ScalaSimpleFeature
 import org.locationtech.geomesa.index.conf.QueryHints
 import org.locationtech.geomesa.utils.collection.SelfClosingIterator
 import org.locationtech.geomesa.utils.io.WithClose
+import org.locationtech.jts.geom.Point
 import org.specs2.mock.Mockito
 import org.specs2.runner.JUnitRunner
 
@@ -44,6 +45,28 @@ class ArrowDeltaIteratorTest extends TestWithFeatureType with Mockito {
   addFeatures(features)
 
   "Arrow delta scans" should {
+    "return projections" in {
+      val filter =
+        ECQL.toFilter("bbox(geom, 38, 59, 42, 70) and dtg DURING 2017-02-03T00:00:00.000Z/2017-02-03T01:00:00.000Z")
+      val query = new Query(sft.getTypeName, filter, Array("name", "x=getX(geom)", "y=getY(geom)", "dtg", "geom"))
+      query.getHints.put(QueryHints.ARROW_ENCODE, true)
+      query.getHints.put(QueryHints.ARROW_BATCH_SIZE, 100)
+      query.getHints.put(QueryHints.ARROW_DICTIONARY_FIELDS, "name")
+      query.getHints.put(QueryHints.ARROW_SORT_FIELD, "dtg")
+      val results = SelfClosingIterator(ds.getFeatureReader(query, Transaction.AUTO_COMMIT))
+      val out = new ByteArrayOutputStream
+      results.foreach(sf => out.write(sf.getAttribute(0).asInstanceOf[Array[Byte]]))
+      def in() = new ByteArrayInputStream(out.toByteArray)
+      val result = WithClose(SimpleFeatureArrowFileReader.streaming(in)) { reader =>
+        WithClose(reader.features())(_.map(ScalaSimpleFeature.copy).toList)
+      }
+      println(result.map(_.getAttributes.asScala))
+      result.map(_.getID) mustEqual features.map(_.getID)
+      result.map(_.getAttributes.asScala) mustEqual features.map { f =>
+        val geom = f.getAttribute("geom").asInstanceOf[Point]
+        Seq(f.getAttribute("name"), geom.getX, geom.getY, f.getAttribute("dtg"), geom)
+      }
+    }
     "return projections into json fields" in {
       val filter =
         ECQL.toFilter("bbox(geom, 38, 59, 42, 70) and dtg DURING 2017-02-03T00:00:00.000Z/2017-02-03T01:00:00.000Z")
