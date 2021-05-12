d4185abdc366ee8e94a6233478f97b61c7479a3c
Alex Barreto <axbarreto@lavabit.com>
2021-05-12 17:10:32 -0400

    GEOMESA-3071 Move all converter state into evaluation context
    * Provided converters are thread-safe as a result of this change
    * Custom Fields and TransformerFunctions should implement new methods,
      but will work without changes

--- geomesa-convert/geomesa-convert-all/src/test/scala/org/locationtech/geomesa/convert/testing/ScriptingFunctionFactoryTest.scala
+++ geomesa-convert/geomesa-convert-all/src/test/scala/org/locationtech/geomesa/convert/testing/ScriptingFunctionFactoryTest.scala
@@ -54,10 +54,20 @@ class ScriptingFunctionFactoryTest extends Specification {
         val sff = new ScriptingFunctionFactory
         val hello = sff.functions.find(_.names.contains("js:hello")).head
         hello.apply(Array("geomesa")) mustEqual "hello: geomesa"
+<<<<<<< HEAD
         val gbye = sff.functions.find(_.names.contains("js:gbye")).head
         gbye.apply(Array("geomesa")) mustEqual "goodbye: geomesa"
         val whatsup = sff.functions.find(_.names.contains("js:whatsup")).head
         whatsup.apply(Array("geomesa")) mustEqual "whatsup: geomesa"
+=======
+        hello.eval(Array("geomesa")) mustEqual "hello: geomesa"
+        val gbye = sff.functions.find(_.names.contains("js:gbye")).head
+        gbye.apply(Array("geomesa")) mustEqual "goodbye: geomesa"
+        gbye.eval(Array("geomesa")) mustEqual "goodbye: geomesa"
+        val whatsup = sff.functions.find(_.names.contains("js:whatsup")).head
+        whatsup.apply(Array("geomesa")) mustEqual "whatsup: geomesa"
+        whatsup.eval(Array("geomesa")) mustEqual "whatsup: geomesa"
+>>>>>>> 1ba2f23b3d (GEOMESA-3071 Move all converter state into evaluation context)
       } finally {
         ScriptingFunctionFactory.ConvertScriptsPath.threadLocalValue.remove()
       }
