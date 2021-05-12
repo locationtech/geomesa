a07496c758db84021dadfb8f628b21518b884927
Alex Barreto <axbarreto@lavabit.com>
2021-05-12 16:55:43 -0400

    GEOMESA-3061 Converters - support bytes in Avro top-level union types (#2762)

--- geomesa-convert/geomesa-convert-avro/src/test/scala/org/locationtech/geomesa/convert/avro/AvroConverterTest.scala
+++ geomesa-convert/geomesa-convert-avro/src/test/scala/org/locationtech/geomesa/convert/avro/AvroConverterTest.scala
@@ -8,6 +8,12 @@
 
 package org.locationtech.geomesa.convert.avro
 
+<<<<<<< HEAD
+=======
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File}
+
+import com.google.common.hash.Hashing
+>>>>>>> b9bdd406e3 (GEOMESA-3061 Converters - support bytes in Avro top-level union types (#2762))
 import com.typesafe.config.{ConfigFactory, ConfigRenderOptions}
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.avro.file.DataFileWriter
