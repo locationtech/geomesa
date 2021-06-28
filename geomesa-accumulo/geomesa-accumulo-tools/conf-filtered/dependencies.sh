655e6ec68ec530f0bfbb08692035e237e374a7f2
Alex Barreto <axbarreto@lavabit.com>
2021-06-28 12:38:52 -0400

    GEOMESA-3092 Support Lambda NiFi processor (#2777)
    * Separate Lambda data store config parsing from Accumulo
    * Move PartitionSchemeArgResolver to data store module
    * Handle check for FeatureIdImpl to avoid proxy errors in NiFi

--- geomesa-accumulo/geomesa-accumulo-tools/conf-filtered/dependencies.sh
+++ geomesa-accumulo/geomesa-accumulo-tools/conf-filtered/dependencies.sh
@@ -87,6 +87,7 @@ function dependencies() {
     gavs+=(
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       "org.apache.thrift:libthrift:0.12.0:jar"
 <<<<<<< HEAD
       "org.apache.htrace:htrace-core:3.1.0-incubating:jar"
@@ -97,7 +98,24 @@ function dependencies() {
 <<<<<<< HEAD
       "org.apache.thrift:libthrift:%%thrift.version%%:jar"
 >>>>>>> 276558f47d3 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+<<<<<<< HEAD
 >>>>>>> 7e635f57dc2 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+=======
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> 234aacdc12 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+      "org.apache.thrift:libthrift:%%thrift.version%%:jar"
+=======
+<<<<<<< HEAD
+      "org.apache.commons:commons-collections4:4.4:jar"
+=======
+      "org.apache.commons:commons-collections4:4.3:jar"
+>>>>>>> 51a90e7f0 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+      "org.apache.accumulo:accumulo-hadoop-mapreduce:${accumulo_version}:jar"
+>>>>>>> 120815d0b (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+>>>>>>> a76720eebac (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+>>>>>>> e036868938d (GEOMESA-3092 Support Lambda NiFi processor (#2777))
     )
   else
     gavs+=(
