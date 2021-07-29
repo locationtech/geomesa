47f9507390d8714307653119d4429b3e5dcab539
Alex Barreto <axbarreto@lavabit.com>
2021-07-29 17:26:14 -0400

    GEOMESA-3062 DtgAgeOff Filter does not work properly with join indexes (#2756)
    Signed-off-by: Jim Hughes <jhughes@ccri.com>

--- geomesa-accumulo/geomesa-accumulo-datastore/src/test/scala/org/locationtech/geomesa/accumulo/iterators/DtgAgeOffTest.scala
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/test/scala/org/locationtech/geomesa/accumulo/iterators/DtgAgeOffTest.scala
@@ -8,14 +8,21 @@
 
 package org.locationtech.geomesa.accumulo.iterators
 
+<<<<<<< HEAD
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
+=======
+import java.time.{ZoneOffset, ZonedDateTime}
+import java.util.{Collections, Date}
+
+import org.apache.accumulo.core.client.Connector
+>>>>>>> 1ec19b5aac (GEOMESA-3062 DtgAgeOff Filter does not work properly with join indexes (#2756))
 import org.geotools.data.{DataStore, DataStoreFinder}
 import org.junit.runner.RunWith
 import org.locationtech.geomesa.accumulo.data.AccumuloDataStoreParams
 import org.locationtech.geomesa.accumulo.{MiniCluster, TestWithFeatureType}
 import org.locationtech.geomesa.features.ScalaSimpleFeature
 import org.locationtech.geomesa.security.SecurityUtils
-import org.locationtech.geomesa.utils.collection.SelfClosingIterator
+import org.locationtech.geomesa.utils.collection.{CloseableIterator, SelfClosingIterator}
 import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
 import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes.Configs
 import org.opengis.feature.simple.SimpleFeature
@@ -102,13 +109,20 @@ class DtgAgeOffTest extends Specification with TestWithFeatureType {
 
   // Scans all GeoMesa Accumulo tables directly and verifies the number of records that the `root` user can see.
   private def scanDirect(expected: Int) = {
+<<<<<<< HEAD
     val conn = MiniCluster.cluster.createAccumuloClient(MiniCluster.Users.root.name, new PasswordToken(MiniCluster.Users.root.password))
+=======
+    val conn: Connector = MiniCluster.cluster.getConnector(MiniCluster.Users.root.name, MiniCluster.Users.root.password)
+>>>>>>> 1ec19b5aac (GEOMESA-3062 DtgAgeOff Filter does not work properly with join indexes (#2756))
     conn.tableOperations().list().asScala.filter(t => t.contains("DtgAgeOffTest_DtgAgeOffTest")).forall { tableName =>
       val scanner = conn.createScanner(tableName, MiniCluster.Users.root.auths)
       val count = scanner.asScala.size
       scanner.close()
       count mustEqual expected
     }
+<<<<<<< HEAD
     conn.close()
+=======
+>>>>>>> 1ec19b5aac (GEOMESA-3062 DtgAgeOff Filter does not work properly with join indexes (#2756))
   }
 }
