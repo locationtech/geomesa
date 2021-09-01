b51333ce3c9486fc063b548eec31216ba33d4a3b
Alex Barreto <axbarreto@lavabit.com>
2021-09-01 17:57:19 -0400

    GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792)

--- geomesa-filter/src/main/scala/org/locationtech/geomesa/filter/FilterHelper.scala
+++ geomesa-filter/src/main/scala/org/locationtech/geomesa/filter/FilterHelper.scala
@@ -203,7 +203,11 @@ object FilterHelper {
         o.getChildren.asScala.map(f =>
 =======
         o.getChildren.map(f =>
+<<<<<<< HEAD
 >>>>>>> 1a21a3c300 (GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792))
+=======
+>>>>>>> 1a21a3c30 (GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792))
+>>>>>>> 425a920afa (GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792))
           extractAttributeBounds(f, attribute, binding)
         ).reduceLeft[FilterValues[Bounds[T]]]((acc, child) => {
           if (acc.isEmpty || child.isEmpty) {
