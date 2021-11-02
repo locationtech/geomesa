721b2bea24bf82a13ec7a911ea8a201605818310
Alex Barreto <axbarreto@lavabit.com>
2021-11-02 14:28:57 -0500

    GEOMESA-3137 Arrow - UUID attribute types cause queries to fail (#2809)
    Signed-off-by: Jim Hughes <jhughes@ccri.com>

--- geomesa-arrow/geomesa-arrow-gt/src/test/scala/org/locationtech/geomesa/arrow/vector/SimpleFeatureVectorTest.scala
+++ geomesa-arrow/geomesa-arrow-gt/src/test/scala/org/locationtech/geomesa/arrow/vector/SimpleFeatureVectorTest.scala
@@ -15,6 +15,7 @@ package org.locationtech.geomesa.arrow.vector
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import java.util.{Date, UUID}
 <<<<<<< HEAD
@@ -43,6 +44,11 @@ import java.util.{Date, UUID}
 import java.util.{Date, UUID}
 >>>>>>> 728c6f1ae (GEOMESA-3137 Arrow - UUID attribute types cause queries to fail (#2809))
 >>>>>>> a362209f73 (GEOMESA-3137 Arrow - UUID attribute types cause queries to fail (#2809))
+=======
+=======
+import java.util.{Date, UUID}
+>>>>>>> 728c6f1ae3 (GEOMESA-3137 Arrow - UUID attribute types cause queries to fail (#2809))
+>>>>>>> bb9821e80f (GEOMESA-3137 Arrow - UUID attribute types cause queries to fail (#2809))
 import org.apache.arrow.vector.complex.FixedSizeListVector
 import org.apache.arrow.vector.{BigIntVector, IntVector}
 import org.geotools.util.Converters
