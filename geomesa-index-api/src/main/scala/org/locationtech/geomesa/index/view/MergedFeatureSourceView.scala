e9e38f1687b6492d6926a8a5b51332c3c78d2ddc
Alex Barreto <axbarreto@lavabit.com>
2021-11-23 12:05:36 -0500

    GEOMESA-3153 Fix merged view to only expand bounds on non-null bounds (#2814)
    Signed-off-by: Nate Hellmuth <nhellmuth@ccri.com>

--- geomesa-index-api/src/main/scala/org/locationtech/geomesa/index/view/MergedFeatureSourceView.scala
+++ geomesa-index-api/src/main/scala/org/locationtech/geomesa/index/view/MergedFeatureSourceView.scala
@@ -119,7 +119,11 @@ class MergedFeatureSourceView(
 =======
     sources.foreach {
       case (source, filter) =>
+<<<<<<< HEAD
         val source_bounds = source.getBounds(mergeFilter(sft, query, filter))
+=======
+        val source_bounds = source.getBounds(mergeFilter(query, filter))
+>>>>>>> 22da407b4 (GEOMESA-3153 Fix merged view to only expand bounds on non-null bounds (#2814))
         if(source_bounds != null){
           bounds.expandToInclude(source_bounds)
         }
