cd1ab724c65b6691ae4d081e364d066c88a3a670
John Gibson <jgibson@mitre.org>
2021-12-17 10:26:36 -0500

    GEOMESA-3162: Geomesa client configuration file properly loaded.
    * Prior to this change only a handful of settings were loaded from the
      accumulo client configuration and the rest were simply Accumulo defaults.
    
    Other related changes:
    
    * New license headers for MITRE 2021 contributions.
    * AvroSchemaRegistryConverterTest now uses a random Jetty port, not 8089.
      (Necessary for the unit tests to complete successfully when port 8089 is in
      use by another process.)
    * Error message is clearer when authentication options are missing.

--- geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/data/AccumuloClientConfig.scala
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/data/AccumuloClientConfig.scala
@@ -1,9 +1,14 @@
 /***********************************************************************
  * Copyright (c) 2013-2021 Commonwealth Computer Research, Inc.
+ * Portions Copyright (c) 2021 The MITRE Corporation
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, Version 2.0
  * which accompanies this distribution and is available at
  * http://www.opensource.org/licenses/apache2.0.php.
+ * This software was produced for the U. S. Government under Basic
+ * Contract No. W56KGU-18-D-0004, and is subject to the Rights in
+ * Noncommercial Computer Software and Noncommercial Computer Software
+ * Documentation Clause 252.227-7014 (FEB 2012)
  ***********************************************************************/
 
 package org.locationtech.geomesa.accumulo.data
@@ -12,6 +17,7 @@ import java.io.{File, FileInputStream}
 import java.util.Properties
 
 import com.typesafe.scalalogging.Logger
+import org.apache.accumulo.core.client.ClientConfiguration
 import org.locationtech.geomesa.utils.io.WithClose
 import org.slf4j.LoggerFactory
 
@@ -20,14 +26,19 @@ import scala.util.Try
 /**
  * Client configuration options, loaded from a config file
  *
- * @param instance instance name
- * @param zookeepers zookeeper connect string
- * @param zkTimeout zookeeper timeout
+ * @param config Accumulo client configuration
+ * @param hasInstances instance id or name set in config file
+ * @param instance instance name (not actually used during configuration)
+ * @param zookeepers zookeeper connect string (not actually used during configuration)
+ * @param zkTimeout zookeeper timeout (not actually used during configuration)
  * @param authType  authentication type ("password", "kerberos" or authentication token class)
  * @param principal authentication principal (username, keytab file, etc)
  * @param token authentication token (password, etc)
  */
+@SuppressWarnings(Array("deprecated"))
 case class AccumuloClientConfig(
+    config: ClientConfiguration,
+    hasInstances: Boolean,
     instance: Option[String],
     zookeepers: Option[String],
     zkTimeout: Option[String],
@@ -37,6 +48,7 @@ case class AccumuloClientConfig(
   ) {
   override def toString: String = {
     val values =
+      Seq(s"hasInstances=$hasInstances") ++
       instance.map(s => s"instance=$s").toSeq ++ zookeepers.map(s => s"zookeepers=$s") ++
         zkTimeout.map(s => s"zookeepers.timeout=$s") ++ authType.map(s => s"auth.type=$s") ++
         principal.map(s => s"principal=$s") ++ token.map(_ => "token=***")
@@ -49,8 +61,6 @@ object AccumuloClientConfig {
   val PasswordAuthType = "password"
   val KerberosAuthType = "kerberos"
 
-  private val Empty = AccumuloClientConfig(None, None, None, None, None, None)
-
   // get the logger directly so that the logger name (and config) doesn't have a $ on the end
   private lazy val logger: Logger = Logger(LoggerFactory.getLogger(classOf[AccumuloClientConfig]))
 
@@ -61,7 +71,8 @@ object AccumuloClientConfig {
    */
   def load(): AccumuloClientConfig = {
     val loader = Option(Thread.currentThread().getContextClassLoader).getOrElse(getClass.getClassLoader)
-    AccumuloClientProperties(loader).orElse(ClientConf(loader)).getOrElse(Empty)
+    AccumuloClientProperties(loader).orElse(ClientConf(loader))
+      .getOrElse(AccumuloClientConfig(ClientConfiguration.create(), false, None, None, None, None, None, None))
   }
 
   /**
@@ -69,7 +80,6 @@ object AccumuloClientConfig {
    */
   private object AccumuloClientProperties extends ConfigLoader {
     override protected val fileName: String = "accumulo-client.properties"
-    override protected val instanceKey: String = "instance.name"
     override protected val zookeeperKey: String = "instance.zookeepers"
     override protected val zookeeperTimeoutKey: String = "instance.zookeepers.timeout"
     override protected val authTypeKey: Option[String] = Some("auth.type")
@@ -82,7 +92,6 @@ object AccumuloClientConfig {
    */
   private object ClientConf extends ConfigLoader {
     override protected val fileName: String = "client.conf"
-    override protected val instanceKey: String = "instance.name"
     override protected val zookeeperKey: String = "instance.zookeeper.host"
     override protected val zookeeperTimeoutKey: String = "instance.zookeeper.timeout"
     override protected val authTypeKey: Option[String] = None
@@ -93,7 +102,6 @@ object AccumuloClientConfig {
   private trait ConfigLoader {
 
     protected def fileName: String
-    protected def instanceKey: String
     protected def zookeeperKey: String
     protected def zookeeperTimeoutKey: String
     protected def authTypeKey: Option[String]
@@ -105,13 +113,15 @@ object AccumuloClientConfig {
         val props = new Properties()
         WithClose(new FileInputStream(f))(props.load)
 
-        val instance = Option(props.getProperty(instanceKey))
+        val instance = Option(props.getProperty("instance.name"))
+        val hasInstance = (instance != None) || props.getProperty("instance.id") != null
         val zk = Option(props.getProperty(zookeeperKey))
         val zkTimeout = Option(props.getProperty(zookeeperTimeoutKey))//=30s
         val auth = authTypeKey.map(props.getProperty).filter(_ != null)
         val principal = principalKey.map(props.getProperty).filter(_ != null)
         val token = tokenKey.map(props.getProperty).filter(_ != null)
-        val config = AccumuloClientConfig(instance, zk, zkTimeout, auth, principal, token)
+        val conf = ClientConfiguration.fromFile(f)
+        val config = AccumuloClientConfig(conf, hasInstance, instance, zk, zkTimeout, auth, principal, token)
         logger.info(s"Loaded Accumulo config from '${f.getAbsolutePath}': $config")
         config
       }
