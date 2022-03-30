46556352888314ec2a1bde50140a90edc362e272
Emilio <elahrvivaz@ccri.com>
2022-03-30 13:33:04 -0400

    Update geomesa-utils/src/main/scala/org/locationtech/geomesa/utils/geotools/Conversions.scala

--- geomesa-utils/src/main/scala/org/locationtech/geomesa/utils/geotools/Conversions.scala
+++ geomesa-utils/src/main/scala/org/locationtech/geomesa/utils/geotools/Conversions.scala
@@ -303,6 +303,9 @@ object RichSimpleFeatureType extends Conversions {
     def getUserDataPrefixes: Seq[String] =
       Seq(GeomesaPrefix) ++ userData[String](UserDataPrefix).map(_.split(",")).getOrElse(Array.empty)
 
+    def setZShards(splits: Int): Unit = sft.getUserData.put(IndexZShards, splits.toString)
+    @deprecated("use z2/z3 specific methods")
+    def getZShards: Int = userData(IndexZShards).map(int).getOrElse(4)
     def setZ2Shards(splits: Int): Unit = sft.getUserData.put(IndexZ2Shards, splits.toString)
     def getZ2Shards: Int =
       userData(IndexZ2Shards).map(int).getOrElse(userData(IndexZShards).map(int).getOrElse(4))
