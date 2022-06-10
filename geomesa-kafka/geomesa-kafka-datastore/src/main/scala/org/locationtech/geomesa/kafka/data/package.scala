73f3a8cb6946e373e627a46c6da8b29ffbe875a9
Emilio <elahrvivaz@ccri.com>
2022-06-10 13:31:34 -0400

    GEOMESA-3198 Kafka streams integration (#2854)
    * GEOMESA-3198 Kafka streams integration
    
    * Add default timestamp extractor, fix event-time timestamp extractor

--- geomesa-kafka/geomesa-kafka-datastore/src/main/scala/org/locationtech/geomesa/kafka/data/package.scala
+++ geomesa-kafka/geomesa-kafka-datastore/src/main/scala/org/locationtech/geomesa/kafka/data/package.scala
@@ -9,7 +9,16 @@
 package org.locationtech.geomesa.kafka
 
 import org.apache.kafka.clients.producer.Producer
+<<<<<<< HEAD
 import org.locationtech.geomesa.utils.concurrent.LazyCloseable
+=======
+<<<<<<< HEAD
+=======
+import org.locationtech.geomesa.kafka.utils.GeoMessageSerializer.GeoMessagePartitioner
+>>>>>>> de758f45a (GEOMESA-3198 Kafka streams integration (#2854))
+
+import java.io.Closeable
+>>>>>>> 1b8cbf843 (GEOMESA-3198 Kafka streams integration (#2854))
 
 package object data {
 
