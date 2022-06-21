2640c232d690faab951bf0d78d342ad787b0d88e
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2022-06-21 19:10:55 -0400

    GEOMESA-3217,GEOMESA-3216 Support Postgis json attributes, top-level arrays in json

--- geomesa-gt/geomesa-gt-partitioning/src/main/scala/org/locationtech/geomesa/gt/partition/postgis/dialect/PartitionedPostgisPsDialect.scala
+++ geomesa-gt/geomesa-gt-partitioning/src/main/scala/org/locationtech/geomesa/gt/partition/postgis/dialect/PartitionedPostgisPsDialect.scala
@@ -8,16 +8,43 @@
 
 package org.locationtech.geomesa.gt.partition.postgis.dialect
 
+import com.github.benmanes.caffeine.cache.{CacheLoader, Caffeine, LoadingCache}
 import org.geotools.data.postgis.PostGISPSDialect
 import org.geotools.jdbc.JDBCDataStore
 import org.opengis.feature.`type`.AttributeDescriptor
 import org.opengis.feature.simple.SimpleFeatureType
 import org.opengis.filter.Filter
 
-import java.sql.{Connection, DatabaseMetaData}
+import java.sql.{Connection, DatabaseMetaData, PreparedStatement, Types}
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class PartitionedPostgisPsDialect(store: JDBCDataStore, delegate: PartitionedPostgisDialect)
     extends PostGISPSDialect(store, delegate){
+
+  import PartitionedPostgisPsDialect.PreparedStatementKey
+
+  // cache for tracking json-type columns
+  private val jsonColumns: LoadingCache[PreparedStatementKey, java.lang.Boolean] =
+    Caffeine.newBuilder()
+        .expireAfterAccess(1, TimeUnit.MINUTES)
+        .build(new CacheLoader[PreparedStatementKey, java.lang.Boolean]() {
+          override def load(key: PreparedStatementKey): java.lang.Boolean = {
+            key.ps.getParameterMetaData.getParameterTypeName(key.column).toLowerCase(Locale.US) match {
+              case "jsonb" | "json" => true
+              case _ => false
+            }
+          }
+        })
+
+  override def setValue(value: Any, binding: Class[_], ps: PreparedStatement, column: Int, cx: Connection): Unit = {
+    // json columns are string type in geotools, but we have to use setObject or else we get a binding error
+    if (binding == classOf[String] && jsonColumns.get(new PreparedStatementKey(ps, column))) {
+      ps.setObject(column, value, Types.OTHER)
+    } else {
+      super.setValue(value, binding, ps, column, cx)
+    }
+  }
 
   override protected def convertToArray(
       value: Any, componentTypeName: String, componentType: Class[_], connection: Connection): java.sql.Array = {
@@ -44,4 +71,23 @@ class PartitionedPostgisPsDialect(store: JDBCDataStore, delegate: PartitionedPos
   override def getDesiredTablesType: Array[String] = delegate.getDesiredTablesType
   override def encodePostColumnCreateTable(att: AttributeDescriptor, sql: StringBuffer): Unit =
     delegate.encodePostColumnCreateTable(att, sql)
+}
+
+object PartitionedPostgisPsDialect {
+
+  // uses eq on the prepared statement to ensure that we compute json fields exactly once per prepared statement/col
+  class PreparedStatementKey(val ps: PreparedStatement, val column: Int) {
+
+    override def equals(other: Any): Boolean = {
+      other match {
+        case that: PreparedStatementKey => ps.eq(that.ps) && column == that.column
+        case _ => false
+      }
+    }
+
+    override def hashCode(): Int = {
+      val state = Seq(ps, column)
+      state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    }
+  }
 }
