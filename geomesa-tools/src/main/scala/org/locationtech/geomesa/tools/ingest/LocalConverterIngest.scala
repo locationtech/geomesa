933292aae160d88f65a8bfb4ec69de134911f8b9
Alex Taylor-Barreto <alex.barreto@databricks.com>
2022-06-28 15:10:44 -0700

    Merge branch 'feature/postgis-fixes'
    * feature/postgis-fixes: (33 commits)
      GEOMESA-3151 Fix CLI GT dependency versions (#2812)
      [maven-release-plugin] prepare for next development iteration
      GEOMESA-3135 Fix classpath for HBase/Kudu/Bigtable GeoServer Avro export (#2805)
      GEOMESA-3100 Kafka layer views (#2784)
      GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792)
      GEOMESA-3109 Json array to object converter function (#2788)
      GEOMESA-3102 Fix removal of user data in FileSystemDataStore.createSchema (#2787)
      GEOMESA-3092 Support Lambda NiFi processor (#2777)
      GEOMESA-3076 Do inexact checking for cardinality values in minmax stat
      GEOMESA-3071 Move all converter state into evaluation context
      GEOMESA-3061 Converters - support bytes in Avro top-level union types (#2762)
      GEOMESA-3151 Fix CLI GT dependency versions (#2812)
      GEOMESA-3153 Fix merged view to only expand bounds on non-null bounds (#2814)
      GEOMESA-3137 Arrow - UUID attribute types cause queries to fail (#2809)
      Fix docutils version for Sphinx docs build
      Add note on NiFi scala version to upgrade guide
      [maven-release-plugin] prepare for next development iteration
      GEOMESA-3135 Fix classpath for HBase/Kudu/Bigtable GeoServer Avro export (#2805)
      GEOMESA-3100 Kafka layer views (#2784)
      GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792)
      ...

--- geomesa-tools/src/main/scala/org/locationtech/geomesa/tools/ingest/LocalConverterIngest.scala
+++ geomesa-tools/src/main/scala/org/locationtech/geomesa/tools/ingest/LocalConverterIngest.scala
@@ -268,6 +268,7 @@ import org.locationtech.geomesa.utils.io.fs.LocalDelegate.StdInHandle
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> d2042eea397 (GEOMESA-3071 Move all converter state into evaluation context)
 =======
 <<<<<<< HEAD
@@ -776,6 +777,8 @@ import org.locationtech.geomesa.utils.io.fs.LocalDelegate.StdInHandle
 >>>>>>> 2cc10cb7932 (GEOMESA-3071 Move all converter state into evaluation context)
 =======
 >>>>>>> bb3613638a5 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+=======
+>>>>>>> 709d36fab5e (Merge branch 'feature/postgis-fixes')
 <<<<<<< HEAD
 =======
 >>>>>>> afff6fd74b (GEOMESA-3071 Move all converter state into evaluation context)
@@ -976,7 +979,12 @@ import org.locationtech.geomesa.utils.io.fs.LocalDelegate.StdInHandle
 =======
 =======
 >>>>>>> 1587fcc39d (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+<<<<<<< HEAD
 >>>>>>> bb3613638a5 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+=======
+=======
+>>>>>>> 42e8565e9b (Merge branch 'feature/postgis-fixes')
+>>>>>>> 709d36fab5e (Merge branch 'feature/postgis-fixes')
 import org.locationtech.geomesa.utils.io.{CloseQuietly, CloseWithLogging, CloseablePool, WithClose}
 =======
 import org.locationtech.geomesa.utils.io.{CloseWithLogging, CloseablePool, WithClose}
@@ -1393,6 +1401,8 @@ import org.locationtech.geomesa.utils.io.{CloseQuietly, CloseWithLogging, Closea
 >>>>>>> a2ac294bf3 (GEOMESA-3071 Move all converter state into evaluation context)
 =======
 >>>>>>> e6fdf06261 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+=======
+>>>>>>> db8d998aa2 (Merge branch 'feature/postgis-fixes')
 =======
 >>>>>>> 054a72ed9 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
 >>>>>>> d04dc6d340 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
@@ -6158,8 +6168,17 @@ import org.locationtech.geomesa.utils.io.{CloseQuietly, CloseWithLogging, Closea
 >>>>>>> c8e685410 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
 >>>>>>> 1c744a07e (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
 >>>>>>> a97477799 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+<<<<<<< HEAD
 >>>>>>> e6fdf06261 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+<<<<<<< HEAD
 >>>>>>> 1587fcc39d (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+=======
+=======
+=======
+import org.locationtech.geomesa.utils.io.{CloseQuietly, CloseWithLogging, CloseablePool, WithClose}
+>>>>>>> 3be8d2a5a (Merge branch 'feature/postgis-fixes')
+>>>>>>> db8d998aa2 (Merge branch 'feature/postgis-fixes')
+>>>>>>> 42e8565e9b (Merge branch 'feature/postgis-fixes')
 import org.locationtech.geomesa.utils.text.TextTools
 
 import java.io.Flushable
