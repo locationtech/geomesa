6f6dec5b85c114d4367e618a36173b42d5fb04d8
Alex Taylor-Barreto <alex.barreto@databricks.com>
2022-06-28 15:10:44 -0700

    Merge branch 'feature/postgis-fixes'
    * feature/postgis-fixes: (33 commits)
      GEOMESA-3151 Fix CLI GT dependency versions (#2812)
      [maven-release-plugin] prepare for next development iteration
      GEOMESA-3135 Fix classpath for HBase/Kudu/Bigtable GeoServer Avro export (#2805)
      GEOMESA-3100 Kafka layer views (#2784)
      GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792)
      GEOMESA-3109 Json array to object converter function (#2788)
      GEOMESA-3102 Fix removal of user data in FileSystemDataStore.createSchema (#2787)
      GEOMESA-3092 Support Lambda NiFi processor (#2777)
      GEOMESA-3076 Do inexact checking for cardinality values in minmax stat
      GEOMESA-3071 Move all converter state into evaluation context
      GEOMESA-3061 Converters - support bytes in Avro top-level union types (#2762)
      GEOMESA-3151 Fix CLI GT dependency versions (#2812)
      GEOMESA-3153 Fix merged view to only expand bounds on non-null bounds (#2814)
      GEOMESA-3137 Arrow - UUID attribute types cause queries to fail (#2809)
      Fix docutils version for Sphinx docs build
      Add note on NiFi scala version to upgrade guide
      [maven-release-plugin] prepare for next development iteration
      GEOMESA-3135 Fix classpath for HBase/Kudu/Bigtable GeoServer Avro export (#2805)
      GEOMESA-3100 Kafka layer views (#2784)
      GEOMESA-3113 Add system property to managing HBase deletes with visibilities (#2792)
      ...

--- geomesa-lambda/geomesa-lambda-datastore/src/main/scala/org/locationtech/geomesa/lambda/stream/kafka/KafkaStore.scala
+++ geomesa-lambda/geomesa-lambda-datastore/src/main/scala/org/locationtech/geomesa/lambda/stream/kafka/KafkaStore.scala
@@ -192,6 +192,7 @@ class KafkaStore(
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   private val producer = KafkaStore.producer(sft, config.producerConfig)
 =======
   private val producer = KafkaStore.producer(config.producerConfig)
@@ -512,6 +513,8 @@ class KafkaStore(
 >>>>>>> 8961e92636 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
 =======
 >>>>>>> 955b731915 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+=======
+>>>>>>> da00c7bd68 (Merge branch 'feature/postgis-fixes')
 =======
 >>>>>>> 054a72ed9 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
 >>>>>>> c6444c154 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
@@ -4232,7 +4235,13 @@ class KafkaStore(
 >>>>>>> c8e685410 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
 >>>>>>> 1c744a07e (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
 >>>>>>> a97477799 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+<<<<<<< HEAD
 >>>>>>> 955b731915 (GEOMESA-3151 Fix CLI GT dependency versions (#2812))
+=======
+=======
+  private val producer = KafkaStore.producer(sft, config.producerConfig)
+>>>>>>> 3be8d2a5a (Merge branch 'feature/postgis-fixes')
+>>>>>>> da00c7bd68 (Merge branch 'feature/postgis-fixes')
 
   private val topic = KafkaStore.topic(config.zkNamespace, sft)
 
