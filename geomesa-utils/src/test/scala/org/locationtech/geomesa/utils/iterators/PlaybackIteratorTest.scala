e837005af6e825c52f1197acb79202b761736071
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2022-11-02 17:26:38 -0400

    GEOMESA-3243 Playback command - support projecting dates to current time

--- geomesa-utils/src/test/scala/org/locationtech/geomesa/utils/iterators/PlaybackIteratorTest.scala
+++ geomesa-utils/src/test/scala/org/locationtech/geomesa/utils/iterators/PlaybackIteratorTest.scala
@@ -74,5 +74,26 @@ class PlaybackIteratorTest extends Specification with LazyLogging {
         iter.hasNext must beFalse
       }
     }
+    "project to current time" in {
+      val filter = Some(ECQL.toFilter("name IN ('name8', 'name7')")) // dates are 1 and 2 seconds into interval
+      WithClose(new PlaybackIterator(ds, sft.getTypeName, interval, dtg, filter = filter, rate = 10f, live = true)) { iter =>
+        // don't time the first result, as it will be inconsistent due to setup and querying
+        iter.hasNext must beTrue
+        val dtg1 = iter.next().getAttribute("dtg").asInstanceOf[Date]
+        val start = System.currentTimeMillis()
+        iter.hasNext must beTrue
+        // should block until second feature time has elapsed, 100 millis from first feature (due to 10x rate)
+        val dtg2 = iter.next().getAttribute("dtg").asInstanceOf[Date]
+        val elapsed = System.currentTimeMillis() - start
+        // due to system load, this test tends to fail in travis, so we don't cause an explicit test failure
+        if (elapsed > 130L) {
+          logger.warn(s"PlaybackIteratorTest - playback result was delayed longer than expected 100ms: ${elapsed}ms")
+        }
+        dtg1.getTime must beCloseTo(start, 30)
+        dtg2.getTime must beCloseTo(start + 100, 30) // + 100 due to 10x rate
+
+        iter.hasNext must beFalse
+      }
+    }
   }
 }
