0aadc7b0293e5b5458df0bc9e02426dfd55bb25d
Alexandre Barreto <abarre25@jh.edu>
2022-12-29 11:39:57 -0500

    Merge pull request #3524 from cffk/merid-update-fix
    In mlfn.cpp, invoke pj_enfn with P->n to avoid recomputating n

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -149,9 +149,19 @@ extensions = [
 =======
     'program_with_link',
 =======
+<<<<<<< HEAD
 >>>>>>> 748ccdbcc6 (Merge pull request #3524 from cffk/merid-update-fix)
+<<<<<<< HEAD
 >>>>>>> a8fbb11965 (Merge pull request #3524 from cffk/merid-update-fix)
+<<<<<<< HEAD
 >>>>>>> 10b5e77237 (Merge pull request #3524 from cffk/merid-update-fix)
+=======
+=======
+=======
+>>>>>>> c59e00e4f (Merge pull request #3524 from cffk/merid-update-fix)
+>>>>>>> 21f069ac96 (Merge pull request #3524 from cffk/merid-update-fix)
+>>>>>>> 68b6f2f484 (Merge pull request #3524 from cffk/merid-update-fix)
+>>>>>>> 9172626758 (Merge pull request #3524 from cffk/merid-update-fix)
 ]
 
 # Add any paths that contain templates here, relative to this directory.
