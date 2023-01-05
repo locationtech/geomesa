f51f3fb459f26a497583c8b0b23007f1c102c818
Alexandre Barreto <abarre25@jh.edu>
2023-01-05 13:44:16 -0500

    install.rst: improve instructions regarding proj-data (fixes #3539)

--- src/geodesic.h
+++ src/geodesic.h
@@ -32,10 +32,14 @@
  * GeographicLib.)
  **********************************************************************/
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define GEODESIC_VERSION_MINOR 1
 =======
 #define GEODESIC_VERSION_MINOR 0
 >>>>>>> c59e00e4fb (Merge pull request #3524 from cffk/merid-update-fix)
+=======
+#define GEODESIC_VERSION_MINOR 1
+>>>>>>> 8cbe5e736e (install.rst: improve instructions regarding proj-data (fixes #3539))
 /**
  * The patch level of the geodesic library.  (This tracks the version of
  * GeographicLib.)
