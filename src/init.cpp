50d0f4a0f141baf7cdd937c25b995a1dac24564a
Alexandre Barreto <abarre25@jh.edu>
2023-01-05 13:44:16 -0500

    install.rst: improve instructions regarding proj-data (fixes #3539)

--- src/init.cpp
+++ src/init.cpp
@@ -785,12 +785,16 @@ pj_init_ctx_with_allow_init_epsg(PJ_CONTEXT *ctx, int argc, char **argv, int all
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 5c88d70ae3 (Merge pull request #3524 from cffk/merid-update-fix)
+=======
+>>>>>>> 2f268a5476 (install.rst: improve instructions regarding proj-data (fixes #3539))
     geod_init(PIN->geod, PIN->a,  PIN->f);
 =======
     geod_init(PIN->geod, PIN->a,  PIN->es / (1 + sqrt (PIN->one_es)));
 >>>>>>> c59e00e4fb (Merge pull request #3524 from cffk/merid-update-fix)
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
     geod_init(PIN->geod, PIN->a,  PIN->f);
@@ -861,6 +865,11 @@ pj_init_ctx_with_allow_init_epsg(PJ_CONTEXT *ctx, int argc, char **argv, int all
 >>>>>>> cc67c2deee (install.rst: improve instructions regarding proj-data (fixes #3539))
 =======
 >>>>>>> 5c88d70ae3 (Merge pull request #3524 from cffk/merid-update-fix)
+=======
+=======
+    geod_init(PIN->geod, PIN->a,  PIN->f);
+>>>>>>> 8cbe5e736e (install.rst: improve instructions regarding proj-data (fixes #3539))
+>>>>>>> 2f268a5476 (install.rst: improve instructions regarding proj-data (fixes #3539))
 
     /* Projection specific initialization */
     err = proj_errno_reset (PIN);
