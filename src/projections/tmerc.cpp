fd2fc71a23ca9e54fdee4ed2a5a2ab9504bbfb9e
Alexandre Barreto <abarre25@jh.edu>
2023-01-22 11:45:34 -0500

    typo fixes

--- src/projections/tmerc.cpp
+++ src/projections/tmerc.cpp
@@ -577,6 +577,7 @@ static PJ_XY auto_e_fwd (PJ_LP lp, PJ *P) {
 static PJ_LP auto_e_inv (PJ_XY xy, PJ *P) {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     // For k = 1 and long = 3 (from central meridian),
 =======
     // For k = 1 and lon = 3 (from central meridian),
@@ -587,7 +588,13 @@ static PJ_LP auto_e_inv (PJ_XY xy, PJ *P) {
 =======
     // For k = 1 and lon = 3 (from central meridian),
 >>>>>>> 748ccdbcc6 (Merge pull request #3524 from cffk/merid-update-fix)
+<<<<<<< HEAD
 >>>>>>> a8fbb11965 (Merge pull request #3524 from cffk/merid-update-fix)
+=======
+=======
+    // For k = 1 and long = 3 (from central meridian),
+>>>>>>> e4a6fd6d75 (typo fixes)
+>>>>>>> aa21c6fa76 (typo fixes)
     // At lat = 0, we get x ~= 0.052, y = 0
     // At lat = 90, we get x = 0, y ~= 1.57
     // And the shape of this x=f(y) frontier curve is very very roughly a
