05a1868e90d8960341127ba626591d0b4ce5dc86
Alexandre Barreto <abarre25@jh.edu>
2023-02-03 22:39:24 -0500

    GEOMESA-3246 Upgrade Arrow to 11.0.0

--- geomesa-gt/geomesa-gt-partitioning/src/test/scala/org/locationtech/geomesa/gt/partition/postgis/PartitionedPostgisDataStoreTest.scala
+++ geomesa-gt/geomesa-gt-partitioning/src/test/scala/org/locationtech/geomesa/gt/partition/postgis/PartitionedPostgisDataStoreTest.scala
@@ -11,6 +11,7 @@ package org.locationtech.geomesa.gt.partition.postgis
 import com.typesafe.scalalogging.LazyLogging
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.geotools.data._
 <<<<<<< HEAD
 import org.geotools.feature.simple.SimpleFeatureBuilder
@@ -55,19 +56,35 @@ import org.locationtech.geomesa.filter.FilterHelper
 import org.locationtech.geomesa.gt.partition.postgis.dialect.procedures.{DropAgedOffPartitions, PartitionMaintenance, RollWriteAheadLog}
 import org.locationtech.geomesa.gt.partition.postgis.dialect.tables.UserDataTable
 import org.locationtech.geomesa.gt.partition.postgis.dialect.{PartitionedPostgisDialect, TableConfig, TypeInfo}
+=======
+import org.geotools.data._
+import org.geotools.feature.simple.SimpleFeatureBuilder
+import org.geotools.filter.text.ecql.ECQL
+import org.geotools.jdbc.JDBCDataStore
+import org.junit.runner.RunWith
+import org.locationtech.geomesa.gt.partition.postgis.dialect.procedures.{PartitionMaintenance, RollWriteAheadLog}
+import org.locationtech.geomesa.gt.partition.postgis.dialect.{TableConfig, TypeInfo}
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 import org.locationtech.geomesa.utils.collection.SelfClosingIterator
 import org.locationtech.geomesa.utils.geotools.{FeatureUtils, SimpleFeatureTypes}
 import org.locationtech.geomesa.utils.io.WithClose
 import org.locationtech.geomesa.utils.text.WKTUtils
+<<<<<<< HEAD
 import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
 import org.opengis.filter.Filter
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 import org.specs2.specification.BeforeAfterAll
+=======
+import org.opengis.feature.simple.SimpleFeature
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.images.builder.ImageFromDockerfile
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 import java.sql.Connection
@@ -80,16 +97,25 @@ import java.util.Collections
 >>>>>>> ee1d5f207 (GEOMESA-3215 Postgis - support List-type attributes)
 import scala.annotation.tailrec
 >>>>>>> f639b39b8 (GEOMESA-3208 Postgis - Fix camel-case feature type names)
+=======
+import java.sql.Connection
+import java.util.Collections
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 import scala.util.Try
 import scala.util.control.NonFatal
 
 @RunWith(classOf[JUnitRunner])
+<<<<<<< HEAD
 class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll with LazyLogging {
+=======
+class PartitionedPostgisDataStoreTest extends Specification with LazyLogging {
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 
   import scala.collection.JavaConverters._
 
   val hours = 1
   val spec =
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -139,7 +165,13 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
 =======
     "name:List[String],props:String:json=true,age:Int,dtg:Date,*geom:Point:srid=4326;" +
 >>>>>>> d845d7c1b (GEOMESA-3254 Add Bloop build support)
+<<<<<<< HEAD
 >>>>>>> 09d87762c5 (GEOMESA-3254 Add Bloop build support)
+=======
+=======
+    "name:List[String],props:String:json=true,age:Int,dtg:Date,*geom:Point:srid=4326;" +
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
+>>>>>>> 7c0158dc6e (GEOMESA-3246 Upgrade Arrow to 11.0.0)
         Seq(
           s"pg.partitions.interval.hours=$hours",
           "pg.partitions.cron.minute=0"/*,
@@ -150,6 +182,7 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
           "pg.partitions.tablespace.main=partition",*/
         ).mkString(",")
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   val schema = "public"
 =======
@@ -164,6 +197,9 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
       remove = false
     )
 >>>>>>> ee1d5f207 (GEOMESA-3215 Postgis - support List-type attributes)
+=======
+  val schema = "public"
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 
   lazy val sft = SimpleFeatureTypes.createType(s"test", spec)
 
@@ -181,22 +217,35 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
 
   lazy val params = Map(
     "dbtype" -> PartitionedPostgisDataStoreParams.DbType.sample,
+<<<<<<< HEAD
     "host" -> host,
     "port" -> port,
+=======
+    "host" -> container.getHost,
+    "port" -> container.getFirstMappedPort.toString,
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
     "schema" -> schema,
     "database" -> "postgres",
     "user" -> "postgres",
     "passwd" -> "postgres",
     "Batch insert size" -> "10",
+<<<<<<< HEAD
+=======
+    "Commit size" -> "20",
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
     "preparedStatements" -> "true"
   )
 
   var container: GenericContainer[_] = _
 
+<<<<<<< HEAD
   lazy val host = Option(container).map(_.getHost).getOrElse("localhost")
   lazy val port = Option(container).map(_.getFirstMappedPort).getOrElse(5432).toString
 
   override def beforeAll(): Unit = {
+=======
+  step {
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
     val image =
       new ImageFromDockerfile("testcontainers/postgis_cron", false)
           .withFileFromClasspath(".", "testcontainers")
@@ -208,6 +257,7 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
     container.followOutput(new Slf4jLogConsumer(logger.underlying))
   }
 
+<<<<<<< HEAD
   override def afterAll(): Unit = {
     if (container != null) {
       container.stop()
@@ -240,12 +290,17 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
         )
 
 >>>>>>> f639b39b8 (GEOMESA-3208 Postgis - Fix camel-case feature type names)
+=======
+  "PartitionedPostgisDataStore" should {
+    "work" in {
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
       val ds = DataStoreFinder.getDataStore(params.asJava)
       ds must not(beNull)
 
       try {
         ds must beAnInstanceOf[JDBCDataStore]
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         foreach(Seq("test", "test-dash")) { name =>
           val sft = SimpleFeatureTypes.renameSft(this.sft, name)
@@ -504,11 +559,76 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
           }
         }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+        ds.getTypeNames must beEmpty
+        ds.createSchema(sft)
+
+        val schema = Try(ds.getSchema(sft.getTypeName)).getOrElse(null)
+        schema must not(beNull)
+        schema.getUserData.asScala must containAllOf(sft.getUserData.asScala.toSeq)
+        logger.debug(s"Schema: ${SimpleFeatureTypes.encodeType(schema)}")
+
+        // write some data
+        WithClose(new DefaultTransaction()) { tx =>
+          WithClose(ds.getFeatureWriterAppend(sft.getTypeName, tx)) { writer =>
+            features.foreach { feature =>
+              FeatureUtils.write(writer, feature, useProvidedFid = true)
+            }
+          }
+          tx.commit()
+        }
+
+        def compFromDb(sf: SimpleFeature): Seq[Any] = Seq(sf.getID) ++ sf.getAttributes.asScala.map {
+          // even though Timestamp extends Date, equals comparison doesn't work between the 2
+          case t: java.sql.Timestamp => new java.util.Date(t.getTime)
+          case a => a
+        }
+        // note: jdbc data store adds the type name into the fid, so we add it here for the comparison
+        def compWithFid(sf: SimpleFeature): Seq[Any] = Seq(s"${sft.getTypeName}.${sf.getID}") ++ sf.getAttributes.asScala
+
+        WithClose(ds.getFeatureReader(new Query(sft.getTypeName), Transaction.AUTO_COMMIT)) { reader =>
+          val result = SelfClosingIterator(reader).toList
+          result.map(compFromDb) must containTheSameElementsAs(features.map(compWithFid))
+        }
+
+        // verify data is being partitioned as expected
+        WithClose(ds.asInstanceOf[JDBCDataStore].getConnection(Transaction.AUTO_COMMIT)) { cx =>
+          val typeInfo = TypeInfo(this.schema, sft)
+          // initially everything is in the write ahead log
+          foreach(Seq(typeInfo.tables.view, typeInfo.tables.writeAhead))(table => count(cx, table) mustEqual 10)
+          foreach(Seq(typeInfo.tables.writeAheadPartitions, typeInfo.tables.mainPartitions))(table => count(cx, table) mustEqual 0)
+          // manually invoke the scheduled crons so we don't have to wait
+          WithClose(cx.prepareCall(s"call ${RollWriteAheadLog.name(typeInfo).quoted}();"))(_.execute())
+          WithClose(cx.prepareCall(s"call ${PartitionMaintenance.name(typeInfo).quoted}();"))(_.execute())
+          // verify that data was sorted into the appropriate tables based on dtg
+          count(cx, typeInfo.tables.view) mustEqual 10
+          count(cx, typeInfo.tables.writeAhead) mustEqual 0
+          count(cx, typeInfo.tables.writeAheadPartitions) must beGreaterThan(0)
+          count(cx, typeInfo.tables.mainPartitions) must beGreaterThan(0)
+        }
+
+        // ensure we still get same results after running partitioning
+        WithClose(ds.getFeatureReader(new Query(sft.getTypeName), Transaction.AUTO_COMMIT)) { reader =>
+          val result = SelfClosingIterator(reader).toList
+          result.map(compFromDb) must containTheSameElementsAs(features.map(compWithFid))
+        }
+
+        features.foreach { feature =>
+          WithClose(ds.getFeatureWriter(sft.getTypeName, ECQL.toFilter(s"IN('${feature.getID}')"), Transaction.AUTO_COMMIT)) { writer =>
+            writer.hasNext must beTrue
+            writer.next()
+            writer.remove()
+          }
+        }
+>>>>>>> 7c0158dc6e (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 
         WithClose(ds.getFeatureReader(new Query(sft.getTypeName), Transaction.AUTO_COMMIT)) { reader =>
           reader.hasNext must beFalse
         }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         ds.removeSchema(sft.getTypeName)
 =======
@@ -547,6 +667,10 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
 >>>>>>> locationtech-main
 =======
 >>>>>>> c3367776f (GEOMESA-3261 Postgis - Fix age-off for non-alpha feature type names)
+=======
+        ds.removeSchema(sft.getTypeName)
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
+>>>>>>> 7c0158dc6e (GEOMESA-3246 Upgrade Arrow to 11.0.0)
       } catch {
         case NonFatal(e) => logger.error("", e); ko
       } finally {
@@ -554,6 +678,7 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
       }
       ok
     }
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -803,6 +928,16 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
   def compWithFid(sf: SimpleFeature, sft: SimpleFeatureType): Seq[Any] =
     Seq(s"${sft.getTypeName}.${sf.getID}") ++ sf.getAttributes.asScala
 
+=======
+  }
+
+  step {
+    if (container != null) {
+      container.stop()
+    }
+  }
+
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
   def count(cx: Connection, table: TableConfig): Int = {
     WithClose(cx.prepareStatement(s"select count(*) from ${table.name.qualified};")) { statement =>
       WithClose(statement.executeQuery()) { rs =>
@@ -812,6 +947,7 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
     }
 <<<<<<< HEAD
   }
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 =======
@@ -902,5 +1038,10 @@ class PartitionedPostgisDataStoreTest extends Specification with BeforeAfterAll 
     def any: Boolean = create || upgrade || write || update || query || delete || remove
   }
 >>>>>>> 58d14a257 (GEOMESA-3254 Add Bloop build support)
+<<<<<<< HEAD
 >>>>>>> 1dae86c846 (GEOMESA-3254 Add Bloop build support)
+=======
+=======
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
+>>>>>>> 7c0158dc6e (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 }
