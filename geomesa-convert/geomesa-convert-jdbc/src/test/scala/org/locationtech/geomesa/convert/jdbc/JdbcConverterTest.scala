0c23c876296c38f402abb00a680a3b997a0c74d1
Alexandre Barreto <abarre25@jh.edu>
2023-02-03 22:39:24 -0500

    GEOMESA-3246 Upgrade Arrow to 11.0.0

--- geomesa-convert/geomesa-convert-jdbc/src/test/scala/org/locationtech/geomesa/convert/jdbc/JdbcConverterTest.scala
+++ geomesa-convert/geomesa-convert-jdbc/src/test/scala/org/locationtech/geomesa/convert/jdbc/JdbcConverterTest.scala
@@ -12,6 +12,7 @@ import com.typesafe.config.ConfigFactory
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.typesafe.scalalogging.LazyLogging
 =======
 >>>>>>> 58d14a257e (GEOMESA-3254 Add Bloop build support)
@@ -23,6 +24,14 @@ import com.typesafe.scalalogging.LazyLogging
 =======
 >>>>>>> 58d14a257 (GEOMESA-3254 Add Bloop build support)
 >>>>>>> fa60953a42 (GEOMESA-3254 Add Bloop build support)
+=======
+import com.typesafe.scalalogging.LazyLogging
+=======
+>>>>>>> 58d14a257 (GEOMESA-3254 Add Bloop build support)
+=======
+import com.typesafe.scalalogging.LazyLogging
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
+>>>>>>> 05a1868e90 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 import org.junit.runner.RunWith
 import org.locationtech.geomesa.convert2.SimpleFeatureConverter
 import org.locationtech.geomesa.features.ScalaSimpleFeature
@@ -30,6 +39,7 @@ import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.locationtech.geomesa.utils.io.WithClose
 =======
 import org.locationtech.geomesa.utils.io.{PathUtils, WithClose}
@@ -38,11 +48,19 @@ import org.locationtech.geomesa.utils.io.{PathUtils, WithClose}
 import org.locationtech.geomesa.utils.io.WithClose
 >>>>>>> a928f2f739 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 =======
+=======
+>>>>>>> 05a1868e90 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 import org.locationtech.geomesa.utils.io.WithClose
 =======
 import org.locationtech.geomesa.utils.io.{PathUtils, WithClose}
 >>>>>>> 58d14a257 (GEOMESA-3254 Add Bloop build support)
+<<<<<<< HEAD
 >>>>>>> fa60953a42 (GEOMESA-3254 Add Bloop build support)
+=======
+=======
+import org.locationtech.geomesa.utils.io.WithClose
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
+>>>>>>> 05a1868e90 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 import org.locationtech.jts.geom.Point
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
@@ -51,6 +69,7 @@ import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.utility.DockerImageName
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 import java.io.ByteArrayInputStream
 import java.nio.charset.StandardCharsets
@@ -58,16 +77,20 @@ import java.sql.{Connection, DriverManager}
 import java.util.Date
 =======
 >>>>>>> a928f2f739 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
+=======
+>>>>>>> 05a1868e90 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 
 import java.io.ByteArrayInputStream
 import java.nio.charset.StandardCharsets
 import java.sql.{Connection, DriverManager}
 import java.util.Date
+=======
+>>>>>>> a928f2f73 (GEOMESA-3246 Upgrade Arrow to 11.0.0)
 
 import java.io.ByteArrayInputStream
 import java.nio.charset.StandardCharsets
-import java.nio.file.Files
 import java.sql.{Connection, DriverManager}
+import java.util.Date
 
 @RunWith(classOf[JUnitRunner])
 class JdbcConverterTest extends Specification with BeforeAfterAll with LazyLogging {
