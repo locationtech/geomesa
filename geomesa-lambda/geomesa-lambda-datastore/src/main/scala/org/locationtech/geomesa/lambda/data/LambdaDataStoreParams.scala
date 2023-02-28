efa05b20f8e6e4d9cdd63ca217a3b177ed996c1b
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2023-02-28 01:42:42 +0000

    Try to fix intermittent zookeeper test failures

--- geomesa-lambda/geomesa-lambda-datastore/src/main/scala/org/locationtech/geomesa/lambda/data/LambdaDataStoreParams.scala
+++ geomesa-lambda/geomesa-lambda-datastore/src/main/scala/org/locationtech/geomesa/lambda/data/LambdaDataStoreParams.scala
@@ -2485,12 +2485,16 @@ object LambdaDataStoreParams extends GeoMesaDataStoreParams with SecurityParams 
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> d420f80210 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+=======
+>>>>>>> 32133570e6 (Try to fix intermittent zookeeper test failures)
 =======
 >>>>>>> 16f912e1ef (GEOMESA-3092 Support Lambda NiFi processor (#2777))
 =======
 >>>>>>> 9b0960d94d (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 >>>>>>> 6c49ac84e4 (Try to fix intermittent zookeeper test failures)
@@ -2508,6 +2512,10 @@ object LambdaDataStoreParams extends GeoMesaDataStoreParams with SecurityParams 
 >>>>>>> 04cf999171 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
 =======
 >>>>>>> d420f80210 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+=======
+=======
+>>>>>>> 6c49ac84e4 (Try to fix intermittent zookeeper test failures)
+>>>>>>> 32133570e6 (Try to fix intermittent zookeeper test failures)
       "lambda.offset-manager",
       "Offset manager instance to use",
       deprecatedKeys = Seq("lamdab.offset-manager", "offsetManager"))
@@ -2537,12 +2545,16 @@ object LambdaDataStoreParams extends GeoMesaDataStoreParams with SecurityParams 
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 32133570e6 (Try to fix intermittent zookeeper test failures)
 =======
 >>>>>>> 51a90e7f0 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
 >>>>>>> 16f912e1ef (GEOMESA-3092 Support Lambda NiFi processor (#2777))
 =======
 >>>>>>> 51a90e7f04 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
 >>>>>>> 9b0960d94d (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 >>>>>>> 6c49ac84e4 (Try to fix intermittent zookeeper test failures)
@@ -2563,6 +2575,10 @@ object LambdaDataStoreParams extends GeoMesaDataStoreParams with SecurityParams 
 >>>>>>> 04cf999171 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
 =======
 >>>>>>> d420f80210 (GEOMESA-3092 Support Lambda NiFi processor (#2777))
+=======
+=======
+>>>>>>> 6c49ac84e4 (Try to fix intermittent zookeeper test failures)
+>>>>>>> 32133570e6 (Try to fix intermittent zookeeper test failures)
 
   def parse(params: java.util.Map[String, _], namespace: String): LambdaConfig = {
     val brokers = BrokersParam.lookup(params)
