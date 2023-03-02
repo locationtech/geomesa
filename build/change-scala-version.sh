c0c2e08359e1e93c686f8d736ed97594738e48ef
Emilio <elahrvivaz@ccri.com>
2023-03-02 15:50:07 -0500

    GEOMESA-3246 Update Scala to 2.12.17 (#2976)

--- build/change-scala-version.sh
+++ build/change-scala-version.sh
@@ -27,6 +27,7 @@ VALID_VERSIONS=("2.12" "2.13")
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 7564665969 (GEOMESA-3254 Add Bloop build support)
 =======
@@ -35,10 +36,13 @@ VALID_VERSIONS=("2.12" "2.13")
 >>>>>>> e74fa3f690 (GEOMESA-3254 Add Bloop build support)
 =======
 >>>>>>> 9e49c1aac7 (GEOMESA-3254 Add Bloop build support)
+=======
+>>>>>>> f01f17feca (GEOMESA-3246 Update Scala to 2.12.17 (#2976))
 FULL_VERSIONS=("2.12.17" "2.13.10")
 =======
 FULL_VERSIONS=("2.12.13" "2.13.10")
 >>>>>>> 58d14a257e (GEOMESA-3254 Add Bloop build support)
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -63,6 +67,11 @@ FULL_VERSIONS=("2.12.17" "2.13.10")
 >>>>>>> e74fa3f690 (GEOMESA-3254 Add Bloop build support)
 =======
 >>>>>>> 9e49c1aac7 (GEOMESA-3254 Add Bloop build support)
+=======
+=======
+FULL_VERSIONS=("2.12.17" "2.13.10")
+>>>>>>> 27d2a13b23 (GEOMESA-3246 Update Scala to 2.12.17 (#2976))
+>>>>>>> f01f17feca (GEOMESA-3246 Update Scala to 2.12.17 (#2976))
 
 usage() {
   echo "Usage: $(basename "$0") [-h|--help] <version>
