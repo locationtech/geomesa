c883d35d8d48ba10e06310857666bad83098d026
Emilio <elahrvivaz@ccri.com>
2023-03-29 09:32:36 -0400

    GEOMESA-3272 Postgis - Add idle transaction timeout (#2978)
    * New `idle_in_transaction_session_timeout` data store param

--- geomesa-gt/geomesa-gt-partitioning/src/main/scala/org/locationtech/geomesa/gt/partition/postgis/PartitionedPostgisDataStoreParams.scala
+++ geomesa-gt/geomesa-gt-partitioning/src/main/scala/org/locationtech/geomesa/gt/partition/postgis/PartitionedPostgisDataStoreParams.scala
@@ -8,14 +8,16 @@
 
 package org.locationtech.geomesa.gt.partition.postgis
 
-import org.geotools.data.{DataAccessFactory, Parameter}
+import org.geotools.data.DataAccessFactory.Param
+import org.geotools.data.Parameter
 
 import java.util.Collections
+import scala.concurrent.duration.Duration
 
 object PartitionedPostgisDataStoreParams {
 
   val DbType =
-    new DataAccessFactory.Param(
+    new Param(
       "dbtype",
       classOf[String],
       "Type",
@@ -23,4 +25,32 @@ object PartitionedPostgisDataStoreParams {
       "postgis-partitioned",
       Collections.singletonMap(Parameter.LEVEL, "program")
     )
+
+  val IdleInTransactionTimeout =
+    new Param(
+      "idle_in_transaction_session_timeout",
+      classOf[Timeout],
+      "Transaction idle timeout (e.g. '2 minutes'). " +
+          "See https://www.postgresql.org/docs/15/runtime-config-client.html#GUC-IDLE-IN-TRANSACTION-SESSION-TIMEOUT",
+      false
+    ) with TimeoutParam
+
+  // note: need a default string constructor so geotools can create it from the param
+  class Timeout(repr: String) {
+    private val duration = Duration(repr)
+    require(duration.isFinite && duration.gt(Duration.Zero), s"Invalid duration: $repr")
+
+    val millis: Long = duration.toMillis
+    val seconds: Int = math.ceil(millis / 1000.0).toInt
+  }
+
+  trait TimeoutParam extends Param {
+    override def lookUp(map: java.util.Map[String, _]): Timeout =
+      super.lookUp(map) match {
+        case t: Timeout => t
+        case _ => null
+      }
+
+    def opt(map: java.util.Map[String, _]): Option[Timeout] = Option(lookUp(map))
+  }
 }
