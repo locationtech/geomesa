ac1d8fb6fb7c0c38f23eb75a7ee92b291c9f248f
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2024-03-22 12:32:38 +0000

    Fix loading of examples from CLI tools path

--- geomesa-gt/geomesa-gt-tools/examples/ingest/avro/ingest_avro.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# type and converter example-xml is registered in $GEOMESA_HOME/conf/application.conf
-bin/geomesa-gt ingest --param dbtype=postgis --param host=localhost --param port=5432 --param schema=public --param database=database --param user=postgres --param passwd=postgres -s example-avro -C example-avro-no-header example_no_header.avro
-
-bin/geomesa-gt ingest --param dbtype=postgis --param host=localhost --param port=5432 --param schema=public --param database=database --param user=postgres --param passwd=postgres -s example-avro -C example-avro-header example_with_header.avro
-
