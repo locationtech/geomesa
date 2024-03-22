0f9823216e684cfaf9545a2d8c2a86d73765a3e5
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2024-03-22 12:32:38 +0000

    Fix loading of examples from CLI tools path

--- geomesa-accumulo/geomesa-accumulo-tools/examples/ingest/avro/ingest_avro.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# type and converter example-xml is registered in $GEOMESA_HOME/conf/application.conf
-bin/geomesa-accumulo ingest -u user -p password -i inst -z zoo -c catalog -s example-avro -C example-avro-no-header example_no_header.avro
-
-bin/geomesa-accumulo ingest -u user -p password -i inst -z zoo -c catalog -s example-avro -C example-avro-header example_with_header.avro
--- geomesa-gt/geomesa-gt-tools/examples/ingest/xml/ingest_xml.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# type and converter example-xml is registered in $GEOMESA_HOME/conf/application.conf
-bin/geomesa-gt ingest --param dbtype=postgis --param host=localhost --param port=5432 --param schema=public --param database=database --param user=postgres --param passwd=postgres -s example-xml -C example-xml example.xml
