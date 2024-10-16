75a6ae04360c0cc56b1a91615c6198074d9e1a54
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2024-10-16 19:42:21 -0400

    fix missing license header

--- geomesa-gt/geomesa-gt-partitioning/src/test/scala/org/locationtech/geomesa/gt/partition/postgis/TestQueryInterceptor.scala
+++ geomesa-gt/geomesa-gt-partitioning/src/test/scala/org/locationtech/geomesa/gt/partition/postgis/TestQueryInterceptor.scala
@@ -1,3 +1,11 @@
+/***********************************************************************
+ * Copyright (c) 2013-2024 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
 package org.locationtech.geomesa.gt.partition.postgis
 
 import org.geotools.api.data.{DataStore, Query}
