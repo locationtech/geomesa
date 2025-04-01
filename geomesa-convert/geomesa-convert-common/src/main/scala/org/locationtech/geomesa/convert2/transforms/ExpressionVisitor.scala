668346d753c0fcd4827b97ec49359e490c2d45c5
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2025-04-01 10:50:55 -0400

    GEOMESA-3457 Converters - Fix feature-to-feature converter circular references

---
+++ geomesa-convert/geomesa-convert-common/src/main/scala/org/locationtech/geomesa/convert2/transforms/ExpressionVisitor.scala
@@ -0,0 +1,46 @@
+package org.locationtech.geomesa.convert2.transforms
+
+import org.locationtech.geomesa.convert2.transforms.Expression._
+
+/**
+ * Visitor pattern for manipulating expressions
+ *
+ * @tparam T result type
+ */
+trait ExpressionVisitor[T] {
+  def visit[U <: AnyRef](e: Literal[U]): T
+  def visit(e: CastToInt): T
+  def visit(e: CastToLong): T
+  def visit(e: CastToFloat): T
+  def visit(e: CastToDouble): T
+  def visit(e: CastToBoolean): T
+  def visit(e: CastToString): T
+  def visit(e: FunctionExpression): T
+  def visit(e: TryExpression): T
+  def visit(e: WithDefaultExpression): T
+  def visit(e: FieldLookup): T
+  def visit(e: Column): T
+  def visit(e: RegexExpression): T
+}
+
+object ExpressionVisitor {
+
+  /**
+   * Base class to manipulate expressions by visiting every expression in the tree
+   */
+  class ExpressionTreeVisitor extends ExpressionVisitor[Expression] {
+    override def visit[U <: AnyRef](e: Literal[U]): Expression = e
+    override def visit(e: Column): Expression = e
+    override def visit(e: RegexExpression): Expression = e
+    override def visit(e: FieldLookup): Expression = e
+    override def visit(e: CastToInt): Expression = e.copy(e.e.accept(this))
+    override def visit(e: CastToLong): Expression = e.copy(e.e.accept(this))
+    override def visit(e: CastToFloat): Expression = e.copy(e.e.accept(this))
+    override def visit(e: CastToDouble): Expression = e.copy(e.e.accept(this))
+    override def visit(e: CastToBoolean): Expression = e.copy(e.e.accept(this))
+    override def visit(e: CastToString): Expression = e.copy(e.e.accept(this))
+    override def visit(e: FunctionExpression): Expression = e.copy(arguments = e.arguments.map(_.accept(this)))
+    override def visit(e: TryExpression): Expression = e.copy(e.toTry.accept(this), e.fallback.accept(this))
+    override def visit(e: WithDefaultExpression): Expression = e.copy(e.expressions.map(_.accept(this)))
+  }
+}
