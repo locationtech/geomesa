1c2f898d2dd2ef32b589c7cbbf0d74acf86021b5
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2025-04-01 14:14:12 -0400

    GEOMESA-3454 FSDS - Fix closing file streams in multi-threaded reader

--- geomesa-fs/geomesa-fs-storage/geomesa-fs-storage-common/src/main/scala/org/locationtech/geomesa/fs/storage/common/FileSystemThreadedReader.scala
+++ geomesa-fs/geomesa-fs-storage/geomesa-fs-storage-common/src/main/scala/org/locationtech/geomesa/fs/storage/common/FileSystemThreadedReader.scala
@@ -16,7 +16,7 @@ import org.locationtech.geomesa.fs.storage.api.StorageMetadata.{StorageFileActio
 import org.locationtech.geomesa.fs.storage.common.AbstractFileSystemStorage.FileSystemPathReader
 import org.locationtech.geomesa.utils.collection.CloseableIterator
 import org.locationtech.geomesa.utils.concurrent.PhaserUtils
-import org.locationtech.geomesa.utils.io.WithClose
+import org.locationtech.geomesa.utils.io.{CloseWithLogging, WithClose}
 
 import java.util.concurrent._
 import scala.util.control.NonFatal
@@ -224,12 +224,14 @@ object FileSystemThreadedReader extends StrictLogging {
     ) extends Runnable {
 
     override def run(): Unit = {
+      val ci = iter
       try {
-        iter.foreach(queue.put)
+        ci.foreach(queue.put)
       } catch {
         case NonFatal(e) => logger.error(s"Error reading file $path", e)
       } finally {
         phaser.arriveAndDeregister()
+        CloseWithLogging(ci)
       }
     }
   }
