0c3a30554969edbda398f66fb6b7c2d75547abe1
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2025-04-09 20:22:01 -0400

    fix unit test

--- geomesa-fs/geomesa-fs-storage/geomesa-fs-storage-parquet/src/test/scala/org/locationtech/geomesa/parquet/ParquetStorageTest.scala
+++ geomesa-fs/geomesa-fs-storage/geomesa-fs-storage-parquet/src/test/scala/org/locationtech/geomesa/parquet/ParquetStorageTest.scala
@@ -355,7 +355,7 @@ class ParquetStorageTest extends Specification with AllExpectations with LazyLog
       }
 
       // note: this is somewhat of a magic number, in that it works the first time through with no remainder
-      val targetSize = 2100L
+      val targetSize = 2400L
 
       withTestDir { dir =>
         val context = FileSystemContext(dir, config)
