88f5d918e0729071ac3f89c13e6e5a1d442e678b
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2025-08-13 08:35:11 -0400

    GEOMESA-3504 Converters - Confluent Avro should expose raw bytes if necessary

--- geomesa-convert/geomesa-convert-avro-schema-registry/src/main/scala/org/locationtech/geomesa/convert/avro/registry/AvroSchemaRegistryConverter.scala
+++ geomesa-convert/geomesa-convert-avro-schema-registry/src/main/scala/org/locationtech/geomesa/convert/avro/registry/AvroSchemaRegistryConverter.scala
@@ -8,20 +8,21 @@
 
 package org.locationtech.geomesa.convert.avro.registry
 
-import com.github.benmanes.caffeine.cache.{CacheLoader, Caffeine, LoadingCache}
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient
-import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.schemaregistry.client.{CachedSchemaRegistryClient, SchemaRegistryClient}
 import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
-import org.apache.avro.io.DecoderFactory
+import org.apache.avro.io.{BinaryDecoder, DecoderFactory}
 import org.geotools.api.feature.simple.SimpleFeatureType
 import org.locationtech.geomesa.convert.EvaluationContext
-import org.locationtech.geomesa.convert.avro.registry.AvroSchemaRegistryConverter.{AvroSchemaRegistryConfig, GenericRecordSchemaRegistryIterator}
+import org.locationtech.geomesa.convert.avro.AvroConverter
+import org.locationtech.geomesa.convert.avro.registry.AvroSchemaRegistryConverter.{AvroSchemaRegistryConfig, GenericRecordSchemaRegistryBytesIterator, GenericRecordSchemaRegistryIterator}
 import org.locationtech.geomesa.convert2.AbstractConverter.{BasicField, BasicOptions}
 import org.locationtech.geomesa.convert2.transforms.Expression
+import org.locationtech.geomesa.convert2.transforms.Expression.Column
 import org.locationtech.geomesa.convert2.{AbstractConverter, ConverterConfig, ConverterName}
 import org.locationtech.geomesa.utils.collection.CloseableIterator
+import org.locationtech.geomesa.utils.io.CopyingInputStream
 
 import java.io.InputStream
 import java.nio.ByteBuffer
@@ -36,40 +37,36 @@ class AvroSchemaRegistryConverter(
 
   private val schemaRegistryClient = new CachedSchemaRegistryClient(config.schemaRegistry, 100)
 
-  private val kafkaAvroDeserializer = new ThreadLocal[KafkaAvroDeserializer]() {
-    override def initialValue(): KafkaAvroDeserializer = new KafkaAvroDeserializer(schemaRegistryClient)
+  // if required, set the raw bytes in the result array
+  private val requiresBytes = {
+    val expressions = config.idField.toSeq ++ fields.flatMap(_.transforms) ++ config.userData.values
+    Expression.flatten(expressions).contains(Column(0))
   }
 
-  // Create schema registry reader from URL string and create Avro reader cache
-  private val schemaRegistryConfig: LoadingCache[Integer, GenericDatumReader[GenericRecord]] =
-    getReaderCache(kafkaAvroDeserializer)
-
-  // Create Avro reader cache to map schema ID to GenericDatumReader
-  def getReaderCache(deserializer: ThreadLocal[KafkaAvroDeserializer]): LoadingCache[Integer, GenericDatumReader[GenericRecord]] = {
-    Caffeine.newBuilder().build(
-      new CacheLoader[Integer, GenericDatumReader[GenericRecord]] {
-        override def load(id: Integer): GenericDatumReader[GenericRecord] = {
-          new GenericDatumReader[GenericRecord](deserializer.get.getById(id))
-        }
-      }
-    )
+  override protected def parse(is: InputStream, ec: EvaluationContext): CloseableIterator[GenericRecord] = {
+    if (requiresBytes) {
+      new GenericRecordSchemaRegistryBytesIterator(new CopyingInputStream(is), ec, schemaRegistryClient)
+    } else {
+      new GenericRecordSchemaRegistryIterator(is, ec, schemaRegistryClient)
+    }
   }
-
-  override protected def parse(is: InputStream, ec: EvaluationContext): CloseableIterator[GenericRecord] =
-    new GenericRecordSchemaRegistryIterator(is, schemaRegistryConfig, ec)
 
   override protected def values(
       parsed: CloseableIterator[GenericRecord],
       ec: EvaluationContext): CloseableIterator[Array[Any]] = {
     val array = Array.ofDim[Any](2)
-    parsed.map { record => array(1) = record; array }
+    if (requiresBytes) {
+      parsed.map { record => array(0) = record.get(AvroConverter.BytesField); array(1) = record; array }
+    } else {
+      parsed.map { record => array(1) = record; array }
+    }
   }
 }
 
 object AvroSchemaRegistryConverter {
 
-  private val MAGIC_BYTE_LENGTH = 1
-  private val SCHEMA_ID_LENGTH = 4
+  private val MagicByteLength = 1
+  private val SchemaIdLength = 4
 
   case class AvroSchemaRegistryConfig(
       `type`: String,
@@ -80,24 +77,19 @@ object AvroSchemaRegistryConverter {
       userData: Map[String, Expression]
     ) extends ConverterConfig with ConverterName
 
-  sealed trait SchemaConfig
-
-  case class SchemaRegistry(url: String) extends SchemaConfig
-
   /**
-    * Reads avro records using a cached confluent-style schema registry
-    *
-    * @param is input stream
-    * @param readerCache GenericDatumReader cache
-    * @param ec evaluation context
-    */
-  class GenericRecordSchemaRegistryIterator private [AvroSchemaRegistryConverter] (
-      is: InputStream,
-      readerCache: LoadingCache[Integer, GenericDatumReader[GenericRecord]],
-      ec: EvaluationContext
-    ) extends CloseableIterator[GenericRecord] with LazyLogging{
+   * Reads avro records using a cached confluent-style schema registry
+   *
+   * @param is input stream
+   * @param ec evaluation context
+   * @param client schema registry lookup
+   */
+  private class GenericRecordSchemaRegistryIterator(is: InputStream, ec: EvaluationContext, client: SchemaRegistryClient)
+      extends CloseableIterator[GenericRecord] with LazyLogging{
 
-    private val decoder = DecoderFactory.get.binaryDecoder(is, null)
+    protected val decoder: BinaryDecoder = DecoderFactory.get.binaryDecoder(is, null)
+    private val readers = scala.collection.mutable.Map.empty[Int, GenericDatumReader[GenericRecord]]
+    private val schemaIdBytes = ByteBuffer.wrap(Array.ofDim[Byte](SchemaIdLength))
     private var record: GenericRecord = _
 
     override def hasNext: Boolean = !decoder.isEnd
@@ -105,17 +97,43 @@ object AvroSchemaRegistryConverter {
     override def next(): GenericRecord = {
       ec.line += 1
       // Read confluent-style bytes
-      decoder.skipFixed(MAGIC_BYTE_LENGTH)
-      val bytes = new Array[Byte](SCHEMA_ID_LENGTH)
-      decoder.readFixed(bytes, 0, SCHEMA_ID_LENGTH)
+      decoder.skipFixed(MagicByteLength)
+      decoder.readFixed(schemaIdBytes.array(), 0, SchemaIdLength)
 
-      val id = ByteBuffer.wrap(bytes).getInt()
-      val reader = readerCache.get(id)
+      val id = schemaIdBytes.position(0).getInt()
+      val reader = readers.getOrElseUpdate(id, loadReader(id))
 
       record = reader.read(record, decoder)
       record
     }
 
     override def close(): Unit = is.close()
+
+    protected def loadReader(id: Int): GenericDatumReader[GenericRecord] =
+      new GenericDatumReader[GenericRecord](client.getById(id))
+  }
+
+  /**
+   * Reads avro records using a cached confluent-style schema registry, adding the raw serialized bytes to the record
+   *
+   * @param is input stream
+   * @param ec evaluation context
+   */
+  private class GenericRecordSchemaRegistryBytesIterator(is: CopyingInputStream, ec: EvaluationContext, client: SchemaRegistryClient)
+      extends GenericRecordSchemaRegistryIterator(is, ec, client) with LazyLogging{
+
+    override def next(): GenericRecord = {
+      val record = super.next()
+      // parse out the bytes read and set them in the record
+      // check to see if the decoder buffered some bytes that weren't actually used
+      val buffered = decoder.inputStream().available()
+      record.put(AvroConverter.BytesField, is.replay(is.copied - buffered))
+      record
+    }
+
+    override protected def loadReader(id: Int): GenericDatumReader[GenericRecord] = {
+      val schema = client.getById(id)
+      new GenericDatumReader[GenericRecord](schema, AvroConverter.addBytes(schema))
+    }
   }
 }
