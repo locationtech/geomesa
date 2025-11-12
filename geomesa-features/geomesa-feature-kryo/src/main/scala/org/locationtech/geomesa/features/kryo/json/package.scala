b0cbc626000c48fd2dad36d057edabf9e9ae8b02
Emilio <elahrvivaz@ccri.com>
2025-11-12 12:09:31 -0500

    GEOMESA-3543 Switch to jackson for json parsing (#3458)

---
+++ geomesa-features/geomesa-feature-kryo/src/main/scala/org/locationtech/geomesa/features/kryo/json/package.scala
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * Copyright (c) 2013-2025 General Atomics Integrated Intelligence, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ ***********************************************************************/
+
+package org.locationtech.geomesa.features.kryo
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+package object json {
+  // TODO configure??
+  private[json] val mapper = new ObjectMapper()
+}
