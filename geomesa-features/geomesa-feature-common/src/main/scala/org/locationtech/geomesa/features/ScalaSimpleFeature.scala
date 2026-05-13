0219d34d30b2c4e8c58219cfc87ae80217ba4b08
Emilio <elahrvivaz@ccri.com>
2026-05-13 12:59:44 -0400

    GEOMESA-3579 Fix size estimations in newer java versions (#3546)

--- geomesa-features/geomesa-feature-common/src/main/scala/org/locationtech/geomesa/features/ScalaSimpleFeature.scala
+++ geomesa-features/geomesa-feature-common/src/main/scala/org/locationtech/geomesa/features/ScalaSimpleFeature.scala
@@ -56,7 +56,23 @@ class ScalaSimpleFeature private (sft: SimpleFeatureType, values: Array[AnyRef])
     userData
   }
 
-  override def calculateSizeOf(): Long = Sizable.sizeOf(this) + Sizable.deepSizeOf(id, values, userData)
+  override def calculateSizeOf(): Long = {
+    var size = Sizable.sizeOf(this) + Sizable.sizeOf(id) + Sizable.sizeOf(values)
+    values.foreach { value =>
+      if (value != null) {
+        size += Sizable.sizeOf(value)
+      }
+    }
+    if (userData != null) {
+      userData.forEach { (k, v) =>
+        size += Sizable.sizeOf(k)
+        if (v != null) {
+          size += Sizable.sizeOf(v)
+        }
+      }
+    }
+    size
+  }
 }
 
 object ScalaSimpleFeature {
