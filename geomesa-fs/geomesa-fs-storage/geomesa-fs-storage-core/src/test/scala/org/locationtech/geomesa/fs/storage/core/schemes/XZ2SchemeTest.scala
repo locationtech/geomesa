bf096d60a6c3da1b65853ffee4957369448e6d7f
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2026-06-01 09:31:40 -0400

    GEOMESA-3575 FSDS - Move xz2 filter function into filter module
    * Allow for push-down predicate evaluation in Accumulo/HBase

--- geomesa-fs/geomesa-fs-storage/geomesa-fs-storage-core/src/test/scala/org/locationtech/geomesa/fs/storage/core/schemes/XZ2SchemeTest.scala
+++ geomesa-fs/geomesa-fs-storage/geomesa-fs-storage-core/src/test/scala/org/locationtech/geomesa/fs/storage/core/schemes/XZ2SchemeTest.scala
@@ -18,6 +18,7 @@ import java.util.Random
 class XZ2SchemeTest extends SpecificationWithJUnit {
 
   val sft = SimpleFeatureTypes.createType("test", "*geom:Point:srid=4326")
+  val sftPoly = SimpleFeatureTypes.createType("test", "*geom:Polygon:srid=4326")
 
   "XZ2Scheme" should {
 
@@ -39,18 +40,77 @@ class XZ2SchemeTest extends SpecificationWithJUnit {
       ps.getPartition(ScalaSimpleFeature.create(sft, "1", "POINT (-75 38)")).value mustEqual "66f2db"
     }
 
-    "calculate covering filters" in {
+    "calculate covering filters for points" in {
       val r = new Random(77)
       val features = Seq.tabulate(100) { i =>
         ScalaSimpleFeature.create(sft, s"$i", s"POINT (${r.nextDouble(-180, 180)} ${r.nextDouble(-90, 90)})")
       }
 
-      foreach(Seq(4, 8, 16)) { bits =>
+      foreach(Seq(4, 8, 12, 16)) { bits =>
         val ps = PartitionSchemeFactory.load(sft, s"xz2:bits=$bits")
         ps must beAnInstanceOf[XZ2Scheme]
         foreach(features.groupBy(ps.getPartition)) { case (partition, group) =>
           val filter = ps.getCoveringFilter(partition)
           features.filter(filter.evaluate) must containTheSameElementsAs(group)
+        }
+      }
+    }
+
+    "calculate covering filters for polygons" in {
+      val r = new Random(42)
+
+      // generate polygons of various sizes
+      val features = Seq.tabulate(100) { i =>
+        val centerX = r.nextDouble(-180, 180)
+        val centerY = r.nextDouble(-90, 90)
+
+        // vary the size from small (0.1 degrees) to large (45 degrees)
+        val size = r.nextDouble(0.1, 45.0) / 2
+
+        val xmin = math.max(-180, centerX - size)
+        val ymin = math.max(-90, centerY - size)
+        val xmax = math.min(180, centerX + size)
+        val ymax = math.min(90, centerY + size)
+
+        ScalaSimpleFeature.create(sftPoly, s"$i", s"POLYGON (($xmin $ymin, $xmax $ymin, $xmax $ymax, $xmin $ymax, $xmin $ymin))")
+      }
+
+      foreach(Seq(4, 8, 12, 16)) { bits =>
+        val ps = PartitionSchemeFactory.load(sftPoly, s"xz2:bits=$bits")
+        ps must beAnInstanceOf[XZ2Scheme]
+        foreach(features.groupBy(ps.getPartition)) { case (partition, group) =>
+          val filter = ps.getCoveringFilter(partition)
+          features.filter(filter.evaluate) must containTheSameElementsAs(group)
+        }
+      }
+    }
+
+    "calculate covering filters for mixed geometry sizes" in {
+      val r = new Random(123)
+
+      // mix of very small and very large polygons
+      val features = Seq.tabulate(50) { i =>
+        val centerX = r.nextDouble(-180, 180)
+        val centerY = r.nextDouble(-90, 90)
+
+        // alternate between tiny and huge polygons
+        val size = (if (i % 2 == 0) { r.nextDouble(0.01, 0.1) } else { r.nextDouble(30.0, 90.0) }) / 2
+
+        val xmin = math.max(-180, centerX - size)
+        val ymin = math.max(-90, centerY - size)
+        val xmax = math.min(180, centerX + size)
+        val ymax = math.min(90, centerY + size)
+
+        ScalaSimpleFeature.create(sftPoly, s"$i", s"POLYGON (($xmin $ymin, $xmax $ymin, $xmax $ymax, $xmin $ymax, $xmin $ymin))")
+      }
+
+      foreach(Seq(4, 8, 12, 16)) { bits =>
+        val ps = PartitionSchemeFactory.load(sftPoly, s"xz2:bits=$bits")
+        ps must beAnInstanceOf[XZ2Scheme]
+        foreach(features.groupBy(ps.getPartition)) { case (partition, group) =>
+          val filter = ps.getCoveringFilter(partition)
+          val matched = features.filter(filter.evaluate)
+          matched must containTheSameElementsAs(group)
         }
       }
     }
