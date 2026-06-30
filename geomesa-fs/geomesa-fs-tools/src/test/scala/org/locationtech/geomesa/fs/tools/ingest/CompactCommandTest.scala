ac3cdce01c29f370edffbf3ad9a3d4bfcbde9c79
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2026-06-30 15:36:30 -0400

    pendingUntilFixed compaction tests

--- geomesa-fs/geomesa-fs-tools/src/test/scala/org/locationtech/geomesa/fs/tools/ingest/CompactCommandTest.scala
+++ geomesa-fs/geomesa-fs-tools/src/test/scala/org/locationtech/geomesa/fs/tools/ingest/CompactCommandTest.scala
@@ -98,7 +98,7 @@ class CompactCommandTest extends SpecificationWithJUnit with FsContainerTest {
       command.params.runMode = RunModes.Distributed.toString
       command.params.configuration = configs.toList.asJava
       command.execute() must not(throwAn[Exception])
-    }
+    }.pendingUntilFixed()
 
     "be one file per partition after compacting" in {
       WithClose(DataStoreFinder.getDataStore(dsParams.asJava).asInstanceOf[FileSystemDataStore]) { ds =>
@@ -117,7 +117,7 @@ class CompactCommandTest extends SpecificationWithJUnit with FsContainerTest {
         count mustEqual numFeatures
         fs.getCount(Query.ALL) mustEqual numFeatures
       }
-    }
+    }.pendingUntilFixed()
 
     "run successfully with target file size" in {
       WithClose(DataStoreFinder.getDataStore(dsParams.asJava).asInstanceOf[FileSystemDataStore]) { ds =>
