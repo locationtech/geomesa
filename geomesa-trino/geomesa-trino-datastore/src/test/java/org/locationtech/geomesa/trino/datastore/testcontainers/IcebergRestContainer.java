04c3d23fa869ae12306165d9cc3a5752b76792f5
Emilio <elahrvivaz@ccri.com>
2026-07-30 13:26:28 -0400

    GEOMESA-9236 Trino - Add testcontainers test (#9240)
    Co-authored-by: Christopher Dobbins <chris@codescratch.com>

---
+++ geomesa-trino/geomesa-trino-datastore/src/test/java/org/locationtech/geomesa/trino/datastore/testcontainers/IcebergRestContainer.java
@@ -0,0 +1,34 @@
+/***********************************************************************
+ * Copyright (c) 2013-2025 General Atomics Integrated Intelligence, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ ***********************************************************************/
+
+package org.locationtech.geomesa.trino.datastore.testcontainers;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
+
+public class IcebergRestContainer extends GenericContainer<IcebergRestContainer> {
+    public IcebergRestContainer(String s3AccessKeyId, String s3SecretAccessKey) {
+        super(DockerImageName.parse("apache/iceberg-rest-fixture").withTag(System.getProperty("iceberg.rest.docker.tag")));
+        setExposedPorts(List.of(8181));
+        setWaitStrategy(
+            Wait.forHttp("/v1/config")
+                   .forPort(8181)
+                   .forStatusCode(200)
+        );
+        addEnv("CATALOG_WAREHOUSE", "s3://geomesa/iceberg/");
+        addEnv("CATALOG_IO__IMPL", "org.apache.iceberg.aws.s3.S3FileIO");
+        addEnv("CATALOG_S3_ENDPOINT", "http://minio:9000");
+        addEnv("CATALOG_S3_PATH__STYLE__ACCESS", "true");
+        addEnv("AWS_REGION", "us-east-1");
+        addEnv("AWS_ACCESS_KEY_ID", s3AccessKeyId);
+        addEnv("AWS_SECRET_ACCESS_KEY", s3SecretAccessKey);
+    }
+}
